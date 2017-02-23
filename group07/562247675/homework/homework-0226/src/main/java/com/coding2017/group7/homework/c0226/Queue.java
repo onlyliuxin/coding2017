b76c841d8aeb8a8c65b9b849e371b0016a4dc938
zoakerc <zoakerc@gmail.com>
2017-02-23 17:02:32 +0800

    初始化0226作业模块

---
+++ group07/562247675/homework/homework-0226/src/main/java/com/coding2017/group7/homework/c0226/Queue.java
@@ -0,0 +1,19 @@
+package com.coding2017.group7.homework.c0226;
+
+public class Queue {
+
+    public void enQueue(Object o) {
+    }
+
+    public Object deQueue() {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return -1;
+    }
+}
---
+++ group07/562247675/homework/homework-0226/src/main/java/com/coding2017/group7/homework/c0226/Stack.java
@@ -0,0 +1,24 @@
+package com.coding2017.group7.homework.c0226;
+
+public class Stack {
+    private ArrayList elementData = new ArrayList();
+
+    public void push(Object o) {
+    }
+
+    public Object pop() {
+        return null;
+    }
+
+    public Object peek() {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return -1;
+    }
+}
