335ea91d7276b0634c4a780555eb6f12ecc1a2f0
wa122as <wa122as@>
2017-02-23 18:04:47 +0800

    基本数据结构作业

---
+++ group15/1521_653895972/basic/Iterator.java
@@ -0,0 +1,7 @@
+package com.oneces.tool.basic;
+
+public interface Iterator {
+	public boolean hasNext();
+	public Object next();
+
+}
---
+++ group15/1521_653895972/basic/Stack.java
@@ -0,0 +1,33 @@
+package com.oneces.tool.basic;
+
+public class Stack {
+    private ArrayList elementData = new ArrayList();
+
+    public void push(Object o) {
+        elementData.add(o);
+    }
+
+    public Object pop() {
+        elementData.remove(elementData.size()-1);
+        return null;
+    }
+
+    public Object peek() {
+        return elementData.get(elementData.size()-1);
+    }
+
+    public boolean isEmpty() {
+        return elementData.size()==0?true:false;
+    }
+
+    public int size() {
+        return elementData.size();
+    }
+
+    @Override
+    public String toString() {
+        return "Stack{" +
+                "elementData=" + elementData +
+                '}';
+    }
+}
