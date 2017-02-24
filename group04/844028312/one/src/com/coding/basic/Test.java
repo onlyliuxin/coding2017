ba607c22e22031d8fba74fba5500bb0f4aa80567
hejj <win10@DESKTOP-60P6LRG>
2017-02-24 17:39:50 +0800

    基础数据结构

---
+++ group04/844028312/one/src/com/coding/basic/Test.java
@@ -0,0 +1,27 @@
+package com.coding.basic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+public class Test {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	@org.junit.Test
+	public void arrayListTest(){
+		
+	}
+
+}
