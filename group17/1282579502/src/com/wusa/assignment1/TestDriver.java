ffba0ce27c81eef67ad3c6fdac9c9f72c0d1fb0f
earliest <earliest@hotmail.com>
2017-02-23 18:59:53 -0800

    add first test class

---
+++ group17/1282579502/src/com/wusa/assignment1/TestDriver.java
@@ -0,0 +1,10 @@
+package com.wusa.assignment1;
+
+public class TestDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
