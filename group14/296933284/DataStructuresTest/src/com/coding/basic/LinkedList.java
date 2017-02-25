839bda6a84ffd217f08dfdc3f1a68731cee3f1f4
tennysons <tennysons@163.com>
2017-02-25 16:34:48 +0800

    BinarySearchTree

--- group14/296933284/DataStructuresTest/src/com/coding/basic/LinkedList.java
+++ group14/296933284/DataStructuresTest/src/com/coding/basic/LinkedList.java
@@ -1,7 +1,11 @@
 package com.coding.basic;
 
+import java.util.Collection;
+
+import org.junit.Test;
+
 /**
- * LinkedList 实现 第14小组 296933284
+ * LinkedList (带头结点的单链表) 实现 第14小组 296933284
  * 
  * @author Tonnyson
  *
@@ -10,144 +14,150 @@ public class LinkedList implements List {
 
 	private Node head;
 	private int size;
-	
+
 	public LinkedList() {
 		super();
 		this.head = new Node();
 		this.size = 0;
 	}
 
-	/**
-	 * 尾插法在链表末尾插入节点
-	 */
 	public void add(Object obj) {
+		addLast(obj);
+	}
 
-		if (head.data == null) {
-			head.data = obj;
-			head.next = null;
+	public void add(int index, Object obj) {
+		
+		if (index == size + 1) {
+			addLast(obj);
 		} else {
-
-			Node r = head;
-			while (r.next != null)
-				r = r.next;
-
+			Node r = getPreNode(index);
 			Node node = new Node();
 			node.data = obj;
-			node.next = null;
-
+			node.next = r.next;
 			r.next = node;
+			size++;
 		}
-
-		size++;
-
 	}
 
+	
+	
 	/**
-	 * 在指定索引位置插入节点
-	 */
-	public void add(int index, Object obj) {
-
-		if (index > size)
-			throw new IndexOutOfBoundsException();
-
-		Node r = head;
-		for (int i = 0; i < index - 1; i++)
-			r = r.next;
-
-		Node node = new Node();
-		node.data = obj;
-		node.next = null;
-		node.next = r.next;
-		r.next = node;
-		size++;
-	}
-
-	/**
-	 * 返回指定位置的值
-	 */
-	public Object get(int index) {
-		if (index > size)
-			throw new IndexOutOfBoundsException();
-
-		Node r = head;
-		for (int i = 0; i < index; i++)
-			r = r.next;
-
-		return r.data;
-	}
-
-	/**
-	 * 删除指定位置节点并返回其值
-	 */
-	public Object remove(int index) {
-		if (index > size)
-			throw new IndexOutOfBoundsException();
-
-		Node node = new Node();
-
-		Node r = head;
-		for (int i = 0; i < index - 1; i++)
-			r = r.next;
-
-		node = r.next;
-		r.next = node.next;
-		node.next = null;
-		size--;
-		return node.data;
-	}
-
-	/**
-	 * 返回链表的长度
-	 */
-	public int size() {
-		return size;
-	}
-
-	/**
-	 * 在链表的起始位置插入节点
+	 * 单链表首部插入节点
 	 * 
-	 * @param obj
+	 * @param obj 所插入节点的节点值
+	 *            
 	 */
 	public void addFirst(Object obj) {
 		Node node = new Node();
 		node.data = obj;
-		node.next = head;
-		head = node;
+
+		node.next = head.next;
+		head.next = node;
 		size++;
 	}
 
 	/**
-	 * 在链表尾部插入节点
+	 * 单链表尾部插入节点
 	 * 
-	 * @param obj
+	 * @param obj 所插入节点的节点值
+	 * 
 	 */
 	public void addLast(Object obj) {
-		add(obj);
+		
+		Node node = new Node();
+		node.data = obj;
+		node.next = null;
+		
+		Node r = head;
+		while (r.next != null) r = r.next;
+		
+		r.next = node;
+		
+		size++;
+
 	}
 
 	/**
-	 * 删除链表的第一个节点
+	 * 将集合中所有元素按顺序插入单链表
 	 * 
-	 * @return 所删除节点的值
+	 * @param c 要插入单链表的所有元素的集合
+	 *            
+	 */
+	public void addAll(Collection c) {
+
+		Iterator iter = (Iterator) c.iterator();
+
+		while (iter.hasNext()) {
+			addLast(iter.next());
+		}
+	}
+
+	/**
+	 * 获取指定位置的节点值
+	 */
+	public Object get(int index) {
+		// rangCheck(index);
+		
+		return getPreNode(index).next.data;
+	}
+
+	/**
+	 * 删除指定位置节点，并返回节点值
+	 */
+	public Object remove(int index) {
+		rangCheck(index);
+		
+		Node r = getPreNode(index);
+		
+		Object result = r.next.data;
+		
+		r.next = r.next.next;
+		size--;
+		return result;
+	}
+	
+	/**
+	 * 删除单链表第一个节点，并返回节点值
+	 * 
+	 * @return 第一个节点的值
 	 */
 	public Object removeFirst() {
-		Node node = new Node();
-		node = head;
-		head = head.next;
-		size--;
-
-		return node.data;
+		return remove(0);
 	}
-
+	
 	/**
-	 * 删除链表的最后一个节点
+	 * 删除单链表最后一个节点，并返回节点值
 	 * 
-	 * @return 所删除节点的值
+	 * @return 最后一个节点的值
 	 */
 	public Object removeLast() {
 		return remove(size - 1);
 	}
 	
+	// 获取指定位置的前驱结点并返回
+	private Node getPreNode(int index) {
+		rangCheck(index);
+		
+		if (index == 0) {
+			return head;
+		} else {
+			Node r = head;
+			
+			for (int i = 0; i < index; i++)
+				r = r.next;
+			
+			return r;			
+		}
+		
+	}
+	
+	/**
+	 * 返回单链表的长度
+	 */
+	public int size() {
+		return size;
+	}
+
 	/**
 	 * 迭代器
 	 * 
@@ -159,7 +169,7 @@ public class LinkedList implements List {
 
 	// 迭代器内部类
 	private class Iter implements Iterator {
-		int current;
+		int current = 0;
 
 		@Override
 		public boolean hasNext() {
@@ -170,8 +180,7 @@ public class LinkedList implements List {
 		public Object next() {
 			int i = current;
 			
-			if (i >= size)
-				throw new IndexOutOfBoundsException();
+			rangCheck(i);
 			
 			current++;
 
@@ -179,10 +188,27 @@ public class LinkedList implements List {
 		}
 
 	}
+	
+	/**
+	 * 检查是否越界
+	 * 
+	 * @param index
+	 */
+	private void rangCheck(int index) {
+		if (index > size || index < 0)
+			throw new IndexOutOfBoundsException();
+	}
 
-	// 节点类定义
 	private static class Node {
 		Object data;
 		Node next;
+		
+		public Node() {
+			super();
+			this.data = null;
+			this.next = null;
+		}
+		
+		
 	}
 }
