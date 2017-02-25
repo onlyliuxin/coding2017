8d9cdb460d5ec76c2d81f6c2bb3914f7451fddfa
xiaobo <begin161109@163.com>
2017-02-25 20:00:09 +0800

    up

---
+++ group04/349184132/src/Collection/Onehomework/BinaryTreeNode.java
@@ -0,0 +1,76 @@
+package Collection.Onehomework;
+
+
+
+
+public class BinaryTreeNode {
+	
+	private Object data;
+	private BinaryTreeNode left;
+	private BinaryTreeNode right;
+
+	private BinaryTreeNode root;
+
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+	public BinaryTreeNode getLeft() {
+		return left;
+	}
+	public void setLeft(BinaryTreeNode left) {
+		this.left = left;
+	}
+	public BinaryTreeNode getRight() {
+		return right;
+	}
+	public void setRight(BinaryTreeNode right) {
+		this.right = right;
+	}
+
+
+	public BinaryTreeNode insert(Object o){
+		BinaryTreeNode newNode =null;
+		if(o==null)
+			throw new NullPointerException("数据不能为空");
+
+		if(root==null){
+			root = new BinaryTreeNode();
+			root.setData(o);
+		}else {
+			 newNode = new BinaryTreeNode();
+			 BinaryTreeNode nowNode = root;
+			 int val = (int)root.getData();
+			 nowNode.setData(o);
+			while(true) {
+
+				if ((int)newNode.getData()< val) {
+					if(nowNode.left==null){
+						nowNode.setLeft(newNode);
+					} else {
+						nowNode = nowNode.left;
+					}
+				} else if((int)newNode.getData()> val){
+					if (nowNode.right==null ) {
+					nowNode.setRight(newNode);
+					} else{
+					nowNode = newNode.right;
+					}
+				}else {
+					System.out.println("已存在元素结点");
+					throw new IllegalArgumentException();
+				}
+			}
+		}
+		return newNode;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+
+
+}
