250c46efb36079a16d68b398194d209ec5ba5dcf
thomas_young <yk_ecust_2007@163.com>
2017-02-25 10:02:24 +0800

    完成二叉树

--- group02/812350401/src/com/github/miniyk2012/coding2017/basic/test/BinaryTreeNodeTest.java
+++ group02/812350401/src/com/github/miniyk2012/coding2017/basic/test/BinaryTreeNodeTest.java
@@ -8,26 +8,24 @@ import com.github.miniyk2012.coding2017.basic.BinaryTreeNode;
 
 public class BinaryTreeNodeTest {
 
-	private BinaryTreeNode binaryTreeNode;
+	private BinaryTreeNode<Integer> binaryTreeNode;
 
 	@Before
 	public void setUpBinaryTreeNode() {
-		binaryTreeNode = new BinaryTreeNode();
+		binaryTreeNode = new BinaryTreeNode<Integer>(4);
 	}
 
 	@Test
 	public void testBinaryTreeNodeFunctional() {
-		binaryTreeNode.insert(4);
 		binaryTreeNode.insert(1);
 		binaryTreeNode.insert(3);
 		binaryTreeNode.insert(5);
 		binaryTreeNode.insert(2);
-		assertEquals(4, binaryTreeNode.getData());
-		assertEquals(1, binaryTreeNode.getLeft().getData());
-		assertEquals(5, binaryTreeNode.getRight().getData());
-		assertEquals(3, binaryTreeNode.getLeft().getRight().getData());
-		assertEquals(2, binaryTreeNode.getLeft().getRight().getLeft().getData());
-		// binaryTreeNode.print();
+		assertEquals(new Integer(4), binaryTreeNode.getData());
+		assertEquals(new Integer(1), binaryTreeNode.getLeft().getData());
+		assertEquals(new Integer(5), binaryTreeNode.getRight().getData());
+		assertEquals(new Integer(3), binaryTreeNode.getLeft().getRight().getData());
+		assertEquals(new Integer(2), binaryTreeNode.getLeft().getRight().getLeft().getData());
 	}
 
 }
