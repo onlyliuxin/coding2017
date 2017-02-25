7f333cb526c178d6120f855b5dc050e2d8ca8bbc
qilei <qilei@wepiao.com>
2017-02-25 11:14:40 +0800

    ArrayList completed

---
+++ group04/916758663/learn01/src/main/java/com/coding/basic/ArrayList.java
@@ -0,0 +1,56 @@
+package com.coding.basic;
+
+import java.util.Arrays;
+
+public class ArrayList implements List {
+	
+	private int size = 0;
+	
+	private Object[] elementData = new Object[3];
+	
+	public void add(Object o){
+		add(size,o);
+	}
+	public void add(int index, Object o){
+		if (index > size){
+			throw new IndexOutOfBoundsException();
+		}
+		// 扩容
+		if (size == elementData.length || index + 1 > elementData.length) {
+			int newLength = index + 1 > size * 2 ? index + 1 :size * 2;
+			elementData = Arrays.copyOf(elementData, newLength);
+		}
+		// 移动元素
+		System.arraycopy(elementData,index,elementData,index + 1 ,size-index);
+		elementData[index] = o;
+		size ++ ;
+	}
+	
+	public Object get(int index){
+		checkIndex(index);
+		return elementData[index];
+	}
+	
+	public Object remove(int index){
+		checkIndex(index);
+		Object removed = elementData[index];
+		System.arraycopy(elementData,index + 1,elementData,index,size-1 - index);
+		size --;
+		return removed;
+	}
+
+	private void checkIndex(int index) {
+		if (index > size-1){
+			throw new IndexOutOfBoundsException();
+		}
+	}
+
+	public int size(){
+		return size;
+	}
+	
+	public Iterator iterator(){
+		return null;
+	}
+	
+}
