de383b739b0315cb634196cd0634817b2558eb31
pwh-PC\pwh <pengwh77@126.com>
2017-02-25 21:52:03 -0500

    stack over

--- group20/2421586846/DataStructure/coding/basic/Queue.java
+++ group20/2421586846/DataStructure/coding/basic/Queue.java
@@ -1,5 +1,20 @@
 package basic;
 
 public class Queue {
-
+	private LinkedList elementData = new LinkedList();
+	
+	public void enQueue(Object o){		
+	}
+	
+	public Object deQueue(){
+		return null;
+	}
+	
+	public boolean isEmpty(){
+		return false;
+	}
+	
+	public int size(){
+		return -1;
+	}
 }
