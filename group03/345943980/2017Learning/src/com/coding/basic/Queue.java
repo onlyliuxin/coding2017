70235dfb6050684ab2ac205c5517682caa7b565e
cmhello88 <cm_20094020@163.com>
2017-02-26 19:47:55 +0800

    basic data structure

--- group03/345943980/2017Learning/src/com/coding/basic/Queue.java
+++ group03/345943980/2017Learning/src/com/coding/basic/Queue.java
@@ -1,19 +1,42 @@
 package com.coding.basic;
 
+/**
+ * 队列（堆）：特点，先进先出
+ * 
+ * @author Administrator
+ * 
+ */
 public class Queue {
-	
-	public void enQueue(Object o){		
+
+	private LinkedList linkedList = new LinkedList();
+	private int size = 0;
+
+	/**
+	 * 入队列
+	 * 
+	 * @param o
+	 */
+	public void enQueue(Object obj) {
+		linkedList.add(obj);
+		size++;
 	}
-	
-	public Object deQueue(){
-		return null;
+
+	/**
+	 * 出队列
+	 * 
+	 * @return
+	 */
+	public Object deQueue() {
+		Object obj = linkedList.remove(0);
+		size--;
+		return obj;
 	}
-	
-	public boolean isEmpty(){
-		return false;
+
+	public boolean isEmpty() {
+		return size == 0;
 	}
-	
-	public int size(){
-		return -1;
+
+	public int size() {
+		return size;
 	}
 }
--- group03/345943980/2017Learning/src/com/coding/basic/Stack.java
+++ group03/345943980/2017Learning/src/com/coding/basic/Stack.java
@@ -1,22 +1,51 @@
 package com.coding.basic;
 
+/**
+ * 栈(堆栈)，特点先进后出的特点
+ * 
+ * @author Administrator
+ * 
+ */
 public class Stack {
 	private ArrayList elementData = new ArrayList();
-	
-	public void push(Object o){		
+	private int size = 0;
+
+	/**
+	 * 在堆栈顶部中添加一个元素
+	 * 
+	 * @param o
+	 */
+	public void push(Object o) {
+		elementData.add(o);
+		size++;
 	}
-	
-	public Object pop(){
-		return null;
+
+	/**
+	 * 在堆栈顶部移去一个元素
+	 * 
+	 * @return
+	 */
+	public Object pop() {
+		Object o = elementData.remove(size - 1);
+		size--;
+		return o;
+
 	}
-	
-	public Object peek(){
-		return null;
+
+	/**
+	 * 总是返回栈顶的元素
+	 * 
+	 * @return
+	 */
+	public Object peek() {
+		return elementData.get(size - 1);
 	}
-	public boolean isEmpty(){
-		return false;
+
+	public boolean isEmpty() {
+		return size == 0;
 	}
-	public int size(){
-		return -1;
+
+	public int size() {
+		return size;
 	}
 }
