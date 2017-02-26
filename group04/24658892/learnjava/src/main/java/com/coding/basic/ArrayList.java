5a627734d645d11117111eafe38fce56752e9749
leo <lzdragonix@gmail.com>
2017-02-26 17:19:41 +0800

    实现arraylist

--- group04/24658892/learnjava/src/main/java/com/coding/basic/ArrayList.java
+++ group04/24658892/learnjava/src/main/java/com/coding/basic/ArrayList.java
@@ -1,10 +1,5 @@
 package com.coding.basic;
 
-import com.sun.xml.internal.xsom.impl.scd.Iterators;
-
-import java.util.Arrays;
-import java.util.Collections;
-
 public class ArrayList implements List {
 
     private int size = 0;
@@ -14,23 +9,49 @@ public class ArrayList implements List {
     public void add(Object o) {
         int len = size + 1;
         if (len == elementData.length) {
-            Object[] temp = new Object[len * 2];
-            System.arraycopy(elementData, 0, temp, 0, size);
-            elementData = temp;
+            expandArray();
         }
         elementData[size++] = o;
     }
 
     public void add(int index, Object o) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        int remain = elementData.length - size;
+        if (remain == 0 || index == elementData.length) {
+            expandArray();
+        }
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = o;
+        size++;
+    }
 
+    private void expandArray() {
+        int len = elementData.length;
+        Object[] temp = new Object[len * 2];
+        System.arraycopy(elementData, 0, temp, 0, len);
+        elementData = temp;
     }
 
     public Object get(int index) {
+        if (index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
         return elementData[index];
     }
 
     public Object remove(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Object removed = elementData[index];
+        int len = size - index - 1;
+        if (len > 0) {
+            System.arraycopy(elementData, index + 1, elementData, index, len);
+        }
+        elementData[--size] = null;
+        return removed;
     }
 
     public int size() {
@@ -38,6 +59,19 @@ public class ArrayList implements List {
     }
 
     public Iterator iterator() {
-        return null;
+        return new ArrayListIterator();
+    }
+
+    private class ArrayListIterator implements Iterator {
+
+        private int cursor;
+
+        public boolean hasNext() {
+            return cursor + 1 < size;
+        }
+
+        public Object next() {
+            return elementData[++cursor];
+        }
     }
 }
--- group04/24658892/learnjava/src/test/java/com/coding/basic/ArrayListTest.java
+++ group04/24658892/learnjava/src/test/java/com/coding/basic/ArrayListTest.java
@@ -2,14 +2,35 @@ package com.coding.basic;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class ArrayListTest {
 
     @Test
     public void add() throws Exception {
-        ArrayList data = new ArrayList();
-        int size = 356;
+        ArrayList data;
+        int size;
+        //size < 100
+        data = new ArrayList();
+        size = 33;
+        for (int i = 0; i < size; i++) {
+            data.add(i);
+        }
+        for (int i = 0; i < size; i++) {
+            assertEquals(i, data.get(i));
+        }
+        //size > 100
+        data = new ArrayList();
+        size = 333;
+        for (int i = 0; i < size; i++) {
+            data.add(i);
+        }
+        for (int i = 0; i < size; i++) {
+            assertEquals(i, data.get(i));
+        }
+        //size = 100
+        data = new ArrayList();
+        size = 100;
         for (int i = 0; i < size; i++) {
             data.add(i);
         }
@@ -20,17 +41,54 @@ public class ArrayListTest {
 
     @Test
     public void add1() throws Exception {
-
+        ArrayList data;
+        int size;
+        int index;
+        boolean b;
+        // size < 100;
+        data = new ArrayList();
+        size = 5;
+        index = 2;
+        b = false;
+        for (int i = 0; i < size; i++) {
+            data.add(i);
+        }
+        data.add(index, index + 10000);
+        for (int i = 0; i < data.size(); i++) {
+            if (i == index) {
+                b = true;
+                assertEquals(index + 10000, data.get(i));
+            }
+            else {
+                if (b) {
+                    assertEquals(i - 1, data.get(i));
+                }
+                else {
+                    assertEquals(i, data.get(i));
+                }
+            }
+        }
     }
 
     @Test
     public void get() throws Exception {
-
+        ArrayList data = new ArrayList();
+        data.add(1);
+        data.add(2);
+        data.add(3);
+        assertEquals(2, data.get(1));
     }
 
     @Test
     public void remove() throws Exception {
-
+        ArrayList data = new ArrayList();
+        data.add(1);
+        data.add(2);
+        data.add(3);
+        data.remove(1);
+        assertEquals(2, data.size());
+        assertEquals(1, data.get(0));
+        assertEquals(3, data.get(1));
     }
 
     @Test
@@ -44,7 +102,14 @@ public class ArrayListTest {
 
     @Test
     public void iterator() throws Exception {
-
+        ArrayList data = new ArrayList();
+        data.add(0);
+        data.add(1);
+        data.add(2);
+        Iterator iterator = data.iterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+            assertEquals(++i, iterator.next());
+        }
     }
-
 }
