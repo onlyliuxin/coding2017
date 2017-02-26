d030831204e638542827b0e255e765417a40822f
twb <tianwenbin104@qq.com>
2017-02-26 19:17:28 +0800

    commit

--- group08/283677872/src/com/coding/basic/ArrayList.java
+++ group08/283677872/src/com/coding/basic/ArrayList.java
@@ -1,32 +1,71 @@
 package com.coding.basic;
 
+import java.util.Arrays;
+
 public class ArrayList implements List {
 	
 	private int size = 0;
 	
 	private Object[] elementData = new Object[100];
 	
+	private int stepLength = 50;
+	
 	public void add(Object o){
+		if(size > elementData.length)
+		{
+			Object[] elementDataNew = new Object[elementData.length + stepLength];
+			System.arraycopy(elementData, 0, elementDataNew, 0, elementData.length);
+			elementData = elementDataNew;
+		}
+		
+		elementData[size] = o;
+		size++;
 		
 	}
 	public void add(int index, Object o){
+		if(size > elementData.length)
+		{
+			Object[] elementDataNew = new Object[elementData.length + stepLength];
+			System.arraycopy(elementData, 0, elementDataNew, 0, elementData.length);
+			elementData = elementDataNew;
+		}
+		System.arraycopy(elementData, index, elementData, index+1, size-index+1);
+		elementData[index] = o;
+		size++;
 		
 	}
 	
 	public Object get(int index){
-		return null;
+		return elementData[index];
 	}
 	
 	public Object remove(int index){
-		return null;
+		Object obj = elementData[index];
+		System.arraycopy(elementData, index+1, elementData, index, size-index-1);
+		size--;
+		return obj;
 	}
 	
 	public int size(){
-		return -1;
+		return size;
 	}
 	
 	public Iterator iterator(){
 		return null;
 	}
 	
+	public String toString()
+	{
+		StringBuffer sb = new StringBuffer();
+		for(int i=0;i<size;i++)
+		{
+			if(i!=0)
+			{
+				sb.append(",");
+			}
+			sb.append(elementData[i]);
+		}
+		return sb.toString();
+		
+	}
 }
