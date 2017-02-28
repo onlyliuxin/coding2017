cf9463d869f83b38722baf8b51db16f8ed148f2c
songbao.yang <songbao.yang@qunar.com>
2017-02-28 08:25:52 +0800

    fix bugs, add ut

---
+++ group05/578505552/src/test/java/com/coding/basic/BinaryTreeNodeTest.java
@@ -0,0 +1,29 @@
+package com.coding.basic;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by songbao.yang on 2017/2/28.
+ *
+ */
+public class BinaryTreeNodeTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void insert() throws Exception {
+
+    }
+
+}
