104c2de98c157cb258601b84ee25ae35de0d89d9
shane <baohua.shan@zhulebei.com>
2017-03-01 20:31:37 +0800

    add ArrayUtilTest.java

---
+++ group16/214074094/src/test/java/study/coderising/ArrayUtilTest.java
@@ -0,0 +1,13 @@
+package study.coderising;
+
+import study.AbstractTest;
+
+/**
+ * @Author shane
+ * @Time 2017/3/1 20:29
+ * @Email shanbaohua@lxfintech.com
+ * @Desc ...
+ */
+public class ArrayUtilTest extends AbstractTest {
+
+}
