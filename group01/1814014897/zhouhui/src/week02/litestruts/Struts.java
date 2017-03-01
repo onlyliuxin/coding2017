4b80517b98bcf618ec242fd064e3500b297713d1
周辉 <1814014897@qq.com>
2017-03-01 15:55:08 +0800

    网页版对齐code

--- group01/1814014897/zhouhui/src/week02/litestruts/Struts.java
+++ group01/1814014897/zhouhui/src/week02/litestruts/Struts.java
@@ -44,13 +44,13 @@ public class Struts {
     	DocumentBuilder builder;
     	Document doc = null; 
     	View view = new View();  //实例化View（后面调用view，存储parameters以及jsp，最后return view）
-		try {
-			builder = factory.newDocumentBuilder();
-			File f = new File("src/week02/litestruts/struts.xml");
+	try {
+		builder = factory.newDocumentBuilder();
+		File f = new File("src/week02/litestruts/struts.xml");
 	    	doc = builder.parse(f);
-		} catch (ParserConfigurationException|SAXException|IOException e) {
-			e.printStackTrace();
-		} 
+	} catch (ParserConfigurationException|SAXException|IOException e) {
+		e.printStackTrace();
+	} 
     	
     	//根据actionName找到相对应的action
     	Element root = doc.getDocumentElement();
@@ -70,48 +70,48 @@ public class Struts {
     	//根据action找到相对应的class
     	String actionClass = action.getAttribute("class");
     	try {
-			Class<?> cls = Class.forName(actionClass);
-			Object obj = cls.newInstance();
-			Method setName = cls.getMethod("setName", String.class); 
-			Method setPassword = cls.getMethod("setPassword", String.class);
-			setName.invoke(obj, parameters.get("name"));
-			setPassword.invoke(obj, parameters.get("password"));
+		Class<?> cls = Class.forName(actionClass);
+		Object obj = cls.newInstance();
+		Method setName = cls.getMethod("setName", String.class); 
+		Method setPassword = cls.getMethod("setPassword", String.class);
+		setName.invoke(obj, parameters.get("name"));
+		setPassword.invoke(obj, parameters.get("password"));
 			
-			//通过反射调用对象的exectue 方法,并获得返回值
-			Method execute = cls.getMethod("execute");
-			String exe_val = (String) execute.invoke(obj);
+		//通过反射调用对象的exectue 方法,并获得返回值
+		Method execute = cls.getMethod("execute");
+		String exe_val = (String) execute.invoke(obj);
 			
-			//通过反射找到对象的所有getter方法,通过反射来调用
-			Method[] met = cls.getDeclaredMethods();
-			List list = new LinkedList();
-			for(int i=0;i<met.length;i++){
-				if(met[i].getName().startsWith("get")){
-					list.add(met[i]);
-				}
+		//通过反射找到对象的所有getter方法,通过反射来调用
+		Method[] met = cls.getDeclaredMethods();
+		List list = new LinkedList();
+		for(int i=0;i<met.length;i++){
+			if(met[i].getName().startsWith("get")){
+				list.add(met[i]);
 			}
+		}
 			
-			Method[] getter = new Method[list.size()];
-			for(int i=0;i<list.size();i++){
-				getter[i] = (Method) list.get(i);
-			}
+		Method[] getter = new Method[list.size()];
+		for(int i=0;i<list.size();i++){
+			getter[i] = (Method) list.get(i);
+		}
 			
-			//通过反射来调用,把值和属性形成一个HashMap,例如 {"message","login successful"} 
-			Map<String ,String> param = new HashMap<>();
-			for(int i=0;i<getter.length;i++){
-				if(getter[i].equals(cls.getMethod("getMessage")))
-				param.put("message",(String) getter[i].invoke(obj));
-			}
+		//通过反射来调用,把值和属性形成一个HashMap,例如 {"message","login successful"} 
+		Map<String ,String> param = new HashMap<>();
+		for(int i=0;i<getter.length;i++){
+			if(getter[i].equals(cls.getMethod("getMessage")))
+			param.put("message",(String) getter[i].invoke(obj));
+		}
 			
-			//放到View对象的parameters
-			view.setParameters(param);
+		//放到View对象的parameters
+		view.setParameters(param);
 			
-			//根据struts.xml中的 <result> 配置,以及execute的返回值,确定哪一个jsp,放到View对象的jsp字段中
-			if(exe_val.equals("success"))
-				view.setJsp("/jsp/homepage.jsp");
-			else view.setJsp("/jsp/showLogin.jsp");
+		//根据struts.xml中的 <result> 配置,以及execute的返回值,确定哪一个jsp,放到View对象的jsp字段中
+		if(exe_val.equals("success"))
+			view.setJsp("/jsp/homepage.jsp");
+		else view.setJsp("/jsp/showLogin.jsp");
 			
-		} catch (ClassNotFoundException|InstantiationException|IllegalAccessException
-				|NoSuchMethodException|SecurityException|IllegalArgumentException|InvocationTargetException e) {
+	} catch (ClassNotFoundException|InstantiationException|IllegalAccessException
+		|NoSuchMethodException|SecurityException|IllegalArgumentException|InvocationTargetException e) {
 			e.printStackTrace();
 		} 
     	
