e29c4bb9a90ae677dd86eccfb003b82b3f9689e1
zj <2258659044@qq.com>
2017-03-02 18:03:45 +0800

    cafa

--- group12/2258659044/zj-2017/src/com/coderising/litestruts/Struts.java
+++ group12/2258659044/zj-2017/src/com/coderising/litestruts/Struts.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -18,28 +20,36 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import com.coderising.litestruts.bean.Action;
+import com.coderising.litestruts.bean.Result;
+
 public class Struts {
 
 	private final static String ACTION = "action";
 	private final static String RESULT = "result";
 	private final static String NAME = "name";
 	private final static String CLASS = "class";
+	private final static String TYPE = "type";
 	private final static String EXECUTE = "execute";
 
+	private final static List<Action> actions;
+	static{
+		String path = "src/com/coderising/litestruts/struts.xml";
+		actions = readStrutsXml(path);
+	}
+	
 	public static View runAction(String actionName,
 			Map<String, String> parameters) {
 
-
 		View view = new View();
 		Map<String,Object> viewMap = new HashMap<String,Object>();
-		
-		String path = "src/com/coderising/litestruts/struts.xml";
-		Map<String, String> xmlMap = readStrutsXml(path, actionName);		
+				
+		Action actionBean= getCurrentAction(actionName);	
 
 		try {
-			String calssPath = xmlMap.get(CLASS);
+			String calssPath = actionBean.getClaz();
 			Class<?> clazz = Class.forName(calssPath);
-			Object action = clazz.newInstance();
+			Object instance = clazz.newInstance();
 			Field[] fields = clazz.getDeclaredFields();
 			String fieldName;
 			String methodName;
@@ -49,25 +59,31 @@ public class Struts {
 					methodName = "set" + fieldName.substring(0, 1).toUpperCase()
 							+ fieldName.substring(1);
 					Method method = clazz.getMethod(methodName, fields[i].getType());
-					method.invoke(action, parameters.get(fieldName));
+					method.invoke(instance, parameters.get(fieldName));
 				}				
 			}
 
 			Method successMethos = clazz.getMethod(EXECUTE);			
-			Object result = successMethos.invoke(action);
+			Object result = successMethos.invoke(instance);
 			
 			for (int i = 0; i < fields.length; i++) {
 				fieldName = fields[i].getName();
 				methodName = "get" + fieldName.substring(0, 1).toUpperCase()
 						+ fieldName.substring(1);
 				Method method = clazz.getMethod(methodName);
-				Object value = method.invoke(action);
+				Object value = method.invoke(instance);
 				viewMap.put(fieldName, value);
 
 			}
 			view.setParameters(viewMap);
 
-			String jsp = xmlMap.get(result.toString());
+			List<Result> results = actionBean.getResults();
+			for (int i = 0; i < results.size(); i++) {
+				if(results.get(i).getName()){
+					
+				}
+			}
+			String jsp = actionBean.get(result.toString());
 			view.setJsp(jsp);
 			
 		} catch (Exception e) {
@@ -85,11 +101,15 @@ public class Struts {
 	 * @param actionName
 	 * @return
 	 */
-	private static Map<String, String> readStrutsXml(String filePath,
-			String actionName) {
+	
+	private static List<Action> readStrutsXml(String filePath) {
 
 		File xmlFile = new File(filePath);
-		Map<String, String> xmlMap = new HashMap<String, String>();
+		Action action = null;
+		Result result = null;
+		List<Result> results = null;
+		List<Action> actions = new ArrayList<Action>();
+
 		try {
 			DocumentBuilder documentBuilder = DocumentBuilderFactory
 					.newInstance().newDocumentBuilder();
@@ -100,30 +120,40 @@ public class Struts {
 			for (int i = 0; i < actionNodes.getLength(); i++) {
 				Node actionNode = actionNodes.item(i);
 				if (ACTION.equals(actionNode.getNodeName())) {
+					action = new Action();
+					// 解析action标签
 					NamedNodeMap actionNodeMap = actionNode.getAttributes();
-					String atName = actionNodeMap.getNamedItem(NAME)
+					String actionName = actionNodeMap.getNamedItem(NAME)
 							.getNodeValue();
-					if (atName.equals(actionName)) {
-						String classPath = actionNodeMap.getNamedItem(CLASS)
-								.getNodeValue();
-						xmlMap.put(CLASS, classPath);
-						NodeList resultNodes = actionNode.getChildNodes();
-						for (int j = 0; j < resultNodes.getLength(); j++) {
-							Node resultNode = resultNodes.item(j);
-							if (RESULT.equals(resultNode.getNodeName())) {
-								NamedNodeMap resultNodeMap = resultNode
-										.getAttributes();
-								String jspName = resultNodeMap.getNamedItem(
-										NAME).getNodeValue();
-								String jspPath = resultNode.getTextContent();
-								xmlMap.put(jspName, jspPath);
-							}
+					String claz = actionNodeMap.getNamedItem(CLASS)
+							.getNodeValue();
+					action.setName(actionName);
+					action.setClaz(claz);
+					// 解析result标签
+					NodeList resultNodes = actionNode.getChildNodes();
+					for (int j = 0; j < resultNodes.getLength(); j++) {
+						results = new ArrayList<Result>();
+						Node resultNode = resultNodes.item(j);
+						if (RESULT.equals(resultNode.getNodeName())) {
+							result = new Result();
+							NamedNodeMap resultNodeMap = resultNode
+									.getAttributes();
+							String resultName = resultNodeMap
+									.getNamedItem(NAME).getNodeValue();
+							String resultType = resultNodeMap
+									.getNamedItem(TYPE).getNodeValue();
+							String jspPath = resultNode.getTextContent();
+							result.setName(resultName);
+							result.setType(resultType);
+							result.setJspPath(jspPath);
+							results.add(result);
 						}
-
 					}
+					action.setResults(results);
 				}
-
 			}
+
+			actions.add(action);
 
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
@@ -132,6 +162,16 @@ public class Struts {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return xmlMap;
+		return actions;
 	}	
+	
+	private static Action getCurrentAction(String actionName){
+		
+		for (int i = 0; i < actions.size(); i++) {
+			if(actions.get(i).getName().equals(actionName)){
+				return actions.get(i);
+			}
+		}
+		return null;
+	}
 }
