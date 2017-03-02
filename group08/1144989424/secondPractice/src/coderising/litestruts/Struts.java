14f2ec470bc08e424ea5f675ba72e3a3e4d91512
wayss000 <wayss000@gmail.com>
2017-03-03 00:25:59 +0800

    save to github

--- group08/1144989424/secondPractice/src/coderising/litestruts/Struts.java
+++ group08/1144989424/secondPractice/src/coderising/litestruts/Struts.java
@@ -1,17 +1,43 @@
 package coderising.litestruts;
 
+import java.io.File;
+import java.util.List;
 import java.util.Map;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader; 
 
 
 
 public class Struts {
+    
+    public static void main(String [] args) throws Exception{
+        Document document = readStrutsXml("src/coderising/litestruts/struts.xml");
+        Element rootElement = document.getRootElement();
+        List<Element> childElements = rootElement.elements();
+        for (Element child : childElements) {
+            System.out.println("login" + child.elementText("login"));
+        }
+    }
 
-    public static View runAction(String actionName, Map<String,String> parameters) {
+    public static View runAction(String actionName, Map<String,String> parameters) throws Exception {
 
         /*
          
 		0. 读取配置文件struts.xml
- 		
+ 		*/
+        Document document = readStrutsXml("src/coderising/litestruts/struts.xml");
+        //拿到根Element
+        Element rootElement = document.getRootElement();
+        //用根遍历节点得到Node
+        List<Element> childElements = rootElement.elements();
+        for (Element child : childElements) {
+            String name = child.getName();
+        }
+        //根据Node名字获得值
+        
+        /*
  		1. 根据actionName找到相对应的class ， 例如LoginAction,   通过反射实例化（创建对象）
 		据parameters中的数据，调用对象的setter方法， 例如parameters中的数据是 
 		("name"="test" ,  "password"="1234") ,     	
@@ -29,6 +55,13 @@ public class Struts {
         */
     	
     	return null;
-    }    
+    }
+    
+    private static Document readStrutsXml(String filePath) throws Exception{
+        SAXReader reader = new SAXReader();
+        Document document = reader.read(new File(filePath));
+        
+        return document;
+    }
 
 }
