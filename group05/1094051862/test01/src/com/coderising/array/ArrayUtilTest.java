cf40794c0349edf3a26bba5ed7301f9dad85a2fe
zhaohongxin <zhaohongxin621@126.com>
2017-03-03 18:00:58 +0800

    ArrayUtil 未重构

--- group05/1094051862/test01/src/com/coderising/array/ArrayUtilTest.java
+++ group05/1094051862/test01/src/com/coderising/array/ArrayUtilTest.java
@@ -1,6 +1,7 @@
 package com.coderising.array;
 
 import static org.junit.Assert.*;
+import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
@@ -10,7 +11,6 @@ public class ArrayUtilTest {
 
 	@Before
 	public void setUp() throws Exception {
-		ArrayUtil u;
 	}
 
 	@After
@@ -19,20 +19,30 @@ public class ArrayUtilTest {
 
 	@Test
 	public void testReverseArray() {
-		ArrayUtil util = new ArrayUtil();
 		int[] a = {7,9,30,3,5};
-		util.reverseArray(a);
-		System.out.println();
+		int[] b = a.clone();
+		ArrayUtil.reverseArray(a);
+		for (int i = 0; i < b.length; i ++) {
+			Assert.assertEquals(b[i], a[b.length - 1 - i]);
+		}
+		
 	}
 
 	@Test
 	public void testRemoveZero() {
-		fail("Not yet implemented");
+		int oldArr[]={1,3,4,5,0,0,6,6,0,5,4,7,6,7,0,5};
+		int[] result = {1,3,4,5,6,6,5,4,7,6,7,5};
+		int[] removeZero = ArrayUtil.removeZero(oldArr);
+		assertArrayEquals(result, removeZero);
 	}
 
 	@Test
 	public void testMerge() {
-		fail("Not yet implemented");
+		 int[] a1 = {3,5,7,8};
+		 int[] a2 = {4,5,6,7};
+		 int[] a3 = {3,4,5,6,7,8};
+		 int[] merge = ArrayUtil.merge(a1, a2);
+		 assertArrayEquals(a3, merge);
 	}
 
 	@Test
