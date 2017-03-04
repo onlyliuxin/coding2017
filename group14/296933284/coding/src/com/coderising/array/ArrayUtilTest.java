ef590c86d1ecd4b800acd652ae47e5911b41b42c
Tennyson <tennysons@163.com>
2017-03-04 13:05:50 +0800

    ArrayUtil Test

--- group14/296933284/coding/src/com/coderising/array/ArrayUtilTest.java
+++ group14/296933284/coding/src/com/coderising/array/ArrayUtilTest.java
@@ -1,6 +1,7 @@
 package com.coderising.array;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,79 +22,79 @@ public class ArrayUtilTest {
 
     @After
     public void tearDown() throws Exception {
-        if (newArray != null)
-            for (int i = 0; i < newArray.length; i++)
-                System.out.print(newArray[i] + "\t");
-
-        System.out.println();
     }
 
 
     @Test
     public void reverseArray() throws Exception {
         oldArray = new int[]{7, 9, 30, 3};
-
+        newArray = new int[]{3, 30, 9, 7};
         arrayUtil.reverseArray(oldArray);
 
-        for (int i = 0; i < oldArray.length; i++)
-            System.out.print(oldArray[i] + "\t");
+        Assert.assertArrayEquals(newArray, oldArray);
 
         oldArray = new int[]{3, 30, 9, 7, 4};
-
-        System.out.println();
-
+        newArray = new int[]{4, 7, 9, 30, 3};
         arrayUtil.reverseArray(oldArray);
 
-        for (int i = 0; i < oldArray.length; i++)
-            System.out.print(oldArray[i] + "\t");
+        Assert.assertArrayEquals(newArray, oldArray);
     }
 
     @Test
     public void removeZero() throws Exception {
         oldArray = new int[]{1, 3, 4, 5, 0, 0, 6, 6, 0, 5, 4, 7, 6, 7, 0, 5};
+        newArray = new int[]{1, 3, 4, 5, 6, 6, 5, 4, 7, 6, 7, 5};
 
-        newArray = arrayUtil.removeZero(oldArray);
+        Assert.assertArrayEquals(newArray, arrayUtil.removeZero(oldArray));
+
     }
 
     @Test
     public void merge() throws Exception {
-        int[] a1 = {3, 5, 7, 8}, a2 = {4, 5, 6, 7};
+        int[] a1 = {3, 5, 7, 8};
+        int[] a2 = {4, 5, 6, 7};
+        newArray = new int[]{3, 4, 5, 6, 7, 8};
 
-        newArray = arrayUtil.merge(a1, a2);
+        Assert.assertArrayEquals(newArray, arrayUtil.merge(a1, a2));
     }
 
     @Test
     public void grow() throws Exception {
         oldArray = new int[]{2, 3, 6};
+        newArray = new int[]{2, 3, 6, 0, 0, 0};
         int size = 3;
 
-        newArray = arrayUtil.grow(oldArray, size);
-
+        Assert.assertArrayEquals(newArray, arrayUtil.grow(oldArray, size));
     }
 
     @Test
     public void fibonacci() throws Exception {
-        newArray = arrayUtil.fibonacci(15);
+        newArray = new int[]{1, 1, 2, 3, 5, 8, 13};
+
+        Assert.assertArrayEquals(newArray, arrayUtil.fibonacci(15));
     }
 
     @Test
     public void getPrimes() throws Exception {
-        newArray = arrayUtil.getPrimes(23);
+        newArray = new int[]{2, 3, 5, 7, 11, 13, 17,19};
+
+        Assert.assertArrayEquals(newArray, arrayUtil.getPrimes(23));
     }
 
     @Test
     public void getPerfectNumbers() throws Exception {
-        newArray = arrayUtil.getPerfectNumbers(500);
+        newArray = new int[]{6, 28, 496};
+
+        Assert.assertArrayEquals(newArray, arrayUtil.getPerfectNumbers(500));
     }
 
     @Test
     public void join() throws Exception {
         oldArray = new int[]{3, 8, 9, 10, 2};
         String seperator = "-";
+        String exception = "3-8-9-10-2";
 
-        String str = arrayUtil.join(oldArray, seperator);
-
-        System.out.println(str);
+        Assert.assertEquals(exception, arrayUtil.join(oldArray, seperator));
     }
 
 }
