6d8c2f0926dc97d35759e96e11cb867a9e6b95a3
zzkmz <1509102580@qq.com>
2017-03-06 00:27:50 +0800

    作业修改
    修改了几个读取xml文件的地方

--- group08/1509102580/3.5/litestruts/Struts.java
+++ group08/1509102580/3.5/litestruts/Struts.java
@@ -59,7 +59,9 @@ public class Struts {
 							if (Pattern.matches(gs, methodName)) {
 								param = getM.matcher(methodName)
 										.replaceAll(rapl).toLowerCase();
-								getMethods.put(param, m);
+								if (!param.equals("class")) {
+									getMethods.put(param, m);
+								}
 							} else if (Pattern.matches(ss, methodName)) {
 								param = setM.matcher(methodName)
 										.replaceAll(rapl).toLowerCase();
@@ -73,9 +75,7 @@ public class Struts {
 						while (it.hasNext()) {
 							Entry<String, String> en = it.next();
 							String Mname = en.getKey();
-							Mname = "set" + Mname.substring(0, 1).toUpperCase()
-									+ Mname.substring(1);
-							Method gMethod = getMethods.get(Mname);
+							Method gMethod = setMethods.get(Mname);
 							if (gMethod != null) {
 								gMethod.invoke(o, en.getValue());
 							}
@@ -85,23 +85,30 @@ public class Struts {
 						String result = (String) m1.invoke(o, null);
 
 						// 遍历action子节点，查找对应结果,并返回参数
-						// NodeList childNodes = e.getChildNodes();
-						NodeList childNodes = element
-								.getElementsByTagName("result");
+						NodeList childNodes = e.getChildNodes();
+						/*
+						 * NodeList childNodes = element
+						 * .getElementsByTagName("result");
+						 */
 						for (int j = 0; j < childNodes.getLength(); j++) {
-							Element Child = (Element) childNodes.item(j);
-							String ChileName = Child.getAttribute("name");
-							if (ChileName.equals(result)) {
-								v.setJsp(Child.getNodeValue());
+							if (childNodes.item(j) instanceof Element) {
+								Element Child = (Element) childNodes.item(j);//这里不加判断会报类型无法转换的错误，查找原因，是说xml中的某些文本会被当做节点存入nodelist中
+								String ChileName = Child.getAttribute("name");
+								if (ChileName.equals(result)) {
+									v.setJsp(Child.getTextContent());//不能用getnodevalue，而要用gettextcontent
 
-								// 给view的parameters参数赋值
-								Map<String, String> vparameters = new HashMap<>();
-								for (Entry<String, Method> entry : getMethods.entrySet()) {
-									String tmp = (String) entry.getValue().invoke(o, null);
-									vparameters.put(entry.getKey(), tmp);
+									// 给view的parameters参数赋值
+									Map<String, String> vparameters = new HashMap<>();
+									for (Entry<String, Method> entry : getMethods
+											.entrySet()) {
+										String tmp = (String) entry.getValue()
+												.invoke(o);
+										vparameters.put(entry.getKey(), tmp);
+									}
+									v.setParameters(vparameters);
+									return v;
 								}
-								v.setParameters(vparameters);
-								return v;
+
 							}
 						}
 					} catch (Exception e2) {
