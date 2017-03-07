4c61b24c65f4c10d0ec206108ee2bfb8b94306a6
x_zhaohu <x_zhaohu@163.com>
2017-03-07 21:41:52 +0800

    xiaoti

--- group11/1178243325/DataStructure/src/main/java/com/coding/basic/LinkedList.java
+++ group11/1178243325/DataStructure/src/main/java/com/coding/basic/LinkedList.java
@@ -60,7 +60,6 @@ public class LinkedList implements List {
 	}
 	
 	public void addFirst(Object o){
-		//就是这么硬!
 		add(0, o);
 	}
 
@@ -131,5 +130,160 @@ public class LinkedList implements List {
 		Object getNext() {
 			return next;
 		}
+	}
+
+
+	/**
+	 * 把该链表逆置
+	 * 例如链表为 3->7->10 , 逆置后变为  10->7->3
+	 */
+	public  void reverse(){	
+		Object[] oldData = new Object[size];
+		Node temp = head;
+		int index = 1;
+		while(temp.next != null) {
+			temp = temp.next;
+			oldData[size - index] = temp.data;
+			index++;
+		}
+
+		index = 0;
+		temp = head;
+		while(temp.next != null) {
+			temp = temp.next;
+			temp.data = oldData[index];
+			index++;
+		}
+	}
+
+	/**
+	 * 删除一个单链表的前半部分
+	 * 例如：list = 2->5->7->8 , 删除以后的值为 7->8
+	 * 如果list = 2->5->7->8->10 ,删除以后的值为7,8,10
+	 */
+
+	public  void removeFirstHalf(){
+		int count = size;
+		if (count % 2 != 0) {
+			for (int i = 0; i <= count/2; i++) {
+				removeFirst();
+			}
+		} else {
+			for (int i = 0; i < count/2; i++) {
+				removeFirst();
+			}
+		}
+	}
+
+	/**
+	 * 从第i个元素开始， 删除length 个元素 ， 注意i从0开始
+	 * @param i
+	 * @param length
+	 */
+
+	public  void remove(int i, int length){
+		if (i < 0 || length < 0) {
+			return;
+		}
+		if (i == 0) {
+			for (int k = 0; k < length; k++)
+				removeFirst();
+		} else {
+			while (length > 0) {
+				remove(i-1);
+				length--;
+			}
+		}
+	}
+
+	/**
+	 * 假定当前链表和list均包含已升序排列的整数
+	 * 从当前链表中取出那些list所指定的元素
+	 * 例如当前链表 = 11->101->201->301->401->501->601->701
+	 * listB = 1->3->4->6
+	 * 返回的结果应该是[101,301,401,601]  
+	 * @param list
+	 */
+
+	public static int[] getElements(LinkedList list){
+
+		return null;
+
+	}
+
+	
+
+	/**
+
+	 * 已知链表中的元素以值递增有序排列，并以单链表作存储结构。
+
+	 * 从当前链表中中删除在list中出现的元素 
+
+
+
+	 * @param list
+
+	 */
+
+	
+
+	public  void subtract(LinkedList list){
+
+		
+
+	}
+
+	
+
+	/**
+
+	 * 已知当前链表中的元素以值递增有序排列，并以单链表作存储结构。
+
+	 * 删除表中所有值相同的多余元素（使得操作后的线性表中所有元素的值均不相同）
+
+	 */
+
+	public  void removeDuplicateValues(){
+
+		
+
+	}
+
+	
+
+	/**
+
+	 * 已知链表中的元素以值递增有序排列，并以单链表作存储结构。
+
+	 * 试写一高效的算法，删除表中所有值大于min且小于max的元素（若表中存在这样的元素）
+
+	 * @param min
+
+	 * @param max
+
+	 */
+
+	public  void removeRange(int min, int max){
+
+		
+
+	}
+
+	
+
+	/**
+
+	 * 假设当前链表和参数list指定的链表均以元素依值递增有序排列（同一表中的元素值各不相同）
+
+	 * 现要求生成新链表C，其元素为当前链表和list中元素的交集，且表C中的元素有依值递增有序排列
+
+	 * @param list
+
+	 */
+
+	public  LinkedList intersection( LinkedList list){
+
+		return null;
+
 	}
 }
