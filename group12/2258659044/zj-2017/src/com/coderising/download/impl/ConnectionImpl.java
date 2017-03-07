0701a76a14cf549a50ede813d38abc56f2821b49
zj <2258659044@qq.com>
2017-03-07 18:05:42 +0800

    多线程下载

--- group12/2258659044/zj-2017/src/com/coderising/download/impl/ConnectionImpl.java
+++ group12/2258659044/zj-2017/src/com/coderising/download/impl/ConnectionImpl.java
@@ -1,27 +1,32 @@
 package com.coderising.download.impl;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 
 import com.coderising.download.api.Connection;
 
 public class ConnectionImpl implements Connection{
 
+	private HttpURLConnection urlCon;
+	
 	@Override
 	public byte[] read(int startPos, int endPos) throws IOException {
-		
+				
 		return null;
 	}
 
 	@Override
-	public int getContentLength() {
-		
-		return 0;
+	public int getContentLength() {	
+		return urlCon.getContentLength();
 	}
 
 	@Override
 	public void close() {
-		
-		
+				
 	}
-
+	
+	public void setUrlCon(HttpURLConnection urlCon) {
+		this.urlCon = urlCon;
+	}
+	
 }
--- group12/2258659044/zj-2017/src/com/coderising/download/impl/ConnectionManagerImpl.java
+++ group12/2258659044/zj-2017/src/com/coderising/download/impl/ConnectionManagerImpl.java
@@ -1,5 +1,10 @@
 package com.coderising.download.impl;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import com.coderising.download.api.Connection;
 import com.coderising.download.api.ConnectionException;
 import com.coderising.download.api.ConnectionManager;
@@ -9,7 +14,18 @@ public class ConnectionManagerImpl implements ConnectionManager {
 	@Override
 	public Connection open(String url) throws ConnectionException {
 		
-		return null;
+		URL remotUrl = null;
+		HttpURLConnection urlCon = null;
+		ConnectionImpl conn = new ConnectionImpl();
+		try {
+			remotUrl = new URL(url);    
+			urlCon = (HttpURLConnection)remotUrl.openConnection();
+			conn.setUrlCon(urlCon);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}  
+		return conn;
 	}
-
 }
