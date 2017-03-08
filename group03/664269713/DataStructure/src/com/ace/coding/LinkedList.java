cef4400dc4923498ab7a5220407df2e2983439ed
sawyerwu <664269713@qq.com>
2017-03-09 07:58:55 +0800

    Added the new homework to repository

--- group03/664269713/DataStructure/src/com/ace/coding/LinkedList.java
+++ group03/664269713/DataStructure/src/com/ace/coding/LinkedList.java
@@ -120,4 +120,149 @@ public class LinkedList implements List {
 		Object data;
 		Node next;
 	}
+
+	/**
+	 * 把该链表逆置
+	 * 例如链表为 3->7->10 , 逆置后变为  10->7->3
+	 */
+	public  void reverse(){
+		//check empty
+		Node headNode = head;
+		reverseNode(headNode, null);
+	}
+
+	private Node reverseNode(Node head, Node newHead){
+		if(head == null){
+			return newHead;
+		}
+
+		Node next = head.next;
+		head.next = newHead;
+		return reverseNode(next, head);
+	}
+
+
+
+	/**
+	 * 删除一个单链表的前半部分
+	 * 例如：list = 2->5->7->8 , 删除以后的值为 7->8
+	 * 如果list = 2->5->7->8->10 ,删除以后的值为7,8,10
+	 */
+	public  void removeFirstHalf(){
+		//check empty
+		int count = 0;
+		Node pNode = head;
+		for (int i = 0; i < size() / 2; i++) {
+			pNode = pNode.next;
+			count++;
+		}
+		head = pNode.next;
+		size = size - count;
+	}
+
+	/**
+	 * 从第i个元素开始， 删除length 个元素 ， 注意i从0开始
+	 * @param i
+	 * @param length
+	 */
+	public  void remove(int i, int length){
+		//check empty
+		// check i and length are validate
+
+		Node pNode = getNode(i);
+		Node tempNode = getNode(i+length);
+		pNode.next = tempNode;
+		size = size - length;
+	}
+	/**
+	 * 假定当前链表和list均包含已升序排列的整数
+	 * 从当前链表中取出那些list所指定的元素
+	 * 例如当前链表 = 11->101->201->301->401->501->601->701
+	 * listB = 1->3->4->6
+	 * 返回的结果应该是[101,301,401,601]
+	 * @param list
+	 */
+	public int[] getElements(LinkedList list){
+	    int[] newArray = new int[list.size()];
+	    ArrayList newArrayList = new ArrayList();
+        for(int i = 0; i < list.size(); i++){
+            for(int j = 0; j < this.size(); j++){
+                if(j == (int)list.get(i)){
+                    newArrayList.add(this.get(j));
+                }
+            }
+        }
+
+        for(int k = 0; k < newArray.length; k++){
+            newArray[k] = (int)newArrayList.get(k);
+        }
+		return newArray;
+	}
+
+	/**
+	 * 已知链表中的元素以值递增有序排列，并以单链表作存储结构。
+	 * 从当前链表中中删除在list中出现的元素
+	 * @param list
+	 */
+
+	public  void subtract(LinkedList list){
+        for(int i = 0; i < this.size(); i++){
+            for(int j = 0; j < list.size(); j++){
+                if(this.get(i) == list.get(j)){
+                    this.remove(i);
+                    this.size--;
+                }
+            }
+        }
+	}
+
+	/**
+	 * 已知当前链表中的元素以值递增有序排列，并以单链表作存储结构。
+	 * 删除表中所有值相同的多余元素（使得操作后的线性表中所有元素的值均不相同）
+	 */
+	public  void removeDuplicateValues(){
+        for(int i = 0; i < this.size()-1; i++){
+            if(this.get(i) == this.get(i+1)){
+                this.remove(i+1);
+                size--;
+            }
+        }
+	}
+
+	/**
+	 * 已知链表中的元素以值递增有序排列，并以单链表作存储结构。
+	 * 试写一高效的算法，删除表中所有值大于min且小于max的元素（若表中存在这样的元素）
+	 * @param min
+	 * @param max
+	 */
+	public  void removeRange(int min, int max){
+
+	}
+
+	/**
+	 * 假设当前链表和参数list指定的链表均以元素依值递增有序排列（同一表中的元素值各不相同）
+	 * 现要求生成新链表C，其元素为当前链表和list中元素的交集，且表C中的元素有依值递增有序排列
+	 * @param list
+	 */
+	public  LinkedList intersection( LinkedList list){
+	    LinkedList newLinkedList = new LinkedList();
+        for (int i = 0; i < this.size(); i++) {
+            for(int j = 0; j < list.size(); j++){
+                if(this.get(i) == list.get(j)){
+                    newLinkedList.add(list.get(j));
+                }
+            }
+        }
+        return newLinkedList;
+	}
+
+	private boolean contains(Object obj){
+	    for(int i = 0; i < this.size(); i++){
+	        if(this.get(i) == obj){
+	            return true;
+            }
+        }
+        return false;
+    }
+
 }
