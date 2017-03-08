87fc9941115857425b5bdd53eb3b4eab6e04023d
Aly <guzishiwo@gmail.com>
2017-03-08 22:52:02 +0800

    ArrayList Done

--- liuxin/src/com/coding/basic/ArrayList.java
+++ liuxin/src/com/coding/basic/ArrayList.java
@@ -5,24 +5,45 @@ public class ArrayList implements List {
 	private int size = 0;
 	
 	private Object[] elementData = new Object[100];
-	
+
 	public void add(Object o){
-		
+        elementData[size++] = o;
 	}
 	public void add(int index, Object o){
-		
-	}
+
+	    if (index < 0|| index > size)
+            throw new IndexOutOfBoundsException();
+        for (int j = size; j > index; j--) {
+            elementData[j] = elementData[j++];
+        }
+        elementData[index] = o;
+    }
 	
 	public Object get(int index){
-		return null;
+        if (index < 0|| index > size)
+            throw new IndexOutOfBoundsException();
+		return elementData[index];
 	}
 	
 	public Object remove(int index){
-		return null;
+        if (index < 0|| index > size)
+            throw new IndexOutOfBoundsException();
+        for (int i = 0; i < size; i++) {
+            if(i == index)
+                break
+        }
+        if(i > index)
+            return null;
+        else{
+            for (int j = i; j < size; j++) {
+                elementData[j] = elementData[j++]
+            }
+            return elementData[index]
+        }
 	}
 	
 	public int size(){
-		return -1;
+		return size;
 	}
 	
 	public Iterator iterator(){
