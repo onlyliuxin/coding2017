73464719e655535833d167758a86f1100366010c
GUK0 <1685605435@qq.com>
2017-03-09 11:24:51 +0800

    对之前的作业做一些修改

--- group12/247565311/week2/Struts.java
+++ group12/247565311/week2/Struts.java
@@ -1,12 +1,15 @@
 package week2;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.io.File;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import org.dom4j.*;
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
 public class Struts {
