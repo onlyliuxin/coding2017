ffc07a4ae94ca5232e51a91821bd76ce0a06fa58
laoheihei <studylalala@163.com>
2017-03-09 10:52:45 +0800

    template

---
+++ group22/2622819383/Task1/Queue.java
@@ -0,0 +1,17 @@
+public class Queue {
+	
+	public void enQueue(Object o){		
+	}
+	
+	public Object deQueue(){
+		return null;
+	}
+	
+	public boolean isEmpty(){
+		return false;
+	}
+	
+	public int size(){
+		return -1;
+	}
+}
