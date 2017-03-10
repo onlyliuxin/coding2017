597b6542e417ed9c9b9fe2935a3ae4d369634ed4
wzy <1264835468@qq.com>
2017-03-10 22:18:29 +0800

    downloadV1

--- group17/1264835468/src/assignment/MyLinkedList.java
+++ group17/1264835468/src/assignment/MyLinkedList.java
@@ -90,7 +90,12 @@ public class MyLinkedList<E> implements List<E>, Iterable<E> {
 
 	@Override
 	public String toString() {
-		StringBuilder stringBuilder = new StringBuilder('[');
+		if (size == 0) {
+			return "[]";
+		}
+		StringBuilder stringBuilder = new StringBuilder();
+
+		stringBuilder.append("[");
 		Node<E> temp = head;
 		while (temp != null) {
 			stringBuilder.append(String.valueOf(temp.toString()) + ",");
@@ -140,5 +145,111 @@ public class MyLinkedList<E> implements List<E>, Iterable<E> {
 			return temp.data;
 		}
 
+	}
+
+	/**
+	 * 把该链表逆置 例如链表为 3->7->10 , 逆置后变为 10->7->3
+	 */
+	public void reverse() {
+		if (head == null || head.next == null) {
+			return;
+		}
+		Node<E> node1 = head;
+		Node<E> node2 = head.next;
+		head.next = null;
+		Node<E> node3 = node2.next;
+		while (node3 != null) {
+			node2.next = node1;
+			node1 = node2;
+			node2 = node3;
+			node3 = node3.next;
+		}
+		head = node2;
+		head.next = node1;
+	}
+
+	/**
+	 * 删除一个单链表的前半部分 例如：list = 2->5->7->8 , 删除以后的值为 7->8 如果list = 2->5->7->8->10
+	 * ,删除以后的值为7,8,10
+	 * 
+	 */
+	public void removeFirstHalf() {
+		head = movePtrTo((size + 1) / 2);
+	}
+
+	/**
+	 * 从第i个元素开始， 删除length 个元素 ， 注意i从0开始
+	 * 
+	 * @param i
+	 * @param length
+	 */
+	public void remove(int i, int length) {
+		rangeCheck(i + length - 1);
+		if (i == 0) {
+			head = movePtrTo(i + length - 1).next;
+			return;
+		}
+		Node<E> node = movePtrTo(i - 1);
+		Node<E> newNext = movePtrTo(i + length - 1).next;
+		node.next = newNext;
+	}
+
+	/**
+	 * 假定当前链表和list均包含已升序排列的整数 从当前链表中取出那些list所指定的元素 例如当前链表 =
+	 * 11->101->201->301->401->501->601->701 listB = 1->3->4->6
+	 * 返回的结果应该是[101,301,401,601]
+	 * 
+	 * @param list
+	 */
+	public int[] getElements(MyLinkedList<Integer> list) {
+
+		return null;
+	}
+
+	/**
+	 * 已知链表中的元素以值递增有序排列，并以单链表作存储结构。 从当前链表中中删除在list中出现的元素
+	 * 
+	 * @param list
+	 */
+
+	public void subtract(MyLinkedList list) {
+
+	}
+
+	/**
+	 * 已知当前链表中的元素以值递增有序排列，并以单链表作存储结构。 删除表中所有值相同的多余元素（使得操作后的线性表中所有元素的值均不相同）
+	 */
+	public void removeDuplicateValues() {
+
+	}
+
+	/**
+	 * 已知链表中的元素以值递增有序排列，并以单链表作存储结构。 试写一高效的算法，删除表中所有值大于min且小于max的元素（若表中存在这样的元素）
+	 * 
+	 * @param min
+	 * @param max
+	 */
+	public void removeRange(int min, int max) {
+
+	}
+
+	/**
+	 * 假设当前链表和参数list指定的链表均以元素依值递增有序排列（同一表中的元素值各不相同）
+	 * 现要求生成新链表C，其元素为当前链表和list中元素的交集，且表C中的元素有依值递增有序排列
+	 * 
+	 * @param list
+	 */
+	public MyLinkedList intersection(MyLinkedList list) {
+		return null;
+	}
+
+	public static void main(String[] args) {
+		MyLinkedList<Integer> linkedList = new MyLinkedList<>();
+		linkedList.add(1);
+		linkedList.add(2);
+		linkedList.add(3);
+		System.out.println(linkedList);
+		linkedList.remove(0, 3);
+		System.out.println(linkedList);
 	}
 }
