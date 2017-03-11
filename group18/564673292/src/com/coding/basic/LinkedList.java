f73970fd5da638a3e590f0bbb2696de38c1aa527
lqt0223 <lqt19920223@gmail.com>
2017-03-11 15:09:47 +0800

    LinkedList uil completed

--- group18/564673292/src/com/coding/basic/LinkedList.java
+++ group18/564673292/src/com/coding/basic/LinkedList.java
@@ -124,10 +124,10 @@ public class LinkedList<E> implements List<E>, Iterable<E> {
      * 例如链表为 3->7->10 , 逆置后变为  10->7->3
      */
     public void reverse(){
-        head.next = getNode(size() - 1);
         for (int i = size() - 1; i > 0; i--) {
             getNode(i).next = getNode(i - 1);
         }
+        head.next = last;
     }
 
     /**
@@ -136,7 +136,7 @@ public class LinkedList<E> implements List<E>, Iterable<E> {
      * 如果list = 2->5->7->8->10 ,删除以后的值为7,8,10
      */
     public void removeFirstHalf(){
-
+        remove(0, (int) Math.floor(size() / 2));
     }
 
     /**
@@ -144,8 +144,10 @@ public class LinkedList<E> implements List<E>, Iterable<E> {
      * @param i
      * @param length
      */
-    public  void remove(int i, int length){
-
+    public void remove(int i, int length){
+        for (int j = length + i - 1 ;j >= i; j--) {
+            remove(j);
+        }
     }
     /**
      * 假定当前链表和listB均包含已升序排列的整数
@@ -155,8 +157,16 @@ public class LinkedList<E> implements List<E>, Iterable<E> {
      * 返回的结果应该是[101,301,401,601]
      * @param list
      */
-    public  int[] getElements(LinkedList list){
-        return null;
+    public int[] getElements(LinkedList list){
+        Iterator itr = list.iterator();
+        int i;
+        int j = 0;
+        int[] result = new int[list.size()];
+        while(itr.hasNext()){
+            i = (int) itr.next();
+            result[j++] = (Integer)get(i);
+        }
+        return result;
     }
 
     /**
@@ -165,16 +175,32 @@ public class LinkedList<E> implements List<E>, Iterable<E> {
      * @param list
      */
 
-    public  void subtract(LinkedList list){
-
+    public void subtract(LinkedList list){
+        for (int i = 0; i < list.size(); i++) {
+            E b = (E) list.get(i);
+            for (int j = 0; j < size(); j++) {
+                E a = get(j);
+                if(a.equals(b)){
+                    remove(j--);
+                }
+            }
+        }
     }
 
     /**
      * 已知当前链表中的元素以值递增有序排列，并以单链表作存储结构。
      * 删除表中所有值相同的多余元素（使得操作后的线性表中所有元素的值均不相同）
      */
-    public  void removeDuplicateValues(){
-
+    public void removeDuplicateValues(){
+        for (int i = 0; i < size() - 1; i++) {
+            E a = get(i);
+            for (int j = i + 1; j < size(); j++) {
+                E b = get(j);
+                if(a.equals(b)){
+                    remove(j--);
+                }
+            }
+        }
     }
 
     /**
@@ -183,8 +209,33 @@ public class LinkedList<E> implements List<E>, Iterable<E> {
      * @param min
      * @param max
      */
-    public  void removeRange(int min, int max){
-
+    public void removeRange(int min, int max){
+        int i = 0;
+        int j = size() - 1;
+        int a;
+        int b;
+        int flag = 0; // for toggling direction
+        while(i < j){
+            // search the head cursor
+            if(flag == 0){
+                a = (Integer) get(i);
+                if(a > min){
+                    flag = 1;
+                }else{
+                    i++;
+                }
+            }
+            // search the tail cursor
+            else if(flag == 1){
+                b = (Integer) get(j);
+                if(b < max){
+                    break;
+                }else{
+                    j--;
+                }
+            }
+        }
+        remove(i, j - i + 1);
     }
 
     /**
@@ -192,7 +243,19 @@ public class LinkedList<E> implements List<E>, Iterable<E> {
      * 现要求生成新链表C，其元素为当前链表和list中元素的交集，且表C中的元素有依值递增有序排列
      * @param list
      */
-    public  LinkedList intersection( LinkedList list){
-        return null;
+    public LinkedList intersection(LinkedList list){
+        LinkedList<E> c = new LinkedList<>();
+
+        for (int i = 0; i < size(); i++) {
+            E a = get(i);
+            for (int j = 0; j < list.size(); j++) {
+                E b = (E) list.get(j);
+                if(a.equals(b)){
+                    c.add(b);
+                }
+            }
+        }
+        return c;
     }
+
 }
