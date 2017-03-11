f7cf095ed9c61f18d65ebc059422b3fadf4d7582
lzb <lzbferrari@gmail.com>
2017-03-11 22:51:22 +0800

    第一周作业，二叉树明天写

---
+++ group24/1148285693/learning2017/learning-basic/src/main/java/me/lzb/homework0312/basic/BinaryTreeNode.java
@@ -0,0 +1,36 @@
+package me.lzb.homework0312.basic;
+
+/**
+ * 左边比父节点大，右边比父节点小
+ * Created by LZB on 2017/3/11.
+ */
+public class BinaryTreeNode {
+	
+	private Object data;
+	private BinaryTreeNode left;
+	private BinaryTreeNode right;
+	
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+	public BinaryTreeNode getLeft() {
+		return left;
+	}
+	public void setLeft(BinaryTreeNode left) {
+		this.left = left;
+	}
+	public BinaryTreeNode getRight() {
+		return right;
+	}
+	public void setRight(BinaryTreeNode right) {
+		this.right = right;
+	}
+	
+	public BinaryTreeNode insert(Object o){
+		return  null;
+	}
+	
+}
