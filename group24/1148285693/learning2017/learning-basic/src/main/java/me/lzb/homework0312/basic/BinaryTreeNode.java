17fe8cade36bc93d912e63311438de9fb1ba5750
lzb <lzbferrari@gmail.com>
2017-03-12 00:25:15 +0800

    二叉树增加节点方法

--- group24/1148285693/learning2017/learning-basic/src/main/java/me/lzb/homework0312/basic/BinaryTreeNode.java
+++ group24/1148285693/learning2017/learning-basic/src/main/java/me/lzb/homework0312/basic/BinaryTreeNode.java
@@ -1,36 +1,53 @@
 package me.lzb.homework0312.basic;
 
 /**
- * 左边比父节点大，右边比父节点小
+ * 左边比父节点小，右边比父节点大
  * Created by LZB on 2017/3/11.
  */
 public class BinaryTreeNode {
 	
-	private Object data;
+	private int data;
 	private BinaryTreeNode left;
 	private BinaryTreeNode right;
-	
-	public Object getData() {
+
+	public BinaryTreeNode(int data){
+	    this.data = data;
+    }
+
+	public int getData() {
 		return data;
 	}
-	public void setData(Object data) {
-		this.data = data;
+
+
+    //这层满了就下一层继续add,直到找到空位
+    public void add(int d){
+        BinaryTreeNode b = new BinaryTreeNode(d);
+        if(compareTo(b)){
+            //比父节点小，左边
+            if(this.left == null){
+                this.left = b;
+            }else {
+                this.left.add(d);
+            }
+
+        }else {//相等不考虑
+            //比父节点大，右边
+            if(this.right == null){
+                this.right = b;
+            }else {
+                this.right.add(d);
+            }
+
+        }
+    }
+
+
+	public boolean compareTo(BinaryTreeNode node){
+		if(this.data > node.getData()){
+			return true;
+		}
+		return false;
 	}
-	public BinaryTreeNode getLeft() {
-		return left;
-	}
-	public void setLeft(BinaryTreeNode left) {
-		this.left = left;
-	}
-	public BinaryTreeNode getRight() {
-		return right;
-	}
-	public void setRight(BinaryTreeNode right) {
-		this.right = right;
-	}
-	
-	public BinaryTreeNode insert(Object o){
-		return  null;
-	}
-	
+
+
 }
