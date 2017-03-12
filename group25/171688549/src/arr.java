aae2a5695be4d89a6badc6e2733ee59ce31d4a22
wujingxin <you@example.com>
2017-03-13 00:01:19 +0800

    Signed-off-by: wujingxin <you@example.com>

---
+++ group25/171688549/src/arr.java
@@ -0,0 +1,15 @@
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+class  Demo
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	System.out.println("a");
+	System.out.print("a");
+	System.out.print("a");
+
+
+	}
+	
+}
