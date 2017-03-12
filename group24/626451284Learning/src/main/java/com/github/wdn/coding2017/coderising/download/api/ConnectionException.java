dcf2f2d29a8aa24880f23f73b1b53169fa21c63e
wdn <626451284@163.com>
2017-03-12 17:28:50 +0800

    去掉本周不相关作业

--- group24/626451284Learning/src/main/java/com/github/wdn/coding2017/coderising/download/api/ConnectionException.java
+++
@@ -1,5 +0,0 @@
-package com.github.wdn.coding2017.coderising.download.api;
-
-public class ConnectionException extends Exception {
-
-}
--- group24/626451284Learning/src/main/java/com/github/wdn/coding2017/coderising/download/api/DownloadListener.java
+++
@@ -1,5 +0,0 @@
-package com.github.wdn.coding2017.coderising.download.api;
-
-public interface DownloadListener {
-	public void notifyFinished();
-}
