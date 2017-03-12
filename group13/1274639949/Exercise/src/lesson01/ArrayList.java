c8c48b0f07e376e679eb88bd00f191c630be4bfd
XiangYangHan <hans.2015soar@gmail.com>
2017-03-12 10:07:08 +0800

    record01
    完成 ArrayList的实现，参考了jdk源码中对内部类Iter的实现方式

--- group13/1274639949/Exercise/src/lesson01/ArrayList.java
+++ group13/1274639949/Exercise/src/lesson01/ArrayList.java
@@ -1,6 +1,7 @@
 package lesson01;
 
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 
 public class ArrayList<E> implements List<E> {
 
@@ -49,64 +50,149 @@ public class ArrayList<E> implements List<E> {
 	@Override
 	
 	public void clear() {
-		elementData = new Object[DEFAULT_CAPACITY];
+		elementData = new Object[elementData.length];
 		size = 0;
 	}
 
 	@Override
 	public boolean contains(Object o) {
-		for(int i = 0; i < size; i++){
-			
+		if(o == null){
+			for(int i = 0; i < size; i++){
+				if(elementData[i] == null){
+					return true;
+				}
+			}
+		}else{
+			for(int i = 0; i < size; i++){
+				if(o.equals(elementData[i])){
+					return true;
+				}
+			}
 		}
 		return false;
 	}
 
 	@Override
 	public int indexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
+		if(o == null){
+			for(int i = 0; i < size; i++){
+				if(elementData[i] == null){
+					return i;
+				}
+			}
+		}else{
+			for(int i = 0; i < size; i++){
+				if(o.equals(elementData[i])){
+					return i;
+				}
+			}
+		}
+		return -1;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public E get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		checkIndex(index);
+		return (E) elementData[index];
 	}
 
+	private void checkIndex(int index) {
+		if(index < 0 || index >= size){
+			throw new IndexOutOfBoundsException("Illegal index:" + index);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public E remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		checkIndex(index);
+		
+		E e = (E) elementData[index];
+		//此处应注意要判断index是否为elementData最后一个元素的下标，因为在下面的arrayCopy方法中要访问index+1的位置，此时有可能发生数组越界。
+		if(index == size -1){
+			size --;
+		}else{
+			System.arraycopy(elementData, index + 1, elementData, index, size - index - 1);
+		}
+		return e;
 	}
 
 	@Override
 	public boolean remove(Object o) {
-		// TODO Auto-generated method stub
-		return false;
+		int index = indexOf(o);
+		if(index >= 0){
+			remove(index);
+			return true;
+		}else{
+			return false;
+		}
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public E set(int index, E element) {
-		// TODO Auto-generated method stub
-		return null;
+		checkIndex(index);
+		E e = (E)elementData[index];
+		elementData[index] = element;
+		return e;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
 	@Override
 	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
+		return Arrays.copyOf(elementData, size);
+	}
+	
+	/**
+	 * 将ArrayList中全部元素存入一个运行时确定类型的数组中
+	 * @param t
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public <T> T[] toArray(T[] t){
+		if(t.length < size){
+			t = (T[]) Arrays.copyOf(elementData, size, t.getClass());
+		}else{
+			System.arraycopy(elementData, 0, t, 0, size);
+		}
+		return t;
 	}
 
 	@Override
 	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Iter();
 	}
+	
+	private final class Iter implements Iterator<E>{
+		
+		int pos = 0;
+		int lastRet = -1;
 
+		@Override
+		public boolean hasNext() {
+			return pos < size;
+		}
+
+		@Override
+		public E next() {
+			if(!hasNext()){
+				throw new NoSuchElementException();
+			}
+			lastRet = pos++;
+			return get(lastRet);
+		}
+
+		@Override
+		public void remove() {
+			if(lastRet < 0){
+				throw new IllegalStateException();
+			}
+			ArrayList.this.remove(lastRet);
+		}
+	}
 }
