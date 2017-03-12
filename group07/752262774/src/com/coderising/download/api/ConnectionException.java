4892b811d7c7d48c0e931b8bc41f6c97c9b73f52
ByrsH <bgxjsh@sina.com>
2017-03-12 19:00:39 +0800

    fileDownloader

--- group07/752262774/src/com/coderising/download/api/ConnectionException.java
+++ group07/752262774/src/com/coderising/download/api/ConnectionException.java
@@ -1,5 +1,30 @@
 package com.coderising.download.api;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 public class ConnectionException extends Exception {
+
+    public ConnectionException (String msg) {
+        super(msg);
+    }
+
+    public ConnectionException (MalformedURLException e) {
+        super(e);
+    }
+
+    public ConnectionException (IOException e){
+        super(e);
+    }
+
+    public ConnectionException (FileNotFoundException e){
+        super(e);
+    }
+
+    public ConnectionException (InterruptedException e){
+        super(e);
+    }
+
 
 }
--- group07/752262774/src/com/coderising/download/impl/ConnectionManagerImpl.java
+++ group07/752262774/src/com/coderising/download/impl/ConnectionManagerImpl.java
@@ -4,12 +4,27 @@ import com.coderising.download.api.Connection;
 import com.coderising.download.api.ConnectionException;
 import com.coderising.download.api.ConnectionManager;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class ConnectionManagerImpl implements ConnectionManager {
 
 	@Override
 	public Connection open(String url) throws ConnectionException {
-		
-		return null;
+		Connection conn = null;
+		try {
+			URL urlObj = new URL(url);
+			URLConnection urlConn = urlObj.openConnection();
+            conn = new ConnectionImpl(urlConn);
+		} catch (MalformedURLException e) {
+			throw new ConnectionException(e);
+		} catch (IOException e) {
+			throw new ConnectionException(e);
+		}
+
+		return conn;
 	}
 
 }
