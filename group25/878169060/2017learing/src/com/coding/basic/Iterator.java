6f84215b7b502dc4f2dd32076b00bb9fa4f71b9d
sunzihu <878169060@qq.com>
2017-03-12 22:40:48 +0800

    123
    123

---
+++ group25/878169060/2017learing/src/com/coding/basic/Iterator.java
@@ -0,0 +1,14 @@
+package com.coding.basic;
+/**
+ * 
+ * @ClassName:     Iterator 
+ * @Description:TODO(Iterator迭代器 用于遍历Collection集合的元素)
+ * 
+ * @author:    QQ：878169060
+ * @date:        2017年3月11日 上午12:22:13
+ */
+public interface Iterator extends java.util.Iterator {
+	public boolean hasNext();
+	public Object next();
+
+}
