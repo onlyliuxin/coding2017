4bb2998654f7e8ef57c27766474fa429dc28eefd
amin <122328576@qq.com>
2017-03-13 11:28:55 +0800

    删除中文乱码

--- group25/122328576/src/com/github/AminLiu/dataStructure/ArrayList.java
+++ group25/122328576/src/com/github/AminLiu/dataStructure/ArrayList.java
@@ -17,32 +17,27 @@ public class ArrayList implements List {
 
 	public void add(int index, Object o) {
 		if (index > this.size || index < 0) {
-			throw new IndexOutOfBoundsException("Index: " + index + ", Size: "
-					+ this.size);
+			throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + this.size);
 		}
 		this.checkSize(this.size + 1);
-		System.arraycopy(this.elementData, index, this.elementData, index + 1,
-				this.size - index);
-		this.elementData[index] = o;// 澶嶅埗瀹屽皢鐩爣瀵硅薄澶嶅埗鍒癷ndex浣嶇疆
+		System.arraycopy(this.elementData, index, this.elementData, index + 1, this.size - index);
+		this.elementData[index] = o;
 		++this.size;
 	}
 
 	public Object get(int index) {
 		if (index < 0 || index > this.size - 1) {
-			throw new IndexOutOfBoundsException("Index: " + index + ", Size: "
-					+ this.size);
+			throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + this.size);
 		}
 		return this.elementData[index];
 	}
 
 	public Object remove(int index) {
 		if (index < 0 || index > this.size - 1) {
-			throw new IndexOutOfBoundsException("Index: " + index + ", Size: "
-					+ this.size);
+			throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + this.size);
 		}
 		Object o = this.elementData[index];
-		System.arraycopy(this.elementData, index + 1, this.elementData, index,
-				this.size - index - 1);
+		System.arraycopy(this.elementData, index + 1, this.elementData, index, this.size - index - 1);
 		this.elementData[--this.size] = null;
 		return o;
 	}
@@ -51,9 +46,8 @@ public class ArrayList implements List {
 		return this.size;
 	}
 
-	// 妫�鏌ユ暟缁勫ぇ灏忥紝灏忎簡鎵╁
 	private void checkSize(int arg0) {
-		if (arg0 > Integer.MAX_VALUE) {// 瓒呰繃int鏈�澶у��
+		if (arg0 > Integer.MAX_VALUE) {
 			throw new OutOfMemoryError();
 		}
 		if (this.elementData.length < arg0) {
@@ -61,9 +55,8 @@ public class ArrayList implements List {
 		}
 	}
 
-	// 鎵╁
 	private void addCapacity(int arg0) {
-		int arg1 = arg0 + (arg0 >> 1);// 澧炲姞鍚庢暟缁勫ぇ灏�=鍘熸暟缁�+鍘熸暟缁�/2
+		int arg1 = arg0 + (arg0 >> 1);
 		this.elementData = Arrays.copyOf(this.elementData, arg1);
 	}
 
@@ -74,12 +67,10 @@ public class ArrayList implements List {
 	private class ite implements Iterator {
 		int arg0;
 
-		@Override
 		public boolean hasNext() {
 			return arg0 != size;
 		}
 
-		@Override
 		public Object next() {
 			int i = arg0;
 			if (i >= size)
