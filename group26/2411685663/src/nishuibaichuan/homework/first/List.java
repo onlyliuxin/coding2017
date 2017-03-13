b95cb325808d8ce6cf735e177e39cffd05517ae6
zhangrui <2411685663@qq.com>
2017-03-13 10:46:46 +0800

    测试目录

---
+++ group26/2411685663/src/nishuibaichuan/homework/first/List.java
@@ -0,0 +1,13 @@
+package JEE_2411685663;
+
+public interface List {
+	public void add(Object o);
+
+	public void add(int index, Object o);
+
+	public Object get(int index);
+
+	public Object remove(int index);
+
+	public int size();
+}
