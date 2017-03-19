33ebc240367d253b1ebf8d83f790cd21d6ca7620
tankexiaomao <514302761@qq.com>
2017-03-20 00:25:03 +0800

    没测试

--- group27/514302761/src/com/company/coderising/litestruts/Struts.java
+++ group27/514302761/src/com/company/coderising/litestruts/Struts.java
@@ -1,5 +1,14 @@
 package com.company.coderising.litestruts;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -9,7 +18,7 @@ public class Struts {
     public static View runAction(String actionName, Map<String,String> parameters) {
 
         /*
-         
+
 		0. 读取配置文件struts.xml
  		
  		1. 根据actionName找到相对应的class ， 例如LoginAction,   通过反射实例化（创建对象）
@@ -27,7 +36,45 @@ public class Struts {
 		放到View对象的jsp字段中。
         
         */
-    	
+        try {
+            DocumentBuilderFactory dbfactory=DocumentBuilderFactory.newInstance();
+            DocumentBuilder dbuilder=dbfactory.newDocumentBuilder();
+            Document doc=dbuilder.parse("src/com/company/litestruts/struts.xml");
+            doc.getDocumentElement().normalize();
+
+            NodeList list=doc.getElementsByTagName("action");
+            for(int i=0;i<list.getLength();i++){
+                Node node=list.item(i);
+                Element element= (Element) node;
+                if (element.getAttribute("name").equals(actionName)){
+                    Class c=Class.forName(element.getAttribute("class"));
+                    Object o=c.getInterfaces();
+                    Method setName=c.getDeclaredMethod("setName",String.class);
+                    setName.invoke(o,parameters.get("name"));
+                    Method setPassword = c.getDeclaredMethod("setPassword", String.class);
+                    setPassword.invoke(o, parameters.get("password"));
+                    Method execute=c.getDeclaredMethod("execute",null);
+                    String result=execute.invoke(o).toString();
+                    Method getMessage=c.getDeclaredMethod("getMessage",null);
+                    HashMap<String,String > map=new HashMap<String, String>();
+                    map.put("message",getMessage.invoke(o).toString());
+                    View view=new View();
+                    view.setParameters(map);
+                    NodeList nodeListT=element.getElementsByTagName("result");
+                    for (int j=0;j<nodeListT.getLength();j++){
+                        Node nodeT=list.item(j);
+                        Element elementT= (Element) nodeT;
+                        if (elementT.getAttribute("name").equals(result)){
+                            view.setJsp(nodeT.getTextContent());
+                        }
+                    }
+                    return view;
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     	return null;
     }    
 
