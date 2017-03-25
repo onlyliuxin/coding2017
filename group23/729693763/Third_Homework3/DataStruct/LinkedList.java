349cd580aadfa428182d59f3e8f68d6fd33dbb47
em14Vito <soulkillty@163.com>
2017-03-25 11:12:51 +0800

    finish hw3

--- group23/729693763/Third_Homework3/DataStruct/LinkedList.java
+++ group23/729693763/Third_Homework3/DataStruct/LinkedList.java
@@ -1,42 +1,152 @@
-package com.coding.basic;
+package com.zhous.DataStruct;
+
+import javafx.beans.binding.ObjectExpression;
+
+
 
 public class LinkedList implements List {
 	
-	private Node head;
-	
+	private Node head = null;
+	private int size = 0;
+
 	public void add(Object o){
-		
+		addLast(o);
+		this.size++;
 	}
+
+	//内部调用的方法：
+	//2 返回添加到哪个Node
+	private Node findBeforeInsertNode(int needInsertIndex){
+		Node findGetNode = this.head;
+		for ( int i=0; i< needInsertIndex - 1; i++ ) {
+			findGetNode = findGetNode.next;
+		}
+		return findGetNode;
+	}
+	//3
+	private void checkPositionIndex(int index) {
+		if ( !( index >= 0 && index <= size ) )
+			throw new IndexOutOfBoundsException("Invalid Index:"+index);
+	}
+
+	//4
+	private void addBeforeNode(int index, Object o) {
+		Node newNode = new Node();
+		newNode.data = o;
+		newNode.next = null;
+
+		Node beforeInserNode = this.head;
+
+
+			beforeInserNode =  findBeforeInsertNode(index);
+
+			newNode.next = beforeInserNode.next;
+			beforeInserNode.next = newNode;
+
+	}
+	//5
+	private Node findIndexNode(int index){
+		Node findGetNode = this.head;
+		for ( int i=0; i< index; i++ ) {
+			findGetNode = findGetNode.next;
+		}
+		return findGetNode;
+	}
+
+
 	public void add(int index , Object o){
-		
+		checkPositionIndex(index);
+		if( index == size()){
+			addLast(o);
+		} else if ( index == 0 ) {
+			addFirst(o);
+		} else{
+			addBeforeNode(index,o);
+		}
+		this.size++;
 	}
+
+
+
+
 	public Object get(int index){
-		return null;
+		checkElementIndex(index);
+		return findIndexNode(index);
 	}
 	public Object remove(int index){
-		return null;
+		checkElementIndex(index);
+		Object deleteData = null;
+
+		if(index == 0){
+			deleteData = removeFirst();
+		} else if(index == (size() - 1) ){
+			deleteData = removeLast();
+		} else {
+			Node temp = findBeforeInsertNode(index);
+			Node tempNext = temp.next;
+			deleteData = tempNext.data;
+
+			temp.next = tempNext.next;
+			tempNext = null;
+		}
+
+		return deleteData;
 	}
-	
+
+	//6
+	private void checkElementIndex(int index) {
+		if ( !( index >= 0 && index < size ) )
+			throw new IndexOutOfBoundsException("Invalid Index:"+index);
+	}
+
 	public int size(){
-		return -1;
+		return this.size;
 	}
 	
 	public void addFirst(Object o){
-		
+		Node newNode = new Node();
+		newNode.data = o;
+		newNode.next = null;
+		if ( this.head == null  ) {
+			this.head = newNode;
+		} else {
+			newNode.next = this.head;
+			this.head = newNode;
+		}
 	}
-	public void addLast(Object o){
-		
+	private void addLast(Object o){
+		Node newNode = new Node();
+		newNode.data = o;
+		newNode.next = null;
+		if ( head == null ) {
+			head = newNode;
+		} else{
+			findBeforeInsertNode(this.size).next = newNode;
+		}
 	}
 	public Object removeFirst(){
-		return null;
+		Object data;
+		Node newHead = new Node();
+		newHead = this.head;
+		this.head = newHead.next;
+
+		data = newHead.data;
+		newHead = null;
+		return  data;
 	}
 	public Object removeLast(){
-		return null;
+		Object data;
+		Node last = findIndexNode(this.size() -1);
+		data = last.data;
+		last = null;
+
+		return data;
 	}
-	public Iterator iterator(){
-		return null;
+	public Iterator iterator()
+	{
+		return new LinkedListIterator();
 	}
-	
+
 	
 	private static  class Node{
 		Object data;
@@ -49,7 +159,17 @@ public class LinkedList implements List {
 	 * 例如链表为 3->7->10 , 逆置后变为  10->7->3
 	 */
 	public  void reverse(){		
-		
+		Node first ;
+		int currentIndex = 1;
+		this.head = findIndexNode(this.size - 1);
+		if(this.size() <= 1){
+			return;
+		}
+		while(currentIndex < this.size() ){
+			first = findIndexNode(this.size() - currentIndex);
+			first.next = findIndexNode(this.size - currentIndex - 1);
+			currentIndex++;
+		}
 	}
 	
 	/**
@@ -59,16 +179,35 @@ public class LinkedList implements List {
 
 	 */
 	public  void removeFirstHalf(){
-		
+		checkElementIndex(this.size());
+
+		int mid = this.size / 2;
+		Node temp = this.head;
+		this.head = findIndexNode(mid);
+		for (int i = 0; i < mid; i++) {
+			Node T = temp.next;
+			temp = null;
+			temp = T;
+		}
+
 	}
 	
 	/**
 	 * 从第i个元素开始， 删除length 个元素 ， 注意i从0开始
-	 * @param i
+	 * @param index
 	 * @param length
 	 */
-	public  void remove(int i, int length){
-		
+	public  void remove(int index, int length){
+		checkElementIndex(length + index );
+
+		Node before = findBeforeInsertNode(index);
+		Node temp = findIndexNode(index);
+		for (int i= 0; i < length; i++) {
+			Node T = temp.next;
+			temp = null;
+			temp = T;
+		}
+		before.next = temp;
 	}
 	/**
 	 * 假定当前链表和listB均包含已升序排列的整数
@@ -79,7 +218,17 @@ public class LinkedList implements List {
 	 * @param list
 	 */
 	public  int[] getElements(LinkedList list){
-		return null;
+		Node listNode = list.head;
+		Node myNode = this.head;
+		int[] data = new int[list.size()];
+		for (int i = 0; i < this.size(); i++) {
+			if(i == (Integer) listNode.data){
+				data[i] = (Integer)myNode.data;
+			}
+			myNode = myNode.next;
+		}
+
+		return data;
 	}
 	
 	/**
@@ -90,12 +239,22 @@ public class LinkedList implements List {
 	 */
 	
 	public  void subtract(LinkedList list){
-		
+
+		int[] data = new int[list.size()];
+		for (int i = 0,j = 0; i < this.size(); i++) {
+			for (; j < list.size(); j++) {
+				if(this.get(i).equals(list.get(j))){
+					this.remove(i);
+					break;
+				}
+			}
+		}
 	}
 	
 	/**
 	 * 已知当前链表中的元素以值递增有序排列，并以单链表作存储结构。
 	 * 删除表中所有值相同的多余元素（使得操作后的线性表中所有元素的值均不相同）
+	 * （选做）
 	 */
 	public  void removeDuplicateValues(){
 		
@@ -106,6 +265,7 @@ public class LinkedList implements List {
 	 * 试写一高效的算法，删除表中所有值大于min且小于max的元素（若表中存在这样的元素）
 	 * @param min
 	 * @param max
+	 * （选做）
 	 */
 	public  void removeRange(int min, int max){
 		
@@ -117,6 +277,51 @@ public class LinkedList implements List {
 	 * @param list
 	 */
 	public  LinkedList intersection( LinkedList list){
-		return null;
+        LinkedList listC = new LinkedList();
+
+        for (int i = 0,j = 0; i < this.size(); i++) {
+            for (; j < list.size(); j++) {
+                if(this.get(i).equals(list.get(j))){
+                    listC.add(this.get(i));
+                    break;
+                }
+            }
+        }
+
+
+
+		return listC;
+	}
+
+
+
+	/************Iterator**************/
+	private class LinkedListIterator implements Iterator{
+		private Node currentNode;
+		private int currentIndex;
+
+		public LinkedListIterator() {
+			// TODO Auto-generated constructor stub
+			this.currentIndex = 0;
+			this.currentNode = new Node();
+		}
+		@Override
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return currentIndex < size();
+		}
+
+		@Override
+		public Object next() {
+			// TODO Auto-generated method stub
+			checkElementIndex(currentIndex);
+			if ( currentNode.data == null ) {
+				this.currentNode = findIndexNode(currentIndex);
+			}
+			Object value = currentNode.data;
+			currentNode = currentNode.next;
+			currentIndex++;
+			return value;
+		}
 	}
 }
