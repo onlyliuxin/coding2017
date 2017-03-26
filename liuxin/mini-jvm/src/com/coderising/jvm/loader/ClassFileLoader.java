da6bf2d2cfa61303e21395385e903bb7791ae485
onlyliuxin <14703250@qq.com>
2017-03-26 17:48:12 +0800

    mini-jvm第一版

---
+++ liuxin/mini-jvm/src/com/coderising/jvm/loader/ClassFileLoader.java
@@ -0,0 +1,51 @@
+package com.coderising.jvm.loader;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+
+
+
+public class ClassFileLoader {
+
+	private List<String> clzPaths = new ArrayList<String>();
+	
+	public byte[] readBinaryCode(String className) {
+		
+		return null;	
+		
+		
+	}
+	
+	private byte[] loadClassFile(String clzFileName) {
+		
+		return null;
+	}
+	
+	
+	
+	public void addClassPath(String path) {
+		
+	}
+	
+	public String getClassPath_V1(){
+		
+		return  null;
+	}
+	
+	public String getClassPath(){
+		return null;
+	}
+
+	
+
+	
+
+}
