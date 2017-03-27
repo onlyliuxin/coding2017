fc1362a063dd500b767bb6488d2ec7188a8f969e
zhanglei <383117348@qq.com>
2017-03-27 14:02:31 +0800

    383117348修改字节码文件读取路径

--- group27/383117348/src/com/coderising/jvm/loader/ClassFileLoader.java
+++ group27/383117348/src/com/coderising/jvm/loader/ClassFileLoader.java
@@ -12,37 +12,49 @@ public class ClassFileLoader {
 
 	private List<String> clzPaths = new ArrayList<String>();
 
-	public byte[] readBinaryCode(String className){
+	public byte[] readBinaryCode(String className) {
 		className = className.replace(".", "\\");
-		File file = new File(getClassPath()+"\\"+className+".class");
-		
-		ByteArrayOutputStream bos = new ByteArrayOutputStream((int)file.length());  
-        BufferedInputStream in = null;  
-        try{  
-            in = new BufferedInputStream(new FileInputStream(file));  
-            int buf_size = 1024;  
-            byte[] buffer = new byte[buf_size];  
-            int len = 0;  
-            while(-1 != (len = in.read(buffer,0,buf_size))){  
-                bos.write(buffer,0,len);  
-            }  
-            return bos.toByteArray();  
-        }catch (IOException e) {  
-            e.printStackTrace(); 
-        }finally{  
-            try{  
-                in.close(); 
-            }catch (IOException e) {  
-                e.printStackTrace();  
-            }   
-            try {
+		File file = null;
+		for(String classPath : clzPaths){
+			file = new File(classPath + "\\" + className + ".class");
+			if(file.exists()){
+				break;
+			}
+		}
+		if(!file.exists()){
+			try {
+				throw new ClassNotFoundException();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		ByteArrayOutputStream bos = new ByteArrayOutputStream((int) file.length());
+		BufferedInputStream in = null;
+		try {
+			in = new BufferedInputStream(new FileInputStream(file));
+			int buf_size = 1024;
+			byte[] buffer = new byte[buf_size];
+			int len = 0;
+			while (-1 != (len = in.read(buffer, 0, buf_size))) {
+				bos.write(buffer, 0, len);
+			}
+			return bos.toByteArray();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
 				bos.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-        }  
-        return null;
+		}
+		return null;
 	}
 
 	public void addClassPath(String path) {
@@ -55,10 +67,10 @@ public class ClassFileLoader {
 
 	public String getClassPath() {
 		String paths = "";
-		for(String s:clzPaths){
-			paths+=s+";";
+		for (String s : clzPaths) {
+			paths += s + ";";
 		}
-		paths = paths.substring(0, paths.length()-1);
+		paths = paths.substring(0, paths.length() - 1);
 		return paths;
 	}
 
