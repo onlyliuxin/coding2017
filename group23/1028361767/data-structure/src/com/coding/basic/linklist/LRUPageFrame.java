9960ec50fb4d28a61f76d65a43b0fbb556d914b5
anxinJ <1028361767@qq.com>
2017-03-28 12:36:17 +0800

    LRU
    LRU

--- group23/1028361767/data-structure/src/com/coding/basic/linklist/LRUPageFrame.java
+++ group23/1028361767/data-structure/src/com/coding/basic/linklist/LRUPageFrame.java
@@ -13,7 +13,8 @@ public class LRUPageFrame {
 		Node next;
 		int pageNum;
 
-		Node() {
+		Node(int pageNum) {
+			this.pageNum = pageNum;
 		}
 	}
 
@@ -22,6 +23,8 @@ public class LRUPageFrame {
 	
 	private Node first;// 链表头
 	private Node last;// 链表尾
+	
+	private int size;
 
 	
 	public LRUPageFrame(int capacity) {
@@ -37,19 +40,68 @@ public class LRUPageFrame {
 	 * @return
 	 */
 	public void access(int pageNum) {
-		
-	
+		/**
+		 * 1、链表为空，直接插入first
+		 * 2、链表未满，往链尾追加
+		 * 3、链表已满，判断链表中是否存在pageNum，若存在，把pageNum，移到链尾；若不存在，把链头删除，把pageNum追加到链尾
+		 */
+		Node newNode = new Node(pageNum);
+		if(size < capacity){
+			if(size == 0){
+				first = newNode;
+			}else{
+				last.next = newNode;
+				newNode.prev = last;
+			}
+			last = newNode;
+			size++;
+			return ;
+		}
+		Node existNode = null;
+		Node tmp = first;
+		do{
+			if(tmp.pageNum == pageNum){
+				existNode = tmp;
+			}
+			tmp = tmp.next;
+		}while(tmp != null);
+		if(existNode == null || existNode == first){
+			tmp = first.next;
+			tmp.prev = null;
+			first = tmp;//链首变为链首后一个节点
+			last.next = newNode;
+			newNode.prev = last;
+			last = newNode;//链尾追加新节点
+		}else if(existNode == last){
+				return ;
+		}else{
+			/**
+			 * 已存在节点的上一个节点的下一个节点指向已存在节点的下一个节点
+			 * 
+			 */
+			Node prev = existNode.prev;
+			Node next = existNode.next;
+			prev.next = next;
+			next.prev = prev;
+			if(prev.prev == null){
+				first = prev;
+			}
+			newNode.prev = last;
+			last.next = newNode;
+			last = newNode;
+			existNode = null;
+		}
 	}
 	
 	
 
 	public String toString(){
 		StringBuilder buffer = new StringBuilder();
-		Node node = first;
+		Node node = last;
 		while(node != null){
 			buffer.append(node.pageNum);			
 			
-			node = node.next;
+			node = node.prev;
 			if(node != null){
 				buffer.append(",");
 			}
--- group23/1028361767/data-structure/src/com/coding/basic/linklist/LRUPageFrameTest.java
+++ group23/1028361767/data-structure/src/com/coding/basic/linklist/LRUPageFrameTest.java
@@ -27,5 +27,31 @@ public class LRUPageFrameTest {
 		frame.access(4);
 		Assert.assertEquals("4,0,3", frame.toString());
 	}
-
+	
+	@Test
+	public void testAccessCustom() {
+		LRUPageFrame frame = new LRUPageFrame(5);
+		frame.access(7);
+		frame.access(0);
+		frame.access(1);
+		frame.access(5);
+		frame.access(3);
+		Assert.assertEquals("3,5,1,0,7", frame.toString());
+		frame.access(2);
+		Assert.assertEquals("2,3,5,1,0", frame.toString());
+		frame.access(0);
+		Assert.assertEquals("0,2,3,5,1", frame.toString());
+		frame.access(0);
+		Assert.assertEquals("0,2,3,5,1", frame.toString());
+		frame.access(3);
+		Assert.assertEquals("3,0,2,5,1", frame.toString());
+		frame.access(0);
+		Assert.assertEquals("0,3,2,5,1", frame.toString());
+		frame.access(4);
+		Assert.assertEquals("4,0,3,2,5", frame.toString());
+		frame.access(5);
+		Assert.assertEquals("5,4,0,3,2", frame.toString());
+		frame.access(2);
+		Assert.assertEquals("2,5,4,0,3", frame.toString());
+	}
 }
