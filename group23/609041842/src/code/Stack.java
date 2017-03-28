189f74d2acd199bfa7546ca8ba24d8b8735f6d21
chk <changhongkaimail@163.com>
2017-03-29 00:20:04 +0800

    33

---
+++ group23/609041842/src/code/Stack.java
@@ -0,0 +1,30 @@
+package code;
+
+public class Stack {
+
+	private ArrayList array = new ArrayList();
+	
+	public void push(Object o){
+		array.add(o);
+	}
+	public Object pop(){
+		return array.remove(array.size()-1);
+	}
+	
+	public boolean isEmpty(){
+		if(array.size()<=0)
+			return true;
+		return false;
+	}
+	
+	public int size(){
+		return array.size();
+	}
+	public static void main(String[] args) {
+		Stack sc = new Stack();
+		sc.push("hello world");
+		sc.push("java");
+		sc.push("jvm");
+	}
+	
+}
