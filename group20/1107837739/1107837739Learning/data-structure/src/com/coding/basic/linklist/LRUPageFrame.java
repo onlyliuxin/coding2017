defa485b9eeaf29a9af5318a42f997478a6c6e3d
Korben_CHY <korben.chy@gmail.com>
2017-04-01 17:40:44 +0800

    submit LRUPageFrame by Korben

--- group20/1107837739/1107837739Learning/data-structure/src/com/coding/basic/linklist/LRUPageFrame.java
+++ group20/1107837739/1107837739Learning/data-structure/src/com/coding/basic/linklist/LRUPageFrame.java
@@ -3,15 +3,16 @@ package com.coding.basic.linklist;
 /**
  * 用双向链表实现LRU算法
  *
- * @author liuxin
+ * @author Korben
  */
 public class LRUPageFrame {
 
     private int capacity;
-    private Node first;// 链表头
-    private Node last;// 链表尾
-    public LRUPageFrame(int capacity) {
+    private int size;
+    private Node first; // 链表头
+    private Node last;  // 链表尾
 
+    public LRUPageFrame(int capacity) {
         this.capacity = capacity;
     }
 
@@ -19,7 +20,61 @@ public class LRUPageFrame {
      * 获取缓存中对象
      */
     public void access(int pageNum) {
+        if (this.first == null) {
+            add2First(pageNum);
+            return;
+        }
 
+        if (this.first.pageNum == pageNum) {
+            return;
+        }
+
+        if (reorderCache(pageNum)) {
+            return;
+        }
+
+        add2First(pageNum);
+
+        if (this.size > this.capacity) {
+            removeNode(this.last);
+        }
+    }
+
+    private boolean reorderCache(int pageNum) {
+        Node node = this.first;
+        for (int i = 0; i < this.size - 1; i++) {
+            node = node.next;
+            if (node.pageNum == pageNum) {
+                removeNode(node);
+                add2First(node.pageNum);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void removeNode(Node node) {
+        node.prev.next = node.next;
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        } else {
+            this.last = node.prev;
+        }
+        this.size--;
+    }
+
+    private void add2First(int pageNum) {
+        Node oldFirst = this.first;
+        this.first = new Node(pageNum);
+        this.first.next = oldFirst;
+        if (oldFirst == null) {
+            this.last = this.first;
+        } else {
+            oldFirst.prev = this.first;
+        }
+
+        this.size++;
     }
 
     public String toString() {
@@ -42,7 +97,8 @@ public class LRUPageFrame {
         Node next;
         int pageNum;
 
-        Node() {
+        Node(int pageNum) {
+            this.pageNum = pageNum;
         }
     }
 }
