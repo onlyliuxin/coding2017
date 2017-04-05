6bc5b7dfcdf58434ed1cdf476d21f3557b56238a
Harry <chenhaowei93@163.com>
2017-04-05 15:25:12 +0800

    统一代码格式

--- group02/727171008/src/com/github/HarryHook/coding2017/linklist/LRUPageFrame.java
+++ group02/727171008/src/com/github/HarryHook/coding2017/linklist/LRUPageFrame.java
@@ -2,35 +2,35 @@ package com.github.HarryHook.coding2017.linklist;
 
 /**
  * 用双向链表实现LRU算法
+ * 
  * @author HarryHook
  *
  */
 public class LRUPageFrame {
-	
+
     private static class Node {
-		
+
 	Node prev;
 	Node next;
 	int pageNum;
 
 	Node() {
-	    
+
 	}
     }
 
     private int capacity;
     private int currentSize = 0;
-    
+
     private Node first;// 链表头
     private Node last;// 链表尾
 
-	
     public LRUPageFrame(int capacity) {
-		
+
 	this.capacity = capacity;
-		
+
     }
-   
+
     /**
      * 获取缓存中对象
      * 
@@ -38,46 +38,46 @@ public class LRUPageFrame {
      * @return
      */
 
-    
     public void access(int pageNum) {
-	
-	if(currentSize == 0) {    
+
+	if (currentSize == 0) {
 	    Node node = new Node();
 	    node.pageNum = pageNum;
 	    node.prev = node.next = null;
-	    first = last = node;  
+	    first = last = node;
 	    currentSize++;
-	}else {
-	    
-	    if(first.pageNum == pageNum) { 
-		return ;
+	} else {
+
+	    if (first.pageNum == pageNum) {
+		return;
 	    }
-	    
-	    if(currentSize == 1) { 
+
+	    if (currentSize == 1) {
 		addToFirst(pageNum);
-	    }else {
-		
-		if(last.pageNum == pageNum) {
-		    moveLastToFirst(); 
-		    return ;
+	    } else {
+
+		if (last.pageNum == pageNum) {
+		    moveLastToFirst();
+		    return;
 		}
 		Node iteratorNode = first;
-		while(iteratorNode.next != null && iteratorNode.pageNum != pageNum) {
+		while (iteratorNode.next != null && iteratorNode.pageNum != pageNum) {
 		    iteratorNode = iteratorNode.next;
 		}
-		if(iteratorNode == last) { 
+		if (iteratorNode == last) {
 		    addToFirst(pageNum);
-		    //若缓存已满，移除最后一个节点
-		    if(currentSize > capacity) { 
+		    // 若缓存已满，移除最后一个节点
+		    if (currentSize > capacity) {
 			last = last.prev;
 			last.next = null;
 		    }
-		}else {	
-		    moveToFirst(iteratorNode);		
+		} else {
+		    moveToFirst(iteratorNode);
 		}
 	    }
-	 }	   
+	}
     }
+
     // 将节点/缓存页添加到fitrst
     public void addToFirst(int pageNum) {
 	Node node = new Node();
@@ -88,7 +88,8 @@ public class LRUPageFrame {
 	first = node;
 	currentSize++;
     }
-    //将last节点移动到first
+
+    // 将last节点移动到first
     public void moveLastToFirst() {
 	last.next = first;
 	first.prev = last;
@@ -97,7 +98,8 @@ public class LRUPageFrame {
 	last.next = null;
 	first.prev = null;
     }
-    //将最近使用的已有缓存页移动到first
+
+    // 将最近使用的已有缓存页移动到first
     public void moveToFirst(Node iteratorNode) {
 	iteratorNode.prev.next = iteratorNode.next;
 	iteratorNode.next.prev = iteratorNode.prev;
@@ -106,19 +108,18 @@ public class LRUPageFrame {
 	first.prev = iteratorNode;
 	first = iteratorNode;
     }
-	
+
     public String toString() {
 	StringBuilder buffer = new StringBuilder();
 	Node node = first;
-	while(node != null) {
-	    buffer.append(node.pageNum);				
-	    node = node.next;	
-	    if(node != null) {
+	while (node != null) {
+	    buffer.append(node.pageNum);
+	    node = node.next;
+	    if (node != null) {
 		buffer.append(",");
 	    }
 	}
 	return buffer.toString();
     }
-  
-	
+
 }
--- group02/727171008/src/com/github/HarryHook/coding2017/litestruts/View.java
+++ group02/727171008/src/com/github/HarryHook/coding2017/litestruts/View.java
@@ -2,30 +2,31 @@ package com.github.HarryHook.coding2017.litestruts;
 
 import java.util.Map;
 
-public class View 
-{
-    	private String jsp;
-	private Map parameters;
-	
-	//对应action获取Jsp
-	public String getJsp() {
-	    
-		return jsp;
-	}
-	public View setJsp(String jsp) {
-	    
-		this.jsp = jsp;
-		return this;
-	}
-	
-	//execute()获取对应参数
-	public Map getParameters() {
-	    
-		return parameters;
-	}
-	public View setParameters(Map parameters) {
-	    
-		this.parameters = parameters;
-		return this;
-	}
+public class View {
+    private String jsp;
+    private Map parameters;
+
+    // 对应action获取Jsp
+    public String getJsp() {
+
+	return jsp;
+    }
+
+    public View setJsp(String jsp) {
+
+	this.jsp = jsp;
+	return this;
+    }
+
+    // execute()获取对应参数
+    public Map getParameters() {
+
+	return parameters;
+    }
+
+    public View setParameters(Map parameters) {
+
+	this.parameters = parameters;
+	return this;
+    }
 }
