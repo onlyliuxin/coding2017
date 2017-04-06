5ca816918c0e98cb5c5c2500e21bf50303d1060a
zheng <765324639@qq.com>
2017-04-06 18:27:25 +0800

    调整代码结构，转为maven项目

---
+++ group01/765324639/src/main/java/datastructure/stack/Stack.java
@@ -0,0 +1,35 @@
+package datastructure.stack;
+
+import java.util.EmptyStackException;
+
+import datastructure.array.ArrayList;
+
+public class Stack {
+    private ArrayList elementData = new ArrayList();
+
+    public void push(Object o) {
+        elementData.add(o);
+    }
+
+    public Object pop() {
+        if (elementData.size() == 0) {
+            throw new EmptyStackException();
+        }
+        return elementData.remove(elementData.size() - 1);
+    }
+
+    public Object peek() {
+        if (elementData.size() == 0) {
+            throw new EmptyStackException();
+        }
+        return elementData.get(elementData.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return elementData.size() == 0;
+    }
+
+    public int size() {
+        return elementData.size();
+    }
+}
---
+++ group01/765324639/src/main/java/minijvm/loader/MyTest.java
@@ -0,0 +1,10 @@
+package minijvm.loader;
+
+import java.io.File;
+
+public class MyTest {
+
+    public static void main(String[] args) {
+        System.out.println(new File(".").getAbsolutePath());
+    }
+}
