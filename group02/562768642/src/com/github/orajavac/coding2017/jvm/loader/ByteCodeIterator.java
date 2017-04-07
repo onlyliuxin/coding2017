7f570d952b0f76582457e8f4bc908985daffe271
orajavac <oraclegit@126.com>
2017-04-07 16:58:42 +0800

    20170407_1658 jvm常量

--- group02/562768642/src/com/github/orajavac/coding2017/jvm/loader/ByteCodeIterator.java
+++ group02/562768642/src/com/github/orajavac/coding2017/jvm/loader/ByteCodeIterator.java
@@ -1,5 +1,30 @@
 package com.github.orajavac.coding2017.jvm.loader;
 
-public class ByteCodeIterator {
+import com.github.orajavac.coding2017.jvm.util.Util;
 
+public class ByteCodeIterator {
+	
+	private byte[] codes;
+	
+	private int pos;
+	
+	public ByteCodeIterator(byte[] codes){
+		this.codes = codes;
+	}
+	
+	public String nextU4ToHexString(){
+		return Util.byteToHexString(new byte[]{codes[pos++],codes[pos++],codes[pos++],codes[pos++]});
+	}
+
+	public int nextU1ToInt(){
+		return Util.byteToInt(new byte[]{codes[pos++]});
+	}
+	
+	public int nextU2ToInt(){
+		return Util.byteToInt(new byte[]{codes[pos++],codes[pos++]});
+	}
+	
+	public int nextU4ToInt(){
+		return Util.byteToInt(new byte[]{codes[pos++],codes[pos++],codes[pos++],codes[pos++]});
+	}
 }
--- group02/562768642/src/com/github/orajavac/coding2017/jvm/loader/ClassFileParser.java
+++ group02/562768642/src/com/github/orajavac/coding2017/jvm/loader/ClassFileParser.java
@@ -3,16 +3,27 @@ package com.github.orajavac.coding2017.jvm.loader;
 import com.github.orajavac.coding2017.jvm.clz.AccessFlag;
 import com.github.orajavac.coding2017.jvm.clz.ClassFile;
 import com.github.orajavac.coding2017.jvm.clz.ClassIndex;
+import com.github.orajavac.coding2017.jvm.constant.ClassInfo;
 import com.github.orajavac.coding2017.jvm.constant.ConstantPool;
+import com.github.orajavac.coding2017.jvm.constant.FieldRefInfo;
+import com.github.orajavac.coding2017.jvm.constant.MethodRefInfo;
+import com.github.orajavac.coding2017.jvm.constant.NameAndTypeInfo;
+import com.github.orajavac.coding2017.jvm.constant.NullConstantInfo;
+import com.github.orajavac.coding2017.jvm.constant.StringInfo;
+import com.github.orajavac.coding2017.jvm.constant.UTF8Info;
 
 public class ClassFileParser {
 	
 	public ClassFile parse(byte[] codes) {
-
+		ClassFile clzFile = new ClassFile();
+		ByteCodeIterator iter = new ByteCodeIterator(codes);
+		String magicNumber = iter.nextU4ToHexString();
+		System.out.println("magicNumber="+magicNumber);
+		clzFile.setMinorVersion(iter.nextU2ToInt());
+		clzFile.setMajorVersion(iter.nextU2ToInt());
 		
-		
-
-		return null;
+		clzFile.setConstPool(parseConstantPool(iter));
+		return clzFile;
 	}
 
 	private AccessFlag parseAccessFlag(ByteCodeIterator iter) {
@@ -27,7 +38,53 @@ public class ClassFileParser {
 	}
 
 	private ConstantPool parseConstantPool(ByteCodeIterator iter) {
-
-		return null;
+		int constPoolCount = iter.nextU2ToInt();
+		System.out.println("Constant Pool Count :"+constPoolCount);
+		ConstantPool pool = new ConstantPool();
+		pool.addConstantInfo(new NullConstantInfo());
+		for (int i=1;i<constPoolCount-1;i++){
+			int tag = iter.nextU1ToInt();
+			if (tag == 7){	//class info
+				int uft8Index = iter.nextU1ToInt();
+				ClassInfo clzInfo = new ClassInfo(pool);
+				clzInfo.setUtf8Index(uft8Index);
+				pool.addConstantInfo(clzInfo);
+			}else if (tag == 1){	//utf-8 string
+				int len = iter.nextU2ToInt();
+				byte[] data = iter.getBytes(len);
+				String value = null;
+				try{
+					value = new String(data,"UTF-8");
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+				UTF8Info utf8info = new UTF8Info(pool);
+				utf8info.setLength(len);
+				utf8info.setValue(value);
+				pool.addConstantInfo(utf8info);
+			}else if (tag == 8){
+				StringInfo info = new StringInfo(pool);
+				info.setIndex(iter.nextU2ToInt());
+				pool.addConstantInfo(info);
+			}else if (tag == 9){
+				FieldRefInfo field = new FieldRefInfo(pool);
+				field.setClassInfoIndex(iter.nextU2ToInt());
+				field.setNameAndTypeIndex(iter.nextU2ToInt());
+				pool.addConstantInfo(field);
+			}else if (tag == 10){
+				MethodRefInfo method = new MethodRefInfo(pool);
+				method.setClassInfoIndex(iter.nextU2ToInt());
+				method.setNameAndTypeIndex(iter.nextU2ToInt());
+				pool.addConstantInfo(method);
+			}else if (tag == 12){
+				NameAndTypeInfo nameType = new NameAndTypeInfo(pool);
+				nameType.setIndex1(iter.nextU2ToInt());
+				nameType.setIndex2(iter.nextU2ToInt());
+				pool.addConstantInfo(nameType);
+			}else{
+				throw new RuntimeException("the constant pool tag "+tag+" has");
+			}
+		}
+		return pool;
 	}
 }
