7d692349b71aaa579e681088a9ebb5e58dd28acb
龚启盼 <gongqipan@qipeipu.com>
2017-04-10 16:55:13 +0800

    init mini-jvm

---
+++ group11/252308879/mini-jvm/src/main/java/com/pan/jvm/loader/ClassFileParser.java
@@ -0,0 +1,33 @@
+package com.pan.jvm.loader;
+
+import com.pan.jvm.clz.AccessFlag;
+import com.pan.jvm.clz.ClassFile;
+import com.pan.jvm.clz.ClassIndex;
+import com.pan.jvm.constant.ConstantPool;
+
+public class ClassFileParser {
+
+    public ClassFile parse(byte[] codes) {
+
+
+        return null;
+    }
+
+    private AccessFlag parseAccessFlag(ByteCodeIterator iter) {
+
+        return null;
+    }
+
+    private ClassIndex parseClassInfex(ByteCodeIterator iter) {
+
+        return null;
+
+    }
+
+    private ConstantPool parseConstantPool(ByteCodeIterator iter) {
+
+        return null;
+    }
+
+
+}
