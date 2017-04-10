7adfa4e86575800dcaa83b5fb2239ed14e7ffbc5
Ren650119726 <102228177@qq.com>
2017-04-10 15:18:38 +0800

    第五次作业

--- group17/102228177/work4_09/src/com/coding/basic/stack/Stack.java
+++ group17/102228177/work4_09/src/com/coding/basic/stack/Stack.java
@@ -1,22 +1,48 @@
 package com.coding.basic.stack;
 
+import java.util.EmptyStackException;
+
 public class Stack {
-	private ArrayList elementData = new ArrayList();
+	private ArrayList elementData;
+	private int elementCount;
 	
-	public void push(Object o){		
+	public Stack() {
+		this.elementData = new ArrayList();
+		this.elementCount = 0;
+	}
+	public void push(Object o){
+		elementData.add(o);
+		elementCount++;
 	}
 	
 	public Object pop(){
-		return null;
+		Object object = elementData.remove(elementCount-1);
+		elementCount--;
+		return object;
 	}
 	
 	public Object peek(){
-		return null;
+		if(isEmpty()){
+			 throw new EmptyStackException();
+		}
+		return elementData.get(elementCount-1);
 	}
 	public boolean isEmpty(){
-		return false;
+		return elementCount==0;
 	}
 	public int size(){
-		return -1;
+		return elementCount;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		for (int i = 0; i < elementData.size(); i++) {
+			sb.append(elementData.get(i)).append(",");
+		}
+		sb = sb.deleteCharAt(sb.length()-1);
+		sb.append("]");
+		return sb.toString();
 	}
 }
--- group17/102228177/work4_09/src/com/coding/basic/stack/StackUtil.java
+++ group17/102228177/work4_09/src/com/coding/basic/stack/StackUtil.java
@@ -8,8 +8,22 @@ public class StackUtil {
 	 * 注意：只能使用Stack的基本操作，即push,pop,peek,isEmpty， 可以使用另外一个栈来辅助
 	 */
 	public static void reverse(Stack s) {
-
+		if(s.isEmpty()){
+			return ;
+		}
+		int temp1 = (int) s.pop();
+		reverse(s);
+		if(s.isEmpty()){
+			s.push(temp1);
+			return ;
+		}
+		int temp2 = (int) s.pop();
+		reverse(s);
+		s.push(temp1);
+		reverse(s);
+		s.push(temp2);
 	}
+	
 
 	/**
 	 * 删除栈中的某个元素 注意：只能使用Stack的基本操作，即push,pop,peek,isEmpty， 可以使用另外一个栈来辅助
@@ -17,7 +31,19 @@ public class StackUtil {
 	 * @param o
 	 */
 	public static void remove(Stack s,Object o) {
-
+		if(s.isEmpty()){
+			return ;
+		}
+		Stack stack = new Stack();
+		while(!s.isEmpty()){
+			Object pop = s.pop();
+			if(!pop.equals(o)){
+				stack.push(pop);
+			}
+		}
+		while(!stack.isEmpty()){
+			s.push(stack.pop());
+		}
 	}
 
 	/**
@@ -27,7 +53,19 @@ public class StackUtil {
 	 * @return
 	 */
 	public static Object[] getTop(Stack s,int len) {
-		return null;
+		int i = 0;
+		Stack stack = new Stack();
+		Object[] objects = new Object[len];
+		while(i < len){
+			Object o = s.pop();
+			stack.push(o);
+			objects[i] = o;
+			i++;
+		}
+		while(!stack.isEmpty()){
+			s.push(stack.pop());
+		}
+		return objects;
 	}
 	/**
 	 * 字符串s 可能包含这些字符：  ( ) [ ] { }, a,b,c... x,yz
@@ -38,6 +76,35 @@ public class StackUtil {
 	 * @return
 	 */
 	public static boolean isValidPairs(String s){
+		Stack stack = new Stack();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			switch (c) {
+			case '(':
+			case '[':
+			case '{':
+				stack.push(c);
+				break;
+			case ')':
+				 if((char)stack.pop()!='('){
+			        return false;
+			      }
+			      break;
+			case ']':
+				 if((char)stack.pop()!='['){
+			        return false;
+			      }
+			      break;
+			case '}':
+				 if((char)stack.pop()!='{'){
+			        return false;
+			      }
+			      break;
+			}
+		}
+		if(stack.isEmpty()){
+			return true;
+		}
 		return false;
 	}
 	
