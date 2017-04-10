9d07aea4cc36cc64028473cd7281a4479ce7ac1b
CoderXLoong <413007522@qq.com>
2017-04-10 10:23:53 +0800

    Signed-off-by: CoderXLoong <413007522@qq.com>

---
+++ group13/413007522/lesson05/src/main/java/cn/xl/jvm/constant/NullConstantInfo.java
@@ -0,0 +1,13 @@
+package cn.xl.jvm.constant;
+
+public class NullConstantInfo extends ConstantInfo {
+
+	public NullConstantInfo(){
+		
+	}
+	@Override
+	public int getType() {		
+		return -1;
+	}
+	
+}
