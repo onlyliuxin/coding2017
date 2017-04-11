f92d3e31ec87abf06dc991cbb0fd29abe58a2b53
xmt <542194147@qq.com>
2017-04-11 16:59:40 +0800

    jvm读取模数作业V2
    优化完全类名的拼接方法（好吧，就是借鉴老刘的方法）

--- group11/542194147/myDataStructure/src/com/coding/basic/LRUPageFrame.java
+++ group11/542194147/myDataStructure/src/com/coding/basic/LRUPageFrame.java
@@ -2,7 +2,7 @@ package com.coding.basic;
 
 /**
  * 用双向链表实现LRU算法
- * @author liuxin
+ * @author 小摩托
  *
  */
 public class LRUPageFrame {
@@ -18,14 +18,14 @@ public class LRUPageFrame {
 	}
 
 	private int capacity;
-	private int current;
+	private int currentSize;
 	
 	private Node first;// 链表头
 	private Node last;// 链表尾
 
 	
 	public LRUPageFrame(int capacity) {
-		
+		this.currentSize = 0;
 		this.capacity = capacity;
 		
 	}
@@ -37,25 +37,71 @@ public class LRUPageFrame {
 	 * @return
 	 */
 	public void access(int pageNum) {
-		if(current<capacity){
-			current+=1;
-			if(current==0){
-				Node node=new Node();
-				node.pageNum=pageNum;
-				node.prev=null;
-				node.next=null;
-				first=node;
-				last=node;
-			}else{
-				Node node=new Node();
-				node.pageNum=pageNum;
+		//检查数据是否存在队列中
+		Node node=findNode(pageNum);
+		if(node!=null){//如果存在,则提到队列头（LRU越活跃的数据越放在前面）
+			moveExistingNodeToHead(node);		
+		}else{
+			Node newNode=new Node();
+			newNode.pageNum=pageNum;
+			if(currentSize>=capacity){//缓存已满删除最后的数据
+				removeLastNode();
 			}
+			addNewNodeTOHead(newNode);//把新数据加到缓存头部
 		}
-		
-	
 	}
 	
+	private void addNewNodeTOHead(Node node) {
+		if(first==null&&last==null){
+			first=node;
+			last=node;
+			node.next=null;
+			node.prev=null;
+		}else{
+			first.prev=node;
+			node.prev=null;
+			node.next=first;
+			first=node;
+		}
+		currentSize++;
+	}
+
+	private void removeLastNode() {
+		Node node=last.prev;
+		node.next=null;
+		last.prev=null;
+		last=node;
+		currentSize--;
+	}
+
+	private void moveExistingNodeToHead(Node node) {
+		if(node==first){
+			return;
+		}else if(node==last){
+			Node prevNode=node.prev;
+			last=prevNode;
+			prevNode.next=null;
+		}else{
+			node.prev.next=node.next;
+			node.next.prev=node.prev;
+		}
+		node.prev=null;
+		node.next=first;
+		first.prev=node;
+		first=node;
+	}
+
 	
+	private Node findNode(int data) {
+		Node node=first;
+		while(node!=null){
+			if(node.pageNum==data){
+				return node;
+			}
+			node=node.next;
+		}
+		return null;
+	}
 
 	public String toString(){
 		StringBuilder buffer = new StringBuilder();
