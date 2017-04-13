46791de1544786ace8f24c5698c6b662056c58d9
gongxun <gongxun@wesai.com>
2017-04-13 22:14:51 +0800

    添加第二个作业模板

---
+++ group17/785396327/4.9/expr/InfixExpr.java
@@ -0,0 +1,18 @@
+package expr;
+
+/**
+ * Created by IBM on 2017/4/13.
+ */
+public class InfixExpr {
+    String expr = null;
+
+    public InfixExpr(String expr) {
+        this.expr = expr;
+    }
+
+    public float evaluate() {
+
+        return 0.0f;
+    }
+
+}
