621674206183f1f5f5c02b522b745dd4192e93ee
kilien <Linerator@163.com>
2017-04-16 18:30:36 +0800

    week06

--- group09/790466157/mini-jvm/src/com/coderising/jvm/loader/ClassFileLoader.java
+++ group09/790466157/mini-jvm/src/com/coderising/jvm/loader/ClassFileLoader.java
@@ -4,9 +4,16 @@ import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.net.URL;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import com.coderising.jvm.clz.ClassFile;
+
+
 
 
 
@@ -14,38 +21,120 @@ public class ClassFileLoader {
 
 	private List<String> clzPaths = new ArrayList<String>();
 	
-	public byte[] readBinaryCode(String className) throws Exception {
-		URL base = this.getClass().getResource("/");
-		String baseToString = ""+base;
-		String filePath = baseToString.replaceAll("file:/", "")+className.replace(".", "\\")+".class";
-		//String filePath = clzPaths.get(0)+"\\"+className.replace(".", "\\")+".class";  //符合Junit测试调用addClassPath方法
-		File file = new File(filePath);
+	public byte[] readBinaryCode(String className) {
 		
-		BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		className = className.replace('.', File.separatorChar) +".class";
 		
-		byte[] buffer = new byte[1024];
-		int len = 0;
-		while((len = bis.read(buffer)) != -1){
-			baos.write(buffer,0,len);
+		for(String path : this.clzPaths){
+			
+			String clzFileName = path + File.separatorChar + className;
+			byte[] codes = loadClassFile(clzFileName);
+			if(codes != null){
+				return codes;
+			}			
 		}
-		return baos.toByteArray();		
+		
+		return null;
+		
+		
+		
 	}
 	
-	public void addClassPath(String path) {
-		clzPaths.add(path);
-	}
+	private byte[] loadClassFile(String clzFileName) {
 		
+		File f = new File(clzFileName);
+		
+		try {
+		
+			return IOUtils.toByteArray(new FileInputStream(f));
+			
+		} catch (IOException e) {			
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	
+	
+	public void addClassPath(String path) {
+		if(this.clzPaths.contains(path)){
+			return;
+		}
+		
+		this.clzPaths.add(path);
+		
+	}
+	
+	
+	
 	public String getClassPath(){
-		StringBuffer strBuffer = new StringBuffer();
-		for(int i=0;i<clzPaths.size();i++){
-			if(i == (clzPaths.size() - 1)){
-				strBuffer.append(clzPaths.get(i));
-			}else{
-				strBuffer.append(clzPaths.get(i)+";");
+		return StringUtils.join(this.clzPaths,";");
+	}
+
+	public ClassFile loadClass(String className) {
+		byte[] codes = this.readBinaryCode(className);
+		ClassFileParser parser = new ClassFileParser();
+		return parser.parse(codes);
+		
+	}
+	
+	
+	
+	// ------------------------------backup------------------------
+    public String getClassPath_V1(){
+		
+		StringBuffer buffer = new StringBuffer();
+		for(int i=0;i<this.clzPaths.size();i++){
+			buffer.append(this.clzPaths.get(i));
+			if(i<this.clzPaths.size()-1){
+				buffer.append(";");
 			}
 		}
-		return strBuffer.toString();
+		return buffer.toString();
 	}
+	
+	private byte[] loadClassFile_V1(String clzFileName) {
+		
+		BufferedInputStream bis = null;
+		
+		try {
+			
+			File f = new File(clzFileName);
+			
+						
+			bis = new BufferedInputStream(new FileInputStream(f));
+			
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			
+			
+			byte[] buffer = new byte[1024];
+			int length = -1;
+			
+			while((length = bis.read(buffer)) != -1){
+				bos.write(buffer, 0, length);				
+			}
+			
+			byte [] codes = bos.toByteArray();
+			
+			return codes;
+			
+		} catch(IOException e){
+			e.printStackTrace();
+			
+		} finally{
+			if(bis != null){
+				try {
+					bis.close();
+				} catch (IOException e) {					
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
+		
+	}
+
+
+	
 
 }
