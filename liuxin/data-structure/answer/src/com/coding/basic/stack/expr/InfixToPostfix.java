265c8c61df2ad645e9c1d103e9c752d83b2fdf92
onlyliuxin <14703250@qq.com>
2017-04-17 10:49:39 +0800

    refactor

--- liuxin/data-structure/answer/src/com/coding/basic/stack/expr/InfixToPostfix.java
+++ liuxin/data-structure/answer/src/com/coding/basic/stack/expr/InfixToPostfix.java
@@ -1,52 +1,13 @@
 package com.coding.basic.stack.expr;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 public class InfixToPostfix {
 	
 	public static List<Token> convert(String expr) {
-		
-		TokenParser parser = new TokenParser();
-		List<Token> tokens = parser.parse(expr);
-		
-		Stack<Token> s = new Stack<Token>();
-		List<Token> result = new ArrayList<Token>();
-		
-		for(Token token : tokens){
-			if(token.isOperator()){
-				if(token.toString().equals("*") || token.toString().equals("/")){
-					
-					while (!s.empty() 
-							&& !s.peek().toString().equals("+") 
-							&& !s.peek().toString().equals("-")) {
-						
-						result.add(s.pop());
-						
-					}
-					
-					s.push(token);
-				}
-				if(token.toString().equals("+") || token.toString().equals("-")){
-					while (!s.empty() ) {
-						result.add(s.pop());
-					}
-					s.push(token);
-				}
-			}
-			if(token.isNumber()){
-				result.add(token);
-			}
-		}
-		
-		return result;
+	    return null;
 	}
 	
-	public static void main(String[] args){
-		List<Token> tokens = convert("10+20*2");
-		System.out.println(tokens);
-		
-	}
+	
 
 }
