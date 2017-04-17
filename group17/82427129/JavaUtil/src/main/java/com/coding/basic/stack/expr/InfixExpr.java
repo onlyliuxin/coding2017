e52aa3c90ec4d53030f9050dce2a23c5fbc4542e
mengxz <82427129@qq.com>
2017-04-17 18:30:07 +0800

    update infixExpr.java complete InfixExpression's evaluate

--- group17/82427129/JavaUtil/src/main/java/com/coding/basic/stack/expr/InfixExpr.java
+++ group17/82427129/JavaUtil/src/main/java/com/coding/basic/stack/expr/InfixExpr.java
@@ -1,52 +1,114 @@
 package com.coding.basic.stack.expr;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
+
+import com.coding.basic.stack.StackUtil;
 
 public class InfixExpr {
 	String expr = null;
-	private Stack<Object> stack = new Stack<>();
+	private List<Token> tokenList = new ArrayList<>();
+	private final static String regex = "-|\\+|\\*|/|\\(|\\)";
 
 	public InfixExpr(String expr) {
 		this.expr = expr;
 		init();
 	}
-	public void init(){
-		splitOpraAndNum();
+
+	private void init() {
+		if (StackUtil.isValidPairs(expr)) {
+			initTokens();
+		} else {
+			throw new RuntimeException(
+					"Syntax error, ( ) is not completed in your Expression");
+		}
 	}
-	private void splitOpraAndNum() {
-		char[] array = expr.toCharArray();
-		int mark = 0;
-		for (int i = 0; i < array.length; i++) {
-			char c = array[i];
-			int cc = c&0xffff;
-			if(cc<48||cc>57){
-				if((i-mark)>0){
-					String num = expr.substring(mark, i);
-					stack.push(Integer.parseInt(num));
-				}
-			}else{
-				mark = i;
+
+	/**
+	 * 表达式分解并入栈
+	 */
+	private void initTokens() {
+		String[] nums = expr.split(regex);
+		ArrayList<String> arrayList = new ArrayList<String>();
+		for (int i = 0; i < nums.length; i++) {
+			if (!nums[i].equals("")) {
+				arrayList.add(nums[i]);
+			}
+		}
+		char[] exChars = expr.toCharArray();
+		int numInx = 0;
+		for (int i = 0; i < exChars.length;) {
+			char ex = exChars[i];
+			int item = ex & 0xffff;
+			if (item >= 48 && item <= 57) {
+				int numSize = arrayList.get(numInx++).length();
+				String num = new String(exChars, i, numSize);
+				Token token = new Token(num, Token.NUMBER);
+				tokenList.add(token);
+				i += numSize;
+			} else {
+				Token token = new Token(String.valueOf(ex), Token.OPERA);
+				tokenList.add(token);
+				i += 1;
 			}
 		}
 	}
-	/**
-	 * 转换成前序表达式进行计算
-	 * @return
-	 */
-	public float evaluatePre(){
-		
-		return 0.0f;
-	}
 
 	/**
-	 * 循环扫描，每次找到最高优先级的操作符，如果是*或者/那么计算这个操作符两边的数字，
-	 * 然后利用返回值改变以前的表达式，再次循环
-	 * 直到表达式变成只有一个数字的时候停止
+	 * 从左向右扫描所有token，当遇到同优先级或低优先级的操作符时，把之前的操作符依次出栈并计算结果
+	 * 扫描完所有token后，依次出栈所有操作数并计算结果
+	 * 
 	 * @return
 	 */
 	public float evaluate() {
-
-		return 0.0f;
+		Stack<Token> opStack = new Stack<>();
+		Stack<Float> numStack = new Stack<>();
+		for (int i = 0; i < tokenList.size(); i++) {
+			Token token = tokenList.get(i);
+			switch (token.getType()) {
+			case Token.NUMBER:
+				numStack.push(token.getNumValue());
+				break;
+			case Token.OPERA:
+				if (opStack.isEmpty()) {
+					opStack.push(token);
+				} else {
+					while (!opStack.isEmpty() && !token.hasHigherPriority(opStack.peek())) {
+						Token preOper = opStack.pop();
+						Float latter = numStack.pop();
+						Float former = numStack.pop();
+						Float result = caculate(preOper, latter, former);
+						numStack.push(result);
+					}
+					opStack.push(token);
+				}
+				break;
+			default:
+				throw new RuntimeException(" error not Number not Operate ");
+			}
+		}
+		while (!opStack.isEmpty()) {
+			Token pre = opStack.pop();
+			Float f1 = numStack.pop();
+			Float f2 = numStack.pop();
+			Float result = caculate(pre, f1, f2);
+			numStack.push(result);
+		}
+		return numStack.pop();
 	}
-	
+	private Float caculate(Token t, Float latter,Float former){
+		switch (t.getValue()) {
+		case "+":
+			return latter + former;
+		case "-":
+			return former - latter;
+		case "*":
+			return latter * former;
+		case "/":
+			return former / latter;
+		default:
+			throw new RuntimeException("operation"+t.getValue()+" isn't implemented");
+		}
+	}
 }
