1f4be5bc40c4d707984520db027f56059ac8bd56
zheng <765324639@qq.com>
2017-04-19 20:55:31 +0800

    完成jvm第四次作业中的jvm部分

---
+++ group01/765324639/src/main/java/minijvm/print/ConstantPoolPrinter.java
@@ -0,0 +1,18 @@
+package minijvm.print;
+
+import minijvm.constant.ConstantPool;
+
+public class ConstantPoolPrinter {
+	ConstantPool pool;
+	ConstantPoolPrinter(ConstantPool pool){
+		this.pool = pool;
+	}
+	public void print(){
+		
+		System.out.println("Constant Pool:");
+		
+		
+		
+		
+	}
+}
