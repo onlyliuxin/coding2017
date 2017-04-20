8e1a1ea91010bdff6d9bba53bacbb72239a0fc76
Haochen <haochen_233333@163.com>
2017-04-20 17:21:10 +0800

    move InfixExpr

--- group01/895457260/code/src/main/java/algorithm/expression/InfixExpr.java
+++ group01/895457260/code/src/main/java/algorithm/expression/InfixExpr.java
@@ -1,4 +1,4 @@
-package algorithm;
+package algorithm.expression;
 
 import datastructure.basic.Stack;
 
--- group01/895457260/code/src/test/java/algorithm/expression/InfixExprTest.java
+++ group01/895457260/code/src/test/java/algorithm/expression/InfixExprTest.java
@@ -1,4 +1,4 @@
-package algorithm;
+package algorithm.expression;
 
 import org.junit.After;
 import org.junit.Assert;
