aaf64e80887e3e642193ab34cdacf2a01f2f4ce3
zhanglei <383117348@qq.com>
2017-04-21 10:24:47 +0800

    前序后序计算表达式

--- group27/383117348/data-structure/com/coding/basic/stack/expr/PostfixExpr.java
+++ group27/383117348/data-structure/com/coding/basic/stack/expr/PostfixExpr.java
@@ -1,5 +1,9 @@
 package com.coding.basic.stack.expr;
 
+import java.util.List;
+import java.util.Stack;
+
+
 public class PostfixExpr {
 	String expr = null;
 	
@@ -8,11 +12,44 @@ public class PostfixExpr {
 	}
 
 	public float evaluate() {
+		Stack<String> stack = new Stack<String>();
+		TokenParser parser = new TokenParser();
+		List<Token> tokens  = parser.parse(expr);
+		for(Token token :tokens){
+			if(token.isNumber()){
+				stack.push(token.toString());
+			}
+			if(token.isOperator()){
+				int num1 = Integer.parseInt(stack.pop());
+				int num2 = Integer.parseInt(stack.pop());
+				String result = parseToResult(num1,num2,token.toString());
+				stack.push(result);
+			}
+		}
 		
 		
 		
-		return 0.0f;
+		return Float.parseFloat(stack.pop());
 	}
 	
-	
+	private String parseToResult(int num1, int num2, String oper) {
+		// TODO Auto-generated method stub
+		String result = "";
+		if(oper.equals("*"))
+			result = (num1*num2)+"";
+		if(oper.equals("/"))
+			result = (num2/num1)+"";
+		if(oper.equals("+"))
+			result = (num1+num2)+"";
+		if(oper.equals("-"))
+			result = (num2-num1)+"";
+		return result;
+	}
+
+	public static void main(String[] args) {
+		//9+(3-1)*3+10/2
+		PostfixExpr pos = new PostfixExpr("9 3 1-3*+ 10 2/+");
+		float f =pos.evaluate();
+		System.out.println(f);
+	}
 }
