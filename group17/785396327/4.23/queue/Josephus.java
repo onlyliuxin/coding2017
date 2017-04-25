f622e290b06d02f455846639b52778751a34aa5a
gongxun <gongxun@wesai.com>
2017-04-25 18:46:39 +0800

    暂存

--- group17/785396327/4.23/queue/Josephus.java
+++ group17/785396327/4.23/queue/Josephus.java
@@ -1,5 +1,6 @@
 package queue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,6 +11,29 @@ import java.util.List;
  */
 public class Josephus {
     public static List<Integer> execute(int n, int m) {
-        return null;
+        if (m <= 0)
+            return null;
+        List<Integer> origin = new ArrayList<Integer>();
+        List<Integer> sequence = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            origin.add(i);
+        }
+        int count = 1;//计数器
+        int index = count - 1;//当前元素的索引
+        while (origin.size() > 1) {
+            if (count == m) {
+                sequence.add(origin.remove(index));
+                count = 1;//删除一个元素，则重新计数
+            } else {
+                count++;
+                if (index == origin.size() - 1)
+                    //遍历到最后一个元素，重回头索引
+                    index = 0;
+                else
+                    index++;
+            }
+        }
+        sequence.add(origin.get(0));
+        return sequence;
     }
 }
