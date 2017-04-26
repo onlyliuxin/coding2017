c21d3a463bf47be3f66ac0b29795a5911e775b05
vegetableDogBai <bdl1994@163.com>
2017-04-26 16:51:38 +0800

    week8 数据结构作业完成

--- group23/563253496/week7_jvm/src/com/coderising/jvm/test/ClassFileloaderTest.java
+++ group23/563253496/week7_jvm/src/com/coderising/jvm/test/ClassFileloaderTest.java
@@ -31,7 +31,7 @@ public class ClassFileloaderTest {
 	
 	private static final String FULL_QUALIFIED_CLASS_NAME = "com/coderising/jvm/test/EmployeeV1";
 	
-	static String path1 = "C:\\Users\\liuxin\\git\\coding2017\\liuxin\\mini-jvm\\bin";
+	static String path1 = "D:\\mygit\\coding2017\\group23\\563253496\\week7_jvm\\out\\production\\week7_jvm";
 	static String path2 = "C:\temp";
 	
 	static ClassFile clzFile = null;
