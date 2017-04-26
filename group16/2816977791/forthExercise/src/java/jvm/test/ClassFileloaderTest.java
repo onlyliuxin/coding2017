51a92b12bca6091ec52110d1b69083f5f20f1ff0
starlight0405 <ang505@163.com>
2017-04-26 10:16:46 +0800

    add stackutil

--- group16/2816977791/forthExercise/src/java/jvm/test/ClassFileloaderTest.java
+++ group16/2816977791/forthExercise/src/java/jvm/test/ClassFileloaderTest.java
@@ -1,6 +1,7 @@
 package jvm.test;
 
 import jvm.loader.ClassFileLoader;
+import jvm.util.Util;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -62,23 +63,9 @@ public class ClassFileloaderTest {
         byte[] codes = new byte[]{byteCodes[0], byteCodes[1], byteCodes[2], byteCodes[3]};
 
 
-        String acctualValue = this.byteToHexString(codes);
+        String acctualValue = Util.byteToHexString(codes);
 
         Assert.assertEquals("cafebabe", acctualValue);
     }
 
-
-    private String byteToHexString(byte[] codes) {
-        StringBuffer buffer = new StringBuffer();
-        for (int i = 0; i < codes.length; i++) {
-            byte b = codes[i];
-            int value = b & 0xFF;
-            String strHex = Integer.toHexString(value);
-            if (strHex.length() < 2) {
-                strHex = "0" + strHex;
-            }
-            buffer.append(strHex);
-        }
-        return buffer.toString();
-    }
 }
