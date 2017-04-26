74f8962a9c1ce09b26ddcc31ff794a5ca5c20ea2
learnGithubChen <729245768@qq.com>
2017-04-26 19:23:25 +0800

    添加命令

---
+++ group11/729245768/DataStructure/src/main/coding_170423/jvm/cmd/InvokeSpecialCmd.java
@@ -0,0 +1,19 @@
+package main.coding_170423.jvm.cmd;
+
+import main.coding_170423.jvm.clz.ClassFile;
+
+/**
+ * Created by peterchen on 2017/4/26.
+ */
+public class InvokeSpecialCmd extends TwoOperandCmd {
+    public InvokeSpecialCmd(ClassFile clzFile, String opCode) {
+        super(clzFile,opCode);
+
+    }
+
+    @Override
+    public String toString() {
+
+        return super.getOperandAsMethod();
+    }
+}
---
+++ group11/729245768/DataStructure/src/main/coding_170423/jvm/cmd/OneOperandCmd.java
@@ -0,0 +1,27 @@
+package main.coding_170423.jvm.cmd;
+
+import main.coding_170423.jvm.clz.ClassFile;
+
+/**
+ * Created by peterchen on 2017/4/26.
+ */
+public class OneOperandCmd extends ByteCodeCommand {
+    private int operand;
+
+    public OneOperandCmd(ClassFile clzFile, String opCode) {
+        super(clzFile, opCode);
+
+    }
+    public  int getOperand() {
+
+        return this.operand;
+    }
+
+    public void setOperand(int oprand1) {
+        this.operand = oprand1;
+
+    }
+    public  int getLength(){
+        return 2;
+    }
+}
