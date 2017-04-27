0ae26092d90ea06baf4c28abf5ab3594bdcc23bb
johnChnia <zhouqiang847@gmail.com>
2017-04-27 23:19:44 +0800

    修改classload支持ioutil、stringutil， 新增loadClassFile方法

--- group24/315863321/src/main/java/jvm/loader/ClassFileLoader.java
+++ group24/315863321/src/main/java/jvm/loader/ClassFileLoader.java
@@ -3,54 +3,112 @@ package jvm.loader;
 
 import com.johnChnia.coding2017.basic.ArrayList;
 import com.johnChnia.coding2017.basic.List;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
 
 /**
- * @// TODO: 2017/4/20 改成  try... with...resource
- * @// TODO: 2017/4/20 close inputstream
- * @// TODO: 2017/4/20 修改TreeInfo直接返回File
+ * Created by john on 2017/4/27.
  */
 public class ClassFileLoader {
 
     private List<String> clzPaths = new ArrayList<String>();
 
     public byte[] readBinaryCode(String className) {
-        for (int i = 0; i < clzPaths.size(); i++) {
-            // 找到指定类文件
-            Directory.TreeInfo treeInfo = Directory.walk(clzPaths.get(i), className);
-            if (treeInfo.files.size() > 0) {
+        className = className.replace('.', File.separatorChar) + ".class";
+        for (String path : this.clzPaths) {
+
+            String clzFileName = path + File.separatorChar + className;
+            byte[] codes = loadClassFile(clzFileName);
+            if (codes != null) {
+                return codes;
+            }
+        }
+
+        return null;
+    }
+
+    private byte[] loadClassFile(String clzFileName) {
+        File f = new File(clzFileName);
+        try {
+
+            return IOUtils.toByteArray(new FileInputStream(f));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+
+    public void addClassPath(String path) {
+        if(this.clzPaths.contains(path)){
+            return;
+        }
+
+        this.clzPaths.add(path);
+    }
+
+
+    public String getClassPath() {
+        return StringUtils.join(this.clzPaths,";");
+
+    }
+
+    // ------------------------------backup------------------------
+    public String getClassPath_V1(){
+
+        StringBuffer buffer = new StringBuffer();
+        for(int i=0;i<this.clzPaths.size();i++){
+            buffer.append(this.clzPaths.get(i));
+            if(i<this.clzPaths.size()-1){
+                buffer.append(";");
+            }
+        }
+        return buffer.toString();
+    }
+
+    private byte[] loadClassFile_V1(String clzFileName) {
+
+        BufferedInputStream bis = null;
+
+        try {
+
+            File f = new File(clzFileName);
+
+
+            bis = new BufferedInputStream(new FileInputStream(f));
+
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+
+            byte[] buffer = new byte[1024];
+            int length = -1;
+
+            while((length = bis.read(buffer)) != -1){
+                bos.write(buffer, 0, length);
+            }
+
+            byte [] codes = bos.toByteArray();
+
+            return codes;
+
+        } catch(IOException e){
+            e.printStackTrace();
+
+        } finally{
+            if(bis != null){
                 try {
-                    FileInputStream fis = new FileInputStream(treeInfo.files.get(0));
-                    ByteArrayOutputStream bos = new ByteArrayOutputStream();  // 自动增长
-                    byte[] buff = new byte[1024];
-                    int len;
-                    while ((len = fis.read(buff)) != -1) {
-                        bos.write(buff, 0, len);
-                    }
-                    return bos.toByteArray();
-                } catch (Exception e) {
+                    bis.close();
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
         return null;
 
-    }
-
-
-    public void addClassPath(String path) {
-        clzPaths.add(path);
-    }
-
-
-    public String getClassPath() {
-        StringBuilder sb = new StringBuilder();
-        for (int index = 0; index < clzPaths.size(); index++) {
-            sb.append(clzPaths.get(index));
-            sb.append(";");
-        }
-        return sb.toString().substring(0, sb.length() - 1);
     }
 
 
