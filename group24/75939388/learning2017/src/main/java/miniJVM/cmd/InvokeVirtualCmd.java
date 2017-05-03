d43fb1ea2e91ca96cbea65ca7b00b530ea17e04c
macvis_qq75939388 <macvis@126.com>
2017-05-03 16:56:04 +0800

    作业完成

--- group24/75939388/learning2017/src/main/java/miniJVM/cmd/InvokeVirtualCmd.java
+++ group24/75939388/learning2017/src/main/java/miniJVM/cmd/InvokeVirtualCmd.java
@@ -24,58 +24,41 @@ public class InvokeVirtualCmd extends TwoOperandCmd {
 
 	@Override
 	public void execute(StackFrame frame, ExecutionResult result) {
-		// TODO: 2017/5/3 没有完全理解，copy老师的先
-		
-		//先得到对该方法的描述
 		MethodRefInfo methodRefInfo = (MethodRefInfo)this.getConstantInfo(this.getIndex());
 
 		String className = methodRefInfo.getClassName();
 		String methodName = methodRefInfo.getMethodName();
 
-		// 我们没有实现System.out.println方法，  所以也不用建立新的栈帧， 直接调用Java的方法， 打印出来即可。
+		//不需要实现println方法
 		if("java/io/PrintStream".equals(className)
 				&& "println".equals(methodName)){
 			JavaObject jo = frame.getOperandStack().pop();
 			String value = jo.toString();
 			System.err.println("-------------------"+value+"----------------");
 
-			// 这里就是那个out对象， 因为是个假的，直接pop出来
+			//假的对象，直接弹出无视掉
 			frame.getOperandStack().pop();
-
 			return;
 		}
 
-		//注意：多态， 这才是真正的对象, 先从该对象的class 中去找对应的方法，找不到的话再去找父类的方法
+		//不停的至父类寻找同名方法，即实现多态
 		JavaObject jo = frame.getOperandStack().peek();
-
 		MethodArea ma = MethodArea.getInstance();
-
 		Method m = null;
-
 		String currentClassName = jo.getClassName();
-
 		while(currentClassName != null){
-
 			ClassFile currentClassFile = ma.findClassFile(currentClassName);
-
-			m = currentClassFile.getMethod(methodRefInfo.getMethodName(),
-					methodRefInfo.getParamAndReturnType());
+			m = currentClassFile.getMethod(methodRefInfo.getMethodName(), methodRefInfo.getParamAndReturnType());
 			if(m != null){
-
 				break;
-
 			} else{
-				//查找父类
 				currentClassName = currentClassFile.getSuperClassName();
 			}
 		}
-
 		if(m == null){
-			throw new RuntimeException("Can't find method for :" + methodRefInfo.toString());
+			throw new RuntimeException("no method find :" + methodRefInfo.toString());
 		}
-
 		result.setNextAction(ExecutionResult.PAUSE_AND_RUN_NEW_FRAME);
-
 		result.setNextMethod(m);
 	}
 
