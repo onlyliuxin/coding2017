1c9a48a9c52d3923b995b0a6d833e93a1f52113a
Harry <chenhaowei93@163.com>
2017-05-04 17:39:29 +0800

    update stack
    实现QuickMinStack, StackWithTwoQueue, TwoStackInOneArray

--- group02/727171008/src/com/github/HarryHook/coding2017/stack/QuickMinStack.java
+++ group02/727171008/src/com/github/HarryHook/coding2017/stack/QuickMinStack.java
@@ -1,5 +1,11 @@
 package com.github.HarryHook.coding2017.stack;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import com.sun.xml.internal.ws.encoding.SwACodec;
+
 /**
  * 设计一个栈，支持栈的push和pop操作，以及第三种操作findMin, 它返回改数据结构中的最小元素
  * finMin操作最坏的情形下时间复杂度应该是O(1) ， 简单来讲，操作一次就可以得到最小值
@@ -8,15 +14,55 @@ package com.github.HarryHook.coding2017.stack;
  *
  */
 public class QuickMinStack {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
     public void push(int data) {
+
+	s1.push(data);
+
+	if (s2.isEmpty()) {
+	    s2.push(data);
+	} else if (data > s2.peek()) {
+	    s2.push(s2.peek());
+	} else {
+	    s2.push(data);
+	}
 
     }
 
     public int pop() {
-	return -1;
+	if (s1.isEmpty()) {
+	    throw new RuntimeException("stack is empty");
+	}
+	s2.pop();
+	return s1.pop();
     }
 
     public int findMin() {
-	return -1;
+	if (s2.isEmpty()) {
+	    throw new RuntimeException("stack is empty");
+	}
+	return s2.peek();
+    }
+
+    public static void main(String[] args) {
+	QuickMinStack stack = new QuickMinStack();
+	stack.push(5);
+	stack.push(3);
+	stack.push(2);
+	stack.push(4);
+	stack.push(1);
+	System.out.println("findMin: " + stack.findMin());
+	System.out.println("pop: " + stack.pop());
+	System.out.println("findMin: " + stack.findMin());
+	System.out.println("pop: " + stack.pop());
+	System.out.println("findMin: " + stack.findMin());
+	System.out.println("pop: " + stack.pop());
+	System.out.println("findMin: " + stack.findMin());
+	System.out.println("pop: " + stack.pop());
+	System.out.println("findMin: " + stack.findMin());
+	System.out.println("pop: " + stack.pop());
+	System.out.println("findMin: " + stack.findMin());
     }
 }
--- group02/727171008/src/com/github/HarryHook/coding2017/stack/TwoStackInOneArray.java
+++ group02/727171008/src/com/github/HarryHook/coding2017/stack/TwoStackInOneArray.java
@@ -1,5 +1,7 @@
 package com.github.HarryHook.coding2017.stack;
 
+import java.util.Arrays;
+
 /**
  * 用一个数组实现两个栈 将数组的起始位置看作是第一个栈的栈底，将数组的尾部看作第二个栈的栈底，压栈时，栈顶指针分别向中间移动，直到两栈顶指针相遇，则扩容。
  * 
@@ -7,7 +9,9 @@ package com.github.HarryHook.coding2017.stack;
  *
  */
 public class TwoStackInOneArray {
-    Object[] data = new Object[10];
+    Object[] data = new Object[6];
+    int i = 0;
+    int j = data.length - 1;
 
     /**
      * 向第一个栈中压入元素
@@ -15,6 +19,10 @@ public class TwoStackInOneArray {
      * @param o
      */
     public void push1(Object o) {
+	if (i > j) {
+	    expansion();
+	}
+	data[i++] = o;
 
     }
 
@@ -24,7 +32,12 @@ public class TwoStackInOneArray {
      * @return
      */
     public Object pop1() {
-	return null;
+	if (data[0] == null) {
+	    throw new RuntimeException("stack1 is empty");
+	}
+	Object tmp = data[i - 1];
+	data[--i] = null;
+	return tmp;
     }
 
     /**
@@ -34,13 +47,17 @@ public class TwoStackInOneArray {
      */
 
     public Object peek1() {
-	return null;
+	return data[i - 1];
     }
 
     /*
      * 向第二个栈压入元素
      */
     public void push2(Object o) {
+	if (i > j) {
+	    expansion();
+	}
+	data[j--] = o;
 
     }
 
@@ -50,7 +67,13 @@ public class TwoStackInOneArray {
      * @return
      */
     public Object pop2() {
-	return null;
+	if (data[data.length - 1] == null) {
+	    throw new RuntimeException("stack2 is empty");
+	}
+	Object tmp = data[j + 1];
+	data[++j] = null;
+	return tmp;
+
     }
 
     /**
@@ -60,7 +83,68 @@ public class TwoStackInOneArray {
      */
 
     public Object peek2() {
-	return null;
+	return data[j + 1];
+    }
+
+    // 给数组扩容
+    public void expansion() {
+	int oldCapacity = data.length;
+	int newCapacity = oldCapacity * 2; // 此时扩容是因为数组有可能全是stack2的元素
+	data = Arrays.copyOf(data, newCapacity);
+	int newIndex = newCapacity - 1;
+	for (int k = oldCapacity - 1; k > j; k--) {
+	    data[newIndex--] = data[k];
+	    data[k] = null;
+	}
+	j += newCapacity - oldCapacity;
+    }
+
+    public static void main(String[] args) {
+	TwoStackInOneArray stack = new TwoStackInOneArray();
+
+	stack.push1(3);
+	stack.push1(5);
+	stack.push1(6);
+	stack.push1(11);
+	System.out.println("peek1: " + stack.peek1());
+	System.out.println("pop1: " + stack.pop1());
+	System.out.println("pop1: " + stack.pop1());
+	stack.push1(61);
+	System.out.println("pop1: " + stack.pop1());
+	System.out.println("pop1: " + stack.pop1());
+	System.out.println("peek1: " + stack.peek1());
+	System.out.println("pop1: " + stack.pop1());
+
+	stack.push2(123);
+	stack.push2(1);
+	stack.push2(4);
+
+	stack.push1(3);
+	stack.push1(5);
+	stack.push1(6);
+
+	stack.push2(123);
+	stack.push2(1);
+	stack.push2(4);
+	stack.push2(12);
+	stack.push2(2);
+	stack.push2(8);
+	System.out.println("pop1: " + stack.pop1());
+	System.out.println("peek1: " + stack.peek1());
+	System.out.println("pop1: " + stack.pop1());
+	System.out.println("pop1: " + stack.pop1());
+
+	System.out.println("pop2: " + stack.pop2());
+	System.out.println("pop2: " + stack.pop2());
+	System.out.println("pop2: " + stack.pop2());
+	System.out.println("pop2: " + stack.pop2());
+	System.out.println("pop2: " + stack.pop2());
+	System.out.println("pop2: " + stack.pop2());
+
+	System.out.println("pop2: " + stack.pop2());
+	System.out.println("pop2: " + stack.pop2());
+	System.out.println("pop2: " + stack.pop2());
+
     }
 
 }
