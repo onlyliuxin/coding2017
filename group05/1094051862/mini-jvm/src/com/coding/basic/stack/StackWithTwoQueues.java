f7c90f1cafbd018975ed1935b2e5f7007d46e432
ZhaoHongxin <zhaohongxin621@126.com>
2017-05-06 17:01:10 +0800

    queue数据结构

---
+++ group05/1094051862/mini-jvm/src/com/coding/basic/stack/StackWithTwoQueues.java
@@ -0,0 +1,16 @@
+package com.coding.basic.stack;
+
+
+public class StackWithTwoQueues {
+	
+
+    public void push(int data) {       
+
+    }
+
+    public int pop() {
+       return -1;
+    }
+
+    
+}
