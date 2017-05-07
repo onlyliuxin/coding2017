38cdfd1a962c2c8c90cd6998ff7c30353453adf4
'1299310140' <'13437282785@163.com'>
2017-05-07 18:24:09 +0800

    InfixExpr&&PrefixExpr&&PostfixExpr&&InfixToPostfix

--- group04/1299310140/src/com/coding/basic/stack/expr/InfixExpr.java
+++ group04/1299310140/src/com/coding/basic/stack/expr/InfixExpr.java
@@ -1,75 +1,48 @@
 package com.coding.basic.stack.expr;
 
+import java.util.List;
 import java.util.Stack;
 
 public class InfixExpr {
-	//操作数为整数，操作符只支持+、-、*、/，/的返回值为float类型
-	private String expr = "";
-	private char[] sc = null;
-	private int pos = 0;
+	String expr = null;
 	
 	public InfixExpr(String expr) {
 		this.expr = expr;
-		this.sc = expr.toCharArray();
-	}
-	
-	//下一个操作数
-	private int nextOperand(){
-		int result = sc[pos] - 48;//char:'0'-->int:0
-		pos++;
-		while(pos < sc.length && sc[pos] >= '0' && sc[pos] <= '9'){
-			result = result * 10 + (sc[pos] - 48);
-			pos++;
-		}
-		return result;
-	}
-	
-	//下一个操作符
-	private char nextOperator(){
-		return sc[pos++];
 	}
 
-	public float evaluate() {		
-		Stack<Number> stack = this.multiplyDivide();
-		float result = add(stack);
-		return result;
-	}
-	
-	//将栈内所有操作数相加
-	private float add(Stack<Number> stack){
-		if(stack == null || stack.isEmpty()){
-			return 0;
-		}
+	public float evaluate() {
+		TokenParser parser = new TokenParser();
+		List<Token> expr_LToken = parser.parse(this.expr);
 		
-		float result = 0;
-		while(!stack.isEmpty()){
-			result = result + (float)stack.pop();
-		}
-		return result;
-	}
-	
-	//操作数入栈，完成乘除运算，'-'随数入栈
-	private Stack<Number> multiplyDivide(){
-		Stack<Number> operandStack = new Stack<Number>();
-		operandStack.push((float)this.nextOperand());
-		int tempOperand = 0;
-		char tempOperator = 0;
-		while(pos < sc.length){
-			tempOperator = this.nextOperator();
-			tempOperand = this.nextOperand();
-			if(tempOperator == '+'){
-				operandStack.push((float)tempOperand);
-			}else if(tempOperator == '-'){
-				operandStack.push((float)(- tempOperand));
-			}else if(tempOperator == '*'){
-				float top = (float)operandStack.pop();
-				operandStack.push(top * tempOperand);
-			}else if(tempOperator == '/'){
-				float top = (float)operandStack.pop();
-				operandStack.push(top / tempOperand);
+		Stack<Float> operandStack_SF = new Stack<Float>();
+		Stack<Token> operatorStack_SToken = new Stack<Token>();
+		for(Token token : expr_LToken){
+			if(token.isNumber()){
+				operandStack_SF.push(new Float(token.getIntValue()));
+			}else{
+				while(!operatorStack_SToken.isEmpty() && 
+						!token.hasHigherPriority(operatorStack_SToken.peek())){
+					//运算符栈不为空 且 当前运算符的优先级 小于等于 栈顶运算符的优先级
+					Float f2 = operandStack_SF.pop();
+					Float f1 = operandStack_SF.pop();
+					String operator = operatorStack_SToken.pop().toString();
+					Float result = CalculateUtil.calculate(operator, f1, f2);
+					operandStack_SF.push(result);
+				}
+				operatorStack_SToken.push(token);
 			}
 		}
 		
-		return operandStack;
+		//此时运算符栈 栈底到栈顶 运算符的优先级 严格递增
+		while(!operatorStack_SToken.isEmpty()){
+			Float f2 = operandStack_SF.pop();
+			Float f1 = operandStack_SF.pop();
+			String operator = operatorStack_SToken.pop().toString();
+			Float result = CalculateUtil.calculate(operator, f1, f2);
+			operandStack_SF.push(result);
+		}
+		return operandStack_SF.pop().floatValue();
 	}
+	
+	
 }
