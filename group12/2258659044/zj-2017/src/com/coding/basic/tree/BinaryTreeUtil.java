0b7c94f47c977bb559505ae969e4c1e202009d57
zj <2258659044@qq.com>
2017-05-08 21:56:07 +0800

    JVM第7次作业

---
+++ group12/2258659044/zj-2017/src/com/coding/basic/tree/BinaryTreeUtil.java
@@ -0,0 +1,131 @@
+package com.coding.basic.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreeUtil {
+	/**
+	 * 用递归的方式实现对二叉树的前序遍历， 需要通过BinaryTreeUtilTest测试
+	 * 
+	 * @param root
+	 * @return
+	 */
+	public static <T> List<T> preOrderVisit(BinaryTreeNode<T> root) {
+		List<T> result = new ArrayList<T>();
+		preOrderTraversal(root,result);
+		return result;
+	}
+
+    private static <T> List<T> preOrderTraversal(BinaryTreeNode<T> node,List<T> datas){
+		
+		if(node !=null){
+			datas.add(node.getData());
+			preOrderTraversal(node.getLeft(),datas);
+			preOrderTraversal(node.getRight(),datas);
+		}
+		return datas;
+	}
+    
+	/**
+	 * 用递归的方式实现对二叉树的中遍历
+	 * 
+	 * @param root
+	 * @return
+	 */
+	public static <T> List<T> inOrderVisit(BinaryTreeNode<T> root) {
+		List<T> result = new ArrayList<T>();
+		inOrderTraversal(root,result);
+		return result;
+	}
+	
+    private static <T> List<T> inOrderTraversal(BinaryTreeNode<T> node,List<T> datas){
+		
+		if(node !=null){			
+			inOrderTraversal(node.getLeft(),datas);
+			datas.add(node.getData());
+			inOrderTraversal(node.getRight(),datas);
+		}
+		return datas;
+	}
+
+	/**
+	 * 用递归的方式实现对二叉树的后遍历
+	 * 
+	 * @param root
+	 * @return
+	 */
+	public static <T> List<T> postOrderVisit(BinaryTreeNode<T> root) {
+		List<T> result = new ArrayList<T>();
+		postOrderTraversal(root,result);
+		return result;
+	}
+	
+    private static <T> List<T> postOrderTraversal(BinaryTreeNode<T> node,List<T> datas){
+		
+		if(node!=null){			
+			postOrderTraversal(node.getLeft(),datas);
+			postOrderTraversal(node.getRight(),datas);
+			datas.add(node.getData());
+		}		
+		return datas;
+	}
+	/**
+	 * 用非递归的方式实现对二叉树的前序遍历
+	 * @param root
+	 * @return
+	 */
+	public static <T> List<T> preOrderWithoutRecursion(BinaryTreeNode<T> root) {
+
+		List<T> result = new ArrayList<T>();
+
+		if(root == null){
+			return result;
+		}
+		
+		Stack<BinaryTreeNode<T>> stack = new Stack<BinaryTreeNode<T>>();		
+		stack.push(root);
+		
+		while (!stack.isEmpty()) {
+			BinaryTreeNode<T> node = stack.pop();
+			result.add(node.getData());
+			if (node.getRight() != null)
+				stack.push(node.getRight());
+			if (node.getLeft() != null)
+				stack.push(node.getLeft());
+		}
+
+		return result;
+	}
+	/**
+	 * 用非递归的方式实现对二叉树的中序遍历
+	 * @param root
+	 * @return
+	 */
+	public static <T> List<T> inOrderWithoutRecursion(BinaryTreeNode<T> root) {
+		
+		List<T> result = new ArrayList<T>();
+		
+		if(root == null){
+			return result;
+		}
+		
+		Stack<BinaryTreeNode<T>> stack = new Stack<BinaryTreeNode<T>>();		
+		
+		do {  
+            //依次将左节点均加入栈中  
+            while(root != null) {  
+                stack.push(root);  
+                root = root.getLeft();  
+            }  
+            if (!stack.isEmpty()) {  
+                root = stack.pop();  
+                result.add(root.getData());    
+                root = root.getRight();  
+            }  
+        } while(!stack.isEmpty() || root != null);  
+
+		return result;
+	}
+	
+}
