fc9f7a506407f550587a46220543a6371fd65534
gongxun <gongxun@wesai.com>
2017-05-09 16:44:47 +0800

    1.完成5月3日作业 2.修复底层链表bug

--- group17/785396327/5.3/StackWithTwoQueues.java
+++ group17/785396327/5.3/StackWithTwoQueues.java
@@ -1,12 +1,34 @@
+import queue.Queue;
+
 /**
- * Created by IBM on 2017/5/7.
+ * Created by william on 2017/5/7.
+ * 真正的数据只存在queue中，另一个只是作为数据的暂存中转，没有必要将另一个队列变成第一个队列的逆序
  */
 public class StackWithTwoQueues {
-    public void push(int data) {
+    private Queue<Integer> queue = new Queue<Integer>();
+    private Queue<Integer> tempQueue = new Queue<Integer>();
 
+    public void push(int data) {
+        queue.offer(data);
     }
 
+
     public int pop() {
-        return -1;
+        int size = queue.size();
+        int index = 0;
+        while (index++ < size - 1) {
+            Integer ele = queue.poll();
+            tempQueue.offer(ele);
+        }
+        Integer removeEle = queue.poll();
+        while (!tempQueue.isEmpty()) {
+            queue.offer(tempQueue.poll());
+        }
+        return removeEle;
+    }
+
+    @Override
+    public String toString() {
+        return queue.toString();
     }
 }
