3d868b1f73a5121c8fc8b91ec95d3a8b5d1691ab
Patrick <Patrick@DESKTOP-JLNF6GI>
2017-05-10 23:17:17 +0800

    5.8-5.14 tree练习完成

--- group24/121111914/src/com/github/ipk2015/coding2017/basic/tree/FileList.java
+++ group24/121111914/src/com/github/ipk2015/coding2017/basic/tree/FileList.java
@@ -7,14 +7,13 @@ import java.io.File;
  * 给定一个目录，递归的列出下面所有的子目录和文件
  * */
 public class FileList {
-	public void list(File f) {
+	private static String stringSpace = "  ";
+	public static void list(File f) {
 		listOneFile(f,0);
 	}
 	
-	private void listOneFile(File f,int space){
+	private static void listOneFile(File f,int space){
 		if(f.exists()){
-			String stringSpace = " ";
-			
 			File[] listFiles = f.listFiles();
 			for(int i = 0;i<listFiles.length;i++){
 				for(int j = 0;j<space;j++){
