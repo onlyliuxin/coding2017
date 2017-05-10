16aae1b4e313e2658367b561ce333260218772c9
gongxun <gongxun@wesai.com>
2017-05-10 18:56:10 +0800

    完成5月8日作业

--- group17/785396327/5.8/tree/BinaryTreeUtil.java
+++ group17/785396327/5.8/tree/BinaryTreeUtil.java
@@ -73,12 +73,16 @@ public class BinaryTreeUtil {
     public static <T> List<T> preOrderWithoutRecursion(BinaryTreeNode<T> root) {
         List<T> result = new ArrayList<T>();
         Stack<BinaryTreeNode<T>> stack = new Stack<BinaryTreeNode<T>>();
+        //这里必须加上“或”的stack不为空，因为下面if判断的某个节点没有右子节点，这样root就为null，但是stack不为空，可以继续弹栈
         while (root != null || !stack.isEmpty()) {
+            //将某个“根”节点的所有左子节点(包含左子孙节点)放入栈中
             while (root != null) {
                 result.add(root.getData());
                 stack.push(root);
                 root = root.getLeft();
             }
+            //将上面所有“根”节点的所有左子节点弹出一个
+            //这里只能一个一个查询，因为必须从离叶子节点最近的“根”节点查询是否有右节点
             if (!stack.isEmpty()) {
                 root = stack.pop();
                 root = root.getRight();
@@ -90,23 +94,30 @@ public class BinaryTreeUtil {
 
     /**
      * 用非递归的方式实现对二叉树的中序遍历
-     *
+     * 思路：1.首先一层层遍历节点的左子节点，并把遍历到节点存入栈中
+     *      2.当遍历到叶子节点，开始循环弹栈
+     *      3.如果弹出的节点存在右子节点，将右子节点设为新的根节点，并结束弹栈循环
      * @param root
      * @return
      */
     public static <T> List<T> inOrderWithoutRecursion(BinaryTreeNode<T> root) {
 
         List<T> result = new ArrayList<T>();
-        Queue<BinaryTreeNode<T>> queue = new Queue<BinaryTreeNode<T>>();
-        if (root != null) {
-            queue.offer(root);
-            while (!queue.isEmpty()) {
-                BinaryTreeNode<T> node = queue.poll();
-                if (node.getLeft() != null)
-                    queue.offer(node);
+        Stack<BinaryTreeNode<T>> stack = new Stack<BinaryTreeNode<T>>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.getLeft();
+            }
+
+            while (!stack.isEmpty()) {
+                BinaryTreeNode<T> node = stack.pop();
                 result.add(node.getData());
-                if (node.getRight() != null)
-                    queue.offer(node);
+                BinaryTreeNode<T> right = node.getRight();
+                if (right != null) {
+                    root = right;
+                    break;
+                }
             }
         }
         return result;
--- group17/785396327/5.8/tree/FileList.java
+++ group17/785396327/5.8/tree/FileList.java
@@ -1,11 +1,35 @@
 package tree;
 
+import queue.Queue;
+
 import java.io.File;
 
 /**
- * Created by IBM on 2017/5/8.
+ * Created by william on 2017/5/8.
  */
 public class FileList {
+    private static final String path = "G:\\Git\\homework";
+
+    public static void main(String[] args) {
+        Queue<File> queue = new Queue<File>();
+        File rootFile = new File(path);
+        queue.offer(rootFile);
+        while (!queue.isEmpty()) {
+            File file = queue.poll();
+            if (file.isDirectory()) {
+                File[] files = file.listFiles();
+                if (files != null) {
+                    for (int i = 0; i < files.length; i++) {
+                        File child = files[i];
+                        queue.offer(child);
+                    }
+                }
+            } else {
+                System.out.println(file.getName());
+            }
+        }
+    }
+
     public void list(File f) {
     }
 
