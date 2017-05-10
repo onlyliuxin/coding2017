a8421d5ccdf3115875861f6c5738d680d81227d2
patrickfeng <patrickfeng@172.26.183.81>
2017-05-10 12:04:17 +0800

    tree练习完成

--- group24/121111914/src/com/github/ipk2015/coding2017/basic/tree/BinaryTreeUtil.java
+++ group24/121111914/src/com/github/ipk2015/coding2017/basic/tree/BinaryTreeUtil.java
@@ -33,10 +33,25 @@ public class BinaryTreeUtil {
 	 */
 	public static <T> List<T> preOrderVisit(BinaryTreeNode<T> root) {
 		List<T> result = new ArrayList<T>();
-		
+		preOrderNode(root,result);
 		return result;
 	}
-
+	private static <T> void preOrderNode(BinaryTreeNode<T> node,List<T> list){
+		if(null == node){
+			return;
+		}
+		
+		T data = node.getData();
+		if(null != data){
+			list.add(data);
+		}
+		
+		BinaryTreeNode<T> leftNode = node.getLeft();
+		preOrderNode(leftNode,list);
+		
+		BinaryTreeNode<T> rightNode = node.getRight();
+		preOrderNode(rightNode,list);
+	}
 	/**
 	 * 用递归的方式实现对二叉树的中遍历
 	 * 
@@ -45,10 +60,26 @@ public class BinaryTreeUtil {
 	 */
 	public static <T> List<T> inOrderVisit(BinaryTreeNode<T> root) {
 		List<T> result = new ArrayList<T>();
-		
+		inOrderNode(root,result);
 		return result;
 	}
 
+	private static <T> void inOrderNode(BinaryTreeNode<T> node,List<T> list){
+		if(null == node){
+			return;
+		}
+		
+		BinaryTreeNode<T> leftNode = node.getLeft();
+		inOrderNode(leftNode,list);
+		
+		T data = node.getData();
+		if(null != data){
+			list.add(data);
+		}
+		
+		BinaryTreeNode<T> rightNode = node.getRight();
+		inOrderNode(rightNode,list);
+	}
 	/**
 	 * 用递归的方式实现对二叉树的后遍历
 	 * 
@@ -57,8 +88,26 @@ public class BinaryTreeUtil {
 	 */
 	public static <T> List<T> postOrderVisit(BinaryTreeNode<T> root) {
 		List<T> result = new ArrayList<T>();
-		
+		postOrderNode(root,result);
 		return result;
+	}
+	
+	private static <T> void postOrderNode(BinaryTreeNode<T> node,List<T> list){
+		if(null == node){
+			return;
+		}
+		
+		BinaryTreeNode<T> leftNode = node.getLeft();
+		postOrderNode(leftNode,list);
+		
+		BinaryTreeNode<T> rightNode = node.getRight();
+		postOrderNode(rightNode,list);
+		
+		T data = node.getData();
+		if(null != data){
+			list.add(data);
+		}
+		
 	}
 	/**
 	 * 用非递归的方式实现对二叉树的前序遍历
@@ -67,7 +116,28 @@ public class BinaryTreeUtil {
 	 */
 	public static <T> List<T> preOrderWithoutRecursion(BinaryTreeNode<T> root) {
 		
-		List<T> result = new ArrayList<T>();		
+		List<T> result = new ArrayList<T>();
+		Stack stack = new Stack();
+		stack.push(root);
+		while(!stack.isEmpty()){
+			BinaryTreeNode<T> node = (BinaryTreeNode<T>) stack.pop();
+			if(null == node){
+				break;
+			}
+			T data = node.getData();
+			if(null != data){
+				result.add(data);
+			}
+				
+			BinaryTreeNode<T> rightNode = node.getRight();
+			if(null != rightNode){
+				stack.push(rightNode);
+			}
+			BinaryTreeNode<T> leftNode = node.getLeft();
+			if(null != leftNode){
+				stack.push(leftNode);
+			}
+		}
 		
 		return result;
 	}
@@ -79,7 +149,34 @@ public class BinaryTreeUtil {
 	public static <T> List<T> inOrderWithoutRecursion(BinaryTreeNode<T> root) {
 		
 		List<T> result = new ArrayList<T>();
-		
+		Stack nodeStack = new Stack();
+		Stack midStack = new Stack();
+		nodeStack.push(root);
+		while(!nodeStack.isEmpty()){
+			BinaryTreeNode<T> node = (BinaryTreeNode<T>) nodeStack.pop();
+			if(null == node){
+				break;
+			}
+			
+			BinaryTreeNode<T> rightNode = node.getRight();
+			if(null != rightNode){
+				nodeStack.push(rightNode);
+			}
+			
+			BinaryTreeNode<T> leftNode = node.getLeft();
+			if(null != leftNode){
+				midStack.push(node.getData());
+				nodeStack.push(leftNode);
+			}else{
+				T data = node.getData();
+				if(null != data){
+					result.add(data);
+				}
+				if(!midStack.isEmpty()){
+					result.add((T) midStack.pop());
+				}
+			}
+		} 
 		return result;
 	}
 	
--- group24/121111914/src/com/github/ipk2015/coding2017/basic/tree/FileList.java
+++ group24/121111914/src/com/github/ipk2015/coding2017/basic/tree/FileList.java
@@ -3,10 +3,29 @@ package com.github.ipk2015.coding2017.basic.tree;
 
 
 import java.io.File;
-
+/*
+ * 给定一个目录，递归的列出下面所有的子目录和文件
+ * */
 public class FileList {
-	public void list(File f) {		
+	public void list(File f) {
+		listOneFile(f,0);
 	}
-
+	
+	private void listOneFile(File f,int space){
+		if(f.exists()){
+			String stringSpace = " ";
+			
+			File[] listFiles = f.listFiles();
+			for(int i = 0;i<listFiles.length;i++){
+				for(int j = 0;j<space;j++){
+					System.out.print(stringSpace);
+				}
+				System.out.println(listFiles[i].getName());
+				if(listFiles[i].isDirectory()){
+					listOneFile(listFiles[i],space+1);
+				}
+			}
+		}
+	}
 	
 }
