ee314308ee167d8eae6cc831723c9b6e369c71c0
lzb <lzbferrari@gmail.com>
2017-05-19 01:44:53 +0800

    加点注释

--- group24/1148285693/learning2017/learning-basic/src/main/java/me/lzb/boom/MemoryBoom.java
+++ group24/1148285693/learning2017/learning-basic/src/main/java/me/lzb/boom/MemoryBoom.java
@@ -9,7 +9,10 @@ import java.util.List;
  */
 public class MemoryBoom {
 
-
+    //-XX:MaxMetaspaceSize，最大空间，默认没有限制
+    //-XX:MetaspaceSize，初始空间大小，达到该值就会触发垃圾收集进行类型卸载，同时GC会对该值进行调整。如果释放了大量的空间，就适当降低该值；如果释放了很少的空间，那么在不超过MaxMetaspaceSize时，适当提高该值。
+    //-XX:MinMetaspaceFreeRatio，在GC之后，最小的Metaspace剩余空间容量的百分比，减少为分配空间所导致的垃圾收集
+    //-XX:MaxMetaspaceFreeRatio，在GC之后，最大的Metaspace剩余空间容量的百分比，减少为释放空间所导致的垃圾收集
     public void metaspaceBoom() {
         String path = MemoryBoom.class.getResource("/").getPath();
         String className = "me.lzb.boom.Boom";
