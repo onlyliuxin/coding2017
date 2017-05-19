cc504454416ea6c72bb5f49ebbc5d5d461762fe7
gongxun <gongxun@wesai.com>
2017-05-19 17:47:26 +0800

    完成作业

--- group17/785396327/5.14/OutOfMemoryDemo.java
+++ group17/785396327/5.14/OutOfMemoryDemo.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by IBM on 2017/5/15.
+ * Created by william on 2017/5/15.
+ * 设置 -Xms20m -Xmx20m
  */
 public class OutOfMemoryDemo {
+
+    public static void main(String[] args) {
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        List<Object> list = new ArrayList<Object>();
+        while (true) {
+            list.add(new OutOfMemoryDemo());
+        }
+    }
+
+
 }
