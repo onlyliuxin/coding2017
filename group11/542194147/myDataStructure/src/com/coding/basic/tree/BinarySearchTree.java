699ac09d20c333d861e54d7c89466695d69cc4a5
XMT-CN <542194147@qq.com>
2017-05-26 09:44:08 +0800

    数据结构6、7、8次作业

--- group11/542194147/myDataStructure/src/com/coding/basic/tree/BinarySearchTree.java
+++ group11/542194147/myDataStructure/src/com/coding/basic/tree/BinarySearchTree.java
@@ -1,5 +1,13 @@
 package com.coding.basic.tree;
 
+import java.util.List;
+
+/**
+ * 二叉查找树
+ * @author 小摩托
+ *
+ * @param <T>
+ */
 public class BinarySearchTree <T extends Comparable> {
 
 
@@ -10,19 +18,108 @@ public class BinarySearchTree <T extends Comparable> {
 	public BinaryTreeNode<T> getRoot(){
 		return root;
 	}
+	
 	public T findMin(){
-		return null;
+		return findMin(root);
 	}
-	public T findMax(){
-		return null;
-	}
-	public int height() {
-	    return -1;
-	}
-	public int size() {
-		return -1;
-	}
-	public void remove(T e){
+	private T findMin(BinaryTreeNode<T> node){
+		if(null==node.getLeft()){
+			return node.getData();
+		}
+		return findMin(node.getLeft());
 		
+	}
+	
+	public T findMax(){
+		return findMax(root);
+	}
+	private T findMax(BinaryTreeNode<T> node){
+		if(null==node.getRight()){
+			return node.getData();
+		}
+		return findMax(node.getRight());
+		
+	}
+	/*
+	 * 查找树的高度
+	 */
+	public int height() {
+	    return height(root);
+	}
+	private int height(BinaryTreeNode<T>node){
+		if (node == null) {
+            return 0;
+        }
+		int right=height(node.right);
+		int left=height(node.left);
+		
+		return (right>left?right+1:left+1);
+		
+	}
+	/*
+	 * 树的大小
+	 */
+	public int size() {
+		return size(root);
+	}
+	private int size(BinaryTreeNode<T>node){
+		if(node==null){
+			return 0;
+		}
+		return size(node.left)+size(node.right)+1;
+		
+	}
+	
+	public void remove(T e){
+		remove(e,root);
+	}
+	private BinaryTreeNode<T> remove(T e,BinaryTreeNode<T>node){
+		if (node == null) {
+            return node;
+        }
+        int compareResult = e.compareTo(node.data);
+        if (compareResult < 0) {
+            node.left = remove(e, node.left);
+        } else if (compareResult > 0) {
+            node.right = remove(e, node.right);
+        } else {
+            if (node.left != null && node.right != null) {
+                node.data = findMin(node.right);
+                node.right = remove(node.data, node.right);
+            } else {
+                node = (node.left != null) ? node.left : node.right;
+            }
+        }
+        return node;
+		
+	}
+	/*
+	 * 逐层查找
+	 */
+	public List<T> levelVisit(){
+		
+		return null;
+	}
+	
+	/*
+	 * 判断是否属于二叉查找树
+	 */
+	public boolean isValid(){
+		return false;
+	}
+	
+	/*
+	 * 获取两个节点的最小公共祖先
+	 */
+	public T getLowestCommonAncestor(T n1, T n2){
+		return null;
+        
+	}
+	
+	/*
+	 * 给定两个值， 获得处于这两个值中间的节点
+	 */
+	public List<T> getNodesBetween(T n1, T n2){
+		return null;
 	}
 }
--- group11/542194147/myDataStructure/src/com/coding/basic/tree/BinaryTreeUtil.java
+++ group11/542194147/myDataStructure/src/com/coding/basic/tree/BinaryTreeUtil.java
@@ -2,50 +2,80 @@ package com.coding.basic.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreeUtil {
 
 	/**
 	 * 用递归的方式实现对二叉树的前序遍历， 需要通过BinaryTreeUtilTest测试
-	 * 
+	 * 前序遍历： 根节点->左节点->右节点
 	 * @param root
 	 * @return
 	 */
 	public static <T> List<T> preOrderVisit(BinaryTreeNode<T> root) {
 		List<T> result = new ArrayList<T>();
-		result.add(root.getData());
-		if(root.getLeft()!=null){
-			preOrderVisit(root.getLeft());
-		}
-		if(root.getRight()!=null){
-			preOrderVisit(root.getRight());
-		}
+		preOrderVisit(root,result);
 		return result;
 	}
-
+	private static<T> void preOrderVisit(BinaryTreeNode<T> root,List<T> result){
+		if(null==root){
+			return;
+		}
+		result.add(root.getData());
+		if(null!=root.getLeft()){
+			preOrderVisit(root.getLeft(), result);
+		}
+		if(null!=root.getRight()){
+			preOrderVisit(root.getRight(), result);
+		}
+	}
 	/**
 	 * 用递归的方式实现对二叉树的中遍历
-	 * 
+	 * 中序遍历:  左节点->根节点->右节点
 	 * @param root
 	 * @return
 	 */
 	public static <T> List<T> inOrderVisit(BinaryTreeNode<T> root) {
 		List<T> result = new ArrayList<T>();
-		
+		inOrderVisit(root,result);
 		return result;
 	}
-
+	private static<T> void inOrderVisit(BinaryTreeNode<T> root,List<T> result){
+		if(null==root){
+			return;
+		}
+		if(null!=root.getLeft()){
+			inOrderVisit(root.getLeft(), result);
+		}
+		result.add(root.getData());
+		if(null!=root.getRight()){
+			inOrderVisit(root.getRight(), result);
+		}
+	}
 	/**
 	 * 用递归的方式实现对二叉树的后遍历
-	 * 
+	 * 后序遍历:  左节点->右节点->根节点
 	 * @param root
 	 * @return
 	 */
 	public static <T> List<T> postOrderVisit(BinaryTreeNode<T> root) {
 		List<T> result = new ArrayList<T>();
-		
+		postOrderVisit(root,result);
 		return result;
 	}
+	private static<T> void postOrderVisit(BinaryTreeNode<T> root,List<T> result){
+		if(null==root){
+			return;
+		}
+		if(null!=root.getLeft()){
+			postOrderVisit(root.getLeft(), result);
+		}
+		if(null!=root.getRight()){
+			postOrderVisit(root.getRight(), result);
+		}
+		result.add(root.getData());
+	}
+	
 	/**
 	 * 用非递归的方式实现对二叉树的前序遍历
 	 * @param root
@@ -54,7 +84,16 @@ public class BinaryTreeUtil {
 	public static <T> List<T> preOrderWithoutRecursion(BinaryTreeNode<T> root) {
 		
 		List<T> result = new ArrayList<T>();		
-		
+		Stack<BinaryTreeNode<T>> stack = new Stack<>();
+        while(root != null || !stack.isEmpty()){  
+            while(root != null){  
+            	result.add(root.getData());  
+                stack.push(root);  
+                root = root.getLeft();  
+            }  
+            root = stack.pop();  
+            root = root.getRight();  
+        }  
 		return result;
 	}
 	/**
@@ -65,7 +104,16 @@ public class BinaryTreeUtil {
 	public static <T> List<T> inOrderWithoutRecursion(BinaryTreeNode<T> root) {
 		
 		List<T> result = new ArrayList<T>();
-		
+		Stack<BinaryTreeNode<T>> stack = new Stack<>();
+        while(root != null || !stack.isEmpty()){  
+            while(root != null){  
+                stack.push(root);  
+                root = root.getLeft();
+            }  
+            root = stack.pop();
+            result.add(root.getData());  
+            root = root.getRight();  
+        }  
 		return result;
 	}
 }
