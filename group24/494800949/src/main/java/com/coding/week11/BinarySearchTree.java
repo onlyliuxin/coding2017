74d69b53bcc9fd13a222efdd3691a4fda8bd9b90
sdnb <xgct0505@qq.com>
2017-05-28 20:23:48 +0800

    week11 finished

--- group24/494800949/src/main/java/com/coding/week11/BinarySearchTree.java
+++ group24/494800949/src/main/java/com/coding/week11/BinarySearchTree.java
@@ -1,10 +1,18 @@
 package com.coding.week11;
 
+import com.coding.weak1.Queue;
 import com.coding.week10.BinaryTreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class BinarySearchTree<T extends Comparable<T>> {
 
 	BinaryTreeNode<T> root;
+
+	public BinarySearchTree() {
+	}
 
 	public BinarySearchTree(BinaryTreeNode<T> root) {
 		this.root = root;
@@ -91,6 +99,143 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		}
 		return node;
 	}
+
+	//按层次遍历： levelVisit
+	@SuppressWarnings("unchecked")
+	public List<T> levelVisit(){
+		List<T> list = new ArrayList<>();
+		if (root == null) {
+			return list;
+		}
+		Queue q = new Queue();
+		q.enQueue(root);
+		while (!q.isEmpty()) {
+			BinaryTreeNode<T> n = (BinaryTreeNode<T>)q.deQueue();
+			list.add(n.getData());
+			if (n.getLeft() != null) {
+				q.enQueue(n.getLeft());
+			}
+			if (n.getRight() != null) {
+				q.enQueue(n.getRight());
+			}
+		}
+		return list;
+	}
+
+	public static <T extends Comparable<T>> List<T> levelVisit(BinaryTreeNode<T> root) {
+		List<T> result = new ArrayList<>();
+		BinaryTreeNode<T> left = root.getLeft();
+		BinaryTreeNode<T> right = root.getRight();
+
+		if (right != null) {
+			List<T> rightList = levelVisit(right);
+			result.addAll(rightList);
+		}
+		if (left != null) {
+			List<T> leftList = levelVisit(left);
+			result.addAll(leftList);
+		}
+		result.add(root.getData());
+		return result;
+	}
+
+	//判断一个二叉树是不是二叉查找树
+	public boolean isValid(){
+
+		return isValid(root);
+	}
+
+	private boolean isValid(BinaryTreeNode<T> node) {
+		boolean valid = true;
+		if (node != null) {
+			T d = node.getData();
+
+			BinaryTreeNode<T> l = node.getLeft();
+			BinaryTreeNode<T> r = node.getRight();
+			if (l != null) {
+				if (d.compareTo(l.getData()) < 0) {
+					return false;
+				}
+			}
+			if (r != null) {
+				if (d.compareTo(r.getData()) >= 0) {
+					return false;
+				}
+			}
+			valid = isValid(l) && isValid(r);
+		}
+		return valid;
+	}
+
+
+	//获取两个节点的最小公共祖先
+	public T getLowestCommonAncestor(T n1, T n2){
+		return betweenNode(n1, n2, root).getData();
+	}
+
+	private BinaryTreeNode<T> betweenNode(T n1, T n2, BinaryTreeNode<T> node) {
+		if (node == null) {
+			return null;
+		} else {
+			T max = n1.compareTo(n2) >= 0 ? n1 : n2;
+			T min = max.compareTo(n1) == 0 ? n2 : n1;
+			if (max.compareTo(node.getData()) < 0) {
+				node = betweenNode(n1, n2, node.getLeft());
+			} else if (min.compareTo(node.getData()) > 0) {
+				node = betweenNode(n1, n2, node.getRight());
+			}
+			return node;
+		}
+
+	}
+
+
+
+	public void insert(List<T> l) {
+		for (T data : l) {
+			if (data == null) {
+				continue;
+			}
+			if (root == null) {
+				root = new BinaryTreeNode<>(data);
+			} else {
+				root.insert(data);
+			}
+		}
+	}
+
+	//给定两个值， 获得处于这两个值中间的节点
+	public List<T> getNodesBetween(T n1, T n2){
+		List<T> result = new ArrayList<T>();
+		Stack<BinaryTreeNode<T>> stack = new Stack<>();
+		stack.push(root);
+		while (!stack.isEmpty()){
+			BinaryTreeNode<T> node = stack.pop();
+			if (isBetween(n1, n2, node.getData())) {
+				result.add(node.getData());
+			}
+			if (node.getRight() != null) {
+				stack.push(node.getRight());
+			}
+			if (node.getLeft() != null) {
+				stack.push(node.getLeft());
+			}
+
+		}
+
+		return result;
+	}
+
+	private boolean isBetween(T n1, T n2, T data) {
+		T max = n1.compareTo(n2) >= 0 ? n1 : n2;
+		T min = max.compareTo(n1) == 0 ? n2 : n1;
+		if (max.compareTo(data) < 0 || min.compareTo(data) > 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
 
 	public static void main(String[] args) {
 		System.out.println(Math.max(0,0));
