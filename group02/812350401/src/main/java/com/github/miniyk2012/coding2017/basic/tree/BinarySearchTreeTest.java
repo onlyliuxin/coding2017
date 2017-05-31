71ecd910fc80f1c15409eb80ded84f37cc1ef222
thomas_young <yk_ecust_2007@163.com>
2017-06-01 07:43:18 +0800

    层序遍历，公共最小父节点，range

--- group02/812350401/src/main/java/com/github/miniyk2012/coding2017/basic/tree/BinarySearchTreeTest.java
+++ group02/812350401/src/main/java/com/github/miniyk2012/coding2017/basic/tree/BinarySearchTreeTest.java
@@ -6,6 +6,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.assertTrue;
 
 
 public class BinarySearchTreeTest {
@@ -97,16 +100,75 @@ public class BinarySearchTreeTest {
 
 	@Test
 	public void testIsValid() {
-        Assert.assertTrue(tree.isValid());
-        Assert.assertTrue(tree2.isValid());
+        assertTrue(tree.isValid());
+        assertTrue(tree2.isValid());
         Assert.assertFalse(tree3.isValid());
 
         BinarySearchTree<Integer> tree4 = new BinarySearchTree<>(tree.root.left);
-        Assert.assertTrue(tree4.isValid());
+        assertTrue(tree4.isValid());
 
         tree.root.right.right = new BinaryTreeNode<>(7);
         Assert.assertFalse(tree.isValid());
-        Assert.assertTrue(tree4.isValid());
+        assertTrue(tree4.isValid());
+
+    }
+
+	@Test
+	public void testLevelVisit() {
+        List<Integer> list = tree.levelVisit();
+        Assert.assertEquals("[6, 2, 8, 1, 4, 3]", Arrays.toString(list.toArray()));
+        List<Integer> list2 = tree2.levelVisit();
+        Assert.assertEquals("[5]", Arrays.toString(list2.toArray()));
+        BinarySearchTree<Integer> nullTree = new BinarySearchTree<Integer>(null);
+        List<Integer> nullList = nullTree.levelVisit();
+        Assert.assertEquals("[]", Arrays.toString(nullList.toArray()));
+    }
+
+    @Test
+    public void testGetLowestCommonAncestor() {
+	    int ancestor = tree.getLowestCommonAncestor(1, 3);
+        Assert.assertEquals(2, ancestor);
+        ancestor = tree.getLowestCommonAncestor(2, 3);
+        Assert.assertEquals(2, ancestor);
+        ancestor = tree.getLowestCommonAncestor(2, 6);
+        Assert.assertEquals(6, ancestor);
+        ancestor = tree.getLowestCommonAncestor(1, 8);
+        Assert.assertEquals(6, ancestor);
+        ancestor = tree.getLowestCommonAncestor(3, 4);
+        Assert.assertEquals(4, ancestor);
+    }
+
+    @Test
+    public void testGetNodesBetween() {
+	    List<Integer> list = tree.getNodesBetween(1, 4);
+	    List <Integer> expectedList = Arrays.asList(2, 3);
+        assertTrue(list.containsAll(expectedList) && expectedList.containsAll(list));
+
+        list = tree.getNodesBetween(6, 8);
+        expectedList = Arrays.asList();
+        assertTrue(list.containsAll(expectedList) && expectedList.containsAll(list));
+
+        list = tree.getNodesBetween(4, 6);
+        expectedList = Arrays.asList();
+        assertTrue(list.containsAll(expectedList) && expectedList.containsAll(list));
+
+        list = tree.getNodesBetween(1, 6);
+        expectedList = Arrays.asList(2, 4, 3);
+        assertTrue(list.containsAll(expectedList) && expectedList.containsAll(list));
+
+        list = tree.getNodesBetween(1, 8);
+        expectedList = Arrays.asList(2, 6, 4, 3);
+        assertTrue(list.containsAll(expectedList) && expectedList.containsAll(list));
+
+
+        // 当n1, n2不一定为某节点值时
+        list = tree.getNodesBetween(5, 8);
+        expectedList = Arrays.asList(6);
+        assertTrue(list.containsAll(expectedList) && expectedList.containsAll(list));
+
+        list = tree.getNodesBetween(-1, 9);
+        expectedList = Arrays.asList(1, 2, 6, 4, 3, 8);
+        assertTrue(list.containsAll(expectedList) && expectedList.containsAll(list));
 
     }
 
