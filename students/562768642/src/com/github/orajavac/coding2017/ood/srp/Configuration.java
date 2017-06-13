57ec996333fbabdd4fc210f3a6701b96941e8d0f
orajavac <oraclegit@126.com>
2017-06-13 15:17:51 +0800

    20170613_1517 测试提交

--- students/562768642/src/com/github/orajavac/coding2017/ood/srp/Configuration.java
+++ students/562768642/src/com/github/orajavac/coding2017/ood/srp/Configuration.java
@@ -9,7 +9,7 @@ public class Configuration {
 		configurations.put(ConfigurationKeys.SMTP_SERVER, "smtp.163.com");
 		configurations.put(ConfigurationKeys.ALT_SMTP_SERVER, "smtp1.163.com");
 		configurations.put(ConfigurationKeys.EMAIL_ADMIN, "admin@company.com");
-	}
+	} 
 	/**
 	 * 应该从配置文件读， 但是这里简化为直接从一个map 中去读
 	 * @param key
