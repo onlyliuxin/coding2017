a4db50f56730011b4ec1da07cbe4f8f83a57cf03
Administrator <Administrator@PC-20170608ABTY>
2017-06-13 22:00:35 +0800

    test

--- students/1054806067/src/com/zhangqian/test/Test.java
+++ students/1054806067/src/com/zhangqian/test/Test.java
@@ -1,7 +1,19 @@
 package com.zhangqian.test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		test("test");
+		
+	}
+	public static void test(String a){
+		ArrayList<String> list = new ArrayList<String>();
+		HashSet<String> set = new HashSet<String>();
+		set.add("1");
+		list.add(a);
+		Iterator<String> iterator = list.iterator();
 	}
 }
