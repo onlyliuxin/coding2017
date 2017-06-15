8f8bfce1102861533ea3094174a094b4fbf6a5b9
macvis <macvis@126.com>
2017-06-15 10:19:27 +0800

    maven structure

---
+++ students/75939388/ood/src/test/java/srp/SrpTest.java
@@ -0,0 +1,23 @@
+package srp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Tee on 2017/6/15.
+ */
+public class SrpTest {
+
+    @Before
+    public void init(){
+
+    }
+
+    /**
+     * 重构后的代码尝试运行
+     */
+    @Test
+    public void runTrial(){
+
+    }
+}
