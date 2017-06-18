3c6d5caf01bda49078d729ffa08eeef8cbde66ac
Z_Z.W <myhongkongzhen@gmail.com>
2017-06-18 14:56:13 +0800

    Added *.java signature.

--- students/511134962/ood-assignment/src/main/java/com/coderising/ood/srp/ConfigurationKeys.java
+++ students/511134962/ood-assignment/src/main/java/com/coderising/ood/srp/ConfigurationKeys.java
@@ -1,3 +1,11 @@
+/**********************************************************************************************************************
+ * Copyright (c) 2017. Lorem ipsum dolor sit amet, consectetur adipiscing elit.                                       *
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.                        *
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.                                                   *
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.                     *
+ * Vestibulum commodo. Ut rhoncus gravida arcu.                                                                       *
+ **********************************************************************************************************************/
+
 package com.coderising.ood.srp;
 
 public class ConfigurationKeys {
