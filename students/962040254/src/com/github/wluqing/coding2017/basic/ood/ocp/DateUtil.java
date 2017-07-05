f03f7ba8d9382e290a4c97a8c43fc115f1dc3e12
Michael <wangluqing.cs@gmail.com>
2017-07-05 17:30:31 +0800

    commit test undo

--- students/962040254/src/com/github/wluqing/coding2017/basic/ood/ocp/DateUtil.java
+++ students/962040254/src/com/github/wluqing/coding2017/basic/ood/ocp/DateUtil.java
@@ -1,11 +1,10 @@
 package com.github.wluqing.coding2017.basic.ood.ocp;
 
 public class DateUtil {
-	
+
 	public static String getCurrentDateAsString() {
-		// commit test
+		
 		return null;
 	}
 
 }
-//commit test
