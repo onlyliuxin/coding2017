a98dea43a6c2e380ac86b5fc00c24f16194689bb
onlyliuxin <14703250@qq.com>
2017-07-10 16:10:13 +0800

    tag builder

--- liuxin/ood/ood-assignment/src/main/java/com/coderising/dp/builder/TagBuilder.java
+++ liuxin/ood/ood-assignment/src/main/java/com/coderising/dp/builder/TagBuilder.java
@@ -1,37 +1,30 @@
 package com.coderising.dp.builder;
 
 public class TagBuilder {
-	private TagNode rootNode;
-	private TagNode currentNode;
-	private TagNode parentNode;
+	
 	public TagBuilder(String rootTagName){
-		rootNode = new TagNode(rootTagName);
-		currentNode = rootNode;
-		parentNode = null;
+		
 	}
 	
 	public TagBuilder addChild(String childTagName){
-		parentNode = this.currentNode;
-		this.currentNode = new TagNode(childTagName);
-		parentNode.add(currentNode);
-		return this;
+		
+		return null;
 	}
 	public TagBuilder addSibling(String siblingTagName){
 	
-		this.currentNode = new TagNode(siblingTagName);
-		parentNode.add(this.currentNode);
-		return this;
+		
+		return null;
 		
 	}
 	public TagBuilder setAttribute(String name, String value){
-		this.currentNode.setAttribute(name, value);
-		return this;
+		
+		return null;
 	}
 	public TagBuilder setText(String value){
-		this.currentNode.setValue(value);
-		return this;
+		
+		return null;
 	}
 	public String toXML(){
-		return this.rootNode.toXML();
+		return null;
 	}
 }
