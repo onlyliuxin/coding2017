e976ae32574869f7d1636139367d087f9da234a4
lorcx <740707954@qq.com>
2017-07-15 12:50:48 +0800

    修改为maven项目
    修改为maven项目

---
+++ students/740707954/src/main/java/740707954/App.java
@@ -0,0 +1,13 @@
+package 740707954;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
