77d3fdd9574adfe6119e24338937986b1f6d354c
Thomas Young <yk_ecust_2007@163.com>
2017-07-26 00:17:14 +0800

    [feature] 实现组合(Composite)模式

--- students/812350401/src/main/java/com/coderising/mydp/composite/Rectangle.java
+++ students/812350401/src/main/java/com/coderising/mydp/composite/Rectangle.java
@@ -5,7 +5,7 @@ public class Rectangle implements Shape {
 	@Override
 	public void draw() {
 		// TODO Auto-generated method stub
-
+		System.out.println("Rectangle");
 	}
 
 }
--- students/812350401/src/main/java/com/coderising/mydp/composite/Square.java
+++ students/812350401/src/main/java/com/coderising/mydp/composite/Square.java
@@ -5,7 +5,7 @@ public class Square implements Shape {
 	@Override
 	public void draw() {
 		// TODO Auto-generated method stub
-
+		System.out.println("Square");
 	}
 
 }
