2d33ec72ed7eb047da9186c5d1f1da7d07ca40f0
bendell02 <1203029076@qq.com>
2017-08-03 01:32:20 +0800

    作业4：实现Composite

---
+++ students/1203029076/ood/ood-assignment/src/main/java/com/coderising/dp/composite/Rectangle.java
@@ -0,0 +1,11 @@
+package com.coderising.dp.composite;
+
+public class Rectangle implements Shape {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("I'm a rectangle");
+	}
+
+}
