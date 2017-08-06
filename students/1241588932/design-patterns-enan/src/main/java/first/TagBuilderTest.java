21714123c4bf094835b147e7b03fc6ad8b03280a
doublesouth <1241588932@qq.com>
2017-08-06 12:32:45 +0800

    1241588932:设计模式第一次作业

---
+++ students/1241588932/design-patterns-enan/src/main/java/first/TagBuilderTest.java
@@ -0,0 +1,41 @@
+package first;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TagBuilderTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testToXML() {
+		
+		TagBuilder builder = new TagBuilder("order");
+		
+		String xml = builder.addChild("line-items")
+			.addChild("line-item").setAttribute("pid", "P3677").setAttribute("qty", "3")
+			.addSibling("line-item").setAttribute("pid", "P9877").setAttribute("qty", "10")
+			.build()
+			.toXML();
+		
+		String expected = "<order>"
+				+ "<line-items>"
+				+ "<line-item pid=\"P3677\" qty=\"3\"/>"
+				+ "<line-item pid=\"P9877\" qty=\"10\"/>"
+				+ "</line-items>"
+				+ "</order>";
+		
+		System.out.println(xml);
+		assertEquals(expected, xml);
+	}
+
+}
