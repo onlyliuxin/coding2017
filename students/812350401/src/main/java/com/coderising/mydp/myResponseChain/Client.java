3e87b07a586d5564d454cac0da3835791b83ad1d
Thomas Young <yk_ecust_2007@163.com>
2017-08-10 20:30:51 +0800

    [Train] 责任链模式尝试

---
+++ students/812350401/src/main/java/com/coderising/mydp/myResponseChain/Client.java
@@ -0,0 +1,36 @@
+package com.coderising.mydp.myResponseChain;
+
+/**
+ * Created by thomas_young on 10/8/2017.
+ */
+
+public class Client {
+
+    public static void main(String[] args) {
+        //先要组装责任链
+        Handler h1 = new GeneralManager();
+        Handler h2 = new DeptManager();
+        Handler h3 = new ProjectManager();
+        h3.setSuccessor(h2);
+        h2.setSuccessor(h1);
+
+        //开始测试
+        String test1 = h3.handleFeeRequest("张三", 300);
+        System.out.println("test1 = " + test1);
+        String test2 = h3.handleFeeRequest("李四", 300);
+        System.out.println("test2 = " + test2);
+        System.out.println("---------------------------------------");
+
+        String test3 = h3.handleFeeRequest("张三", 700);
+        System.out.println("test3 = " + test3);
+        String test4 = h3.handleFeeRequest("李四", 700);
+        System.out.println("test4 = " + test4);
+        System.out.println("---------------------------------------");
+
+        String test5 = h3.handleFeeRequest("张三", 1500);
+        System.out.println("test5 = " + test5);
+        String test6 = h3.handleFeeRequest("李四", 1500);
+        System.out.println("test6 = " + test6);
+    }
+
+}
