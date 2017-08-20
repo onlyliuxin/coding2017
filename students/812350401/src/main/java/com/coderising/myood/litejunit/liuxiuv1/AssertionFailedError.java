c46ab2eb569c80d91bc6e80ffc624cfce1fb5905
Thomas Young <yk_ecust_2007@163.com>
2017-08-20 21:44:51 +0800

    [Feature] 刘老师的junit-v1 init

---
+++ students/812350401/src/main/java/com/coderising/myood/litejunit/liuxiuv1/AssertionFailedError.java
@@ -0,0 +1,13 @@
+package com.coderising.myood.litejunit.liuxiuv1;
+
+/**
+ * Thrown when an assertion failed.
+ */
+public class AssertionFailedError extends Error {
+
+	public AssertionFailedError () {
+	}
+	public AssertionFailedError (String message) {
+		super (message);
+	}
+}
---
+++ students/812350401/src/main/java/com/coderising/myood/litejunit/liuxiuv1/Test.java
@@ -0,0 +1,6 @@
+package com.coderising.myood.litejunit.liuxiuv1;
+
+public interface Test {
+	public abstract int countTestCases();
+	public void run(TestResult tr);
+}
